920689174cdc4a62a30e42f8baddb78b7cfa68a1
Akim Demaille <demaille@gostai.com>
2012-02-20 11:54:36 +0100

    tests: hook rat-exp check.
    * tests/rat_exp/rat_exp.test: Rename as...
    * tests/rat_exp/test.rat_exp: this.
    * tests/rat_exp/local.mk: New.
    * tests/Makefile.am: Load it.

---
+++ tests/rat_exp/local.mk
@@ -0,0 +1,5 @@
+dist_noinst_SCRIPTS = rat_exp/rat_exp.pl
+TEST_EXTENSIONS = .rat_exp
+RAT_EXP_LOG_COMPILER = $(srcdir)/rat_exp/rat_exp.pl
+AM_RAT_EXP_LOG_FLAGS = $(top_builddir)/vcsn/io/rat-exp
+TESTS += rat_exp/test.rat_exp
