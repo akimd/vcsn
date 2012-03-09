a08be368b202d07fac7474ba69dd8acff86524b4
Akim Demaille <akim@lrde.epita.fr>
2012-03-09 09:59:11 +0100

    maint: fix automake warnings.
    	* tests/rat_exp/local.mk: here.

--- tests/rat_exp/local.mk
+++ tests/rat_exp/local.mk
@@ -1,4 +1,17 @@
-dist_noinst_SCRIPTS = rat_exp/rat_exp.pl
+# Vaucanson 2, a generic library for finite state machines.
+# Copyright (C) 2012 Vaucanson Group.
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# The complete GNU General Public Licence Notice can be found as the
+# `COPYING' file in the root directory.
+#
+# The Vaucanson Group consists of people listed in the `AUTHORS' file.
+
+dist_noinst_SCRIPTS += rat_exp/rat_exp.pl
 TEST_EXTENSIONS = .rat_exp
 RAT_EXP_LOG_COMPILER = $(srcdir)/rat_exp/rat_exp.pl
 AM_RAT_EXP_LOG_FLAGS = $(top_builddir)/vcsn/io/rat-exp
