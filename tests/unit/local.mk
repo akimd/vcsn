bf8ddae636cdec0a159e93d05682a0fc09bb67cf
Akim Demaille <akim@lrde.epita.fr>
2012-03-13 15:45:59 +0100

    test: derecurse tests/unit.
    * tests/unit/Makefile.am: Rename as...
    * tests/unit/local.mk: this.
    Adjust, and simplify.
    * configure.ac, tests/Makefile.am: Adjust.

---
+++ tests/unit/local.mk
@@ -0,0 +1,10 @@
+noinst_PROGRAMS = 				\
+  unit/ladybird-b unit/ladybird-z 		\
+  unit/poly					\
+  unit/simplaw unit/simpleaut
+
+unit_ladybird_b_SOURCES = unit/ladybird.cc
+unit_ladybird_b_CPPFLAGS = $(AM_CPPFLAGS) -DW=b
+
+unit_ladybird_z_SOURCES = unit/ladybird.cc
+unit_ladybird_z_CPPFLAGS = $(AM_CPPFLAGS) -DW=z
