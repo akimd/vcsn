b51f74c3e5ef51e37cb5ab9d47fa1f7e953bbe85
Akim Demaille <akim@lrde.epita.fr>
2012-03-19 15:23:20 +0100

    rat::printer: typo.
    * vcsn/core/rat/printer.hxx: Fix opertor output.

--- vcsn/core/rat/printer.hxx
+++ vcsn/core/rat/printer.hxx
@@ -34,7 +34,7 @@ namespace vcsn
     void
     printer<WeightSet>::visit(const plus<WeightSet>& v)
     {
-      print_iterable(v, '.', out_, *this, show_unit_, debug_);
+      print_iterable(v, '+', out_, *this, show_unit_, debug_);
     }
 
     template <class WeightSet>
@@ -66,6 +66,72 @@ namespace vcsn
     {
       print_weight(v.left_weight(), v.get_weight_set(), out_, show_unit_);
       out_ <<* v.get_word();
+    }
+
+    template <class WeightSet>
+    void
+    print_weight(const typename WeightSet::value_t& w,
+                 const WeightSet& ws, std::ostream& out,
+                 const double show_unit)
+    {
+      if (show_unit || !ws.is_unit(w))
+        out << '{' << w << '}';
+    }
+
+    template <class Iterate>
+    void
+    print_iterable(const Iterate& o,
+                   const char op,
+                   std::ostream& out,
+                   typename visitor_traits<typename Iterate::weightset_t>::ConstVisitor& v,
+                   const bool show_unit,
+                   const bool debug)
+    {
+      if (!o.empty())
+      {
+        print_weight(o.left_weight(), o.get_weight_set(), out, show_unit);
+
+        if (debug)
+          out << op;
+        for (unsigned i = o.size(); i != 0; --i)
+          out << '(';
+
+        auto end = o.end();
+        auto it = o.begin();
+        (* it)->accept(v);
+        ++it;
+        for(; end != it; ++it)
+        {
+          out << op;
+          (* it)->accept(v);
+          out << ')';
+        }
+        out << ')';
+
+        print_weight(o.right_weight(), o.get_weight_set(), out, show_unit);
+      }
+    }
+
+    template <class PostExp>
+    void
+    print_post_exp(const PostExp& e,
+                   const char op,
+                   std::ostream& out,
+                   typename visitor_traits<typename PostExp::weightset_t>::ConstVisitor& v,
+                   const bool show_unit,
+                   const bool debug)
+    {
+      auto sub_exp = e.get_sub();
+      if (sub_exp != nullptr)
+      {
+        print_weight(e.left_weight(), e.get_weight_set(), out, show_unit);
+        if (debug)
+          out << op;
+        out << '(';
+        sub_exp->accept(v);
+        out << ')' << op;
+        print_weight(e.right_weight(), e.get_weight_set(), out, show_unit);
+      }
     }
 
   } // !rat
