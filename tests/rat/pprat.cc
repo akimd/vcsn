0cb6368045f75b22d8ea51c94bb0aaa567f73caa
Akim Demaille <akim@lrde.epita.fr>
2012-03-27 15:12:58 +0200

    pp: command line interface.
    * tests/rat/pprat.cc: Support -e, -f, -w.
    From gdb, it is easier to run a program only with command line
    arguments.
    * tests/rat/rat: Adjust.

--- tests/rat/pprat.cc
+++ tests/rat/pprat.cc
@@ -1,4 +1,5 @@
 #include <cstdlib>
+#include <getopt.h>
 
 #include <vcsn/weights/b.hh>
 #include <vcsn/weights/z.hh>
@@ -9,45 +10,75 @@ static
 void
 usage(const char* prog, int status)
 {
-  std::cerr <<
-    "usage: " << prog << " WEIGHT-SET\n"
-    "\n"
-    "WEIGHT-SET:\n"
-    "  z    for Z\n"
-    "  b    for Boolean\n"
-    "  rat  for RatExp<b>\n";
+  if (status == EXIT_SUCCESS)
+    std::cout <<
+      "usage: " << prog << " [OPTION...] [EXP...]\n"
+      "\n"
+      "Options:\n"
+      "  -w WEIGHT-SET   define the kind of the weights [b]\n"
+      "  -e EXP          pretty-print the rational expression EXP\n"
+      "  -f FILE         pretty-print the rational expression in FILE\n"
+      "\n"
+      "WEIGHT-SET:\n"
+      "  z    for Z\n"
+      "  b    for Boolean\n"
+      "  rat  for RatExp<b>\n";
+  else
+    std::cerr << "Try `" << prog << " --help' for more information."
+              << std::endl;
   exit(status);
 }
 
-template <typename WeightSet>
-int
-pp(const WeightSet& ws)
+const vcsn::factory_<vcsn::b> b_fact{ vcsn::b() };
+const vcsn::factory_<vcsn::z> z_fact{ vcsn::z() };
+const vcsn::factory_<vcsn::factory_<vcsn::b>> rat_fact{ b_fact };
+
+
+void
+pp(const std::string& w, const char* s, bool file)
 {
-  typedef WeightSet weightset_t;
-  typedef typename weightset_t::value_t weight_t;
-  vcsn::factory_<weightset_t> factory(ws);
-  vcsn::rat::driver d(factory);
-  if (vcsn::rat::exp* e = d.parse_file("-"))
-    factory.print(std::cout, e) << std::endl;
-  return 1;
+  const vcsn::factory* factory = nullptr;
+  if (w == "b")
+    factory = &b_fact;
+  else if (w == "rat")
+    factory = &rat_fact;
+  else
+    factory = &z_fact;
+  vcsn::rat::driver d(*factory);
+  if (vcsn::rat::exp* e = file ? d.parse_file(s) : d.parse_string(s))
+    factory->print(std::cout, e) << std::endl;
+  else
+    exit(EXIT_FAILURE);
 }
 
 int
-main(int argc, const char* argv[])
+main(int argc, char* const argv[])
 {
-  if (argc != 2)
-    usage(argv[0], EXIT_FAILURE);
-  std::string w(argv[1]);
-
-  if (w == "z")
-    return pp(vcsn::z());
-  else if (w == "b")
-    return pp(vcsn::b());
-  else if (w == "rat")
-    {
-      vcsn::factory_<vcsn::b> rat{ vcsn::b() };
-      return pp(rat);
-    }
-  else
-    usage(argv[0], EXIT_FAILURE);
+  int opt;
+  std::string w = "b";
+  while ((opt = getopt(argc, argv, "e:f:hw:")) != -1)
+    switch (opt)
+      {
+      case 'e':
+        pp(w, optarg, false);
+        break;
+      case 'f':
+        pp(w, optarg, true);
+        break;
+      case 'w':
+        w = optarg;
+        if (w != "b" && w != "rat" && w != "z")
+          usage(argv[0], EXIT_FAILURE);
+        break;
+      case 'h':
+        usage(argv[0], EXIT_SUCCESS);
+        break;
+      case '?':
+        usage(argv[0], EXIT_FAILURE);
+        break;
+      }
+  argc -= optind;
+  argv += optind;
+  for (int i = 0; i < argc; ++i)
+    pp(w, argv[i], false);
 }
