37d9a56e21ae90df4d3cd39449d825df55974e6d
Akim Demaille <akim@lrde.epita.fr>
2012-04-11 17:37:34 +0200

    down_caster: improve.
    * vcsn/misc/cast.hh (down_caster): Factor, and hide in a hidden namespace.

--- vcsn/misc/cast.hh
+++ vcsn/misc/cast.hh
@@ -1,9 +1,9 @@
 #ifndef VCSN_MISC_CAST_HH
 # define VCSN_MISC_CAST_HH
+
 # ifdef NDEBUG
 #  define down_cast static_cast
 #  define maybe_down_cast static_cast
-
 # else
 
 template <typename T, typename U>
@@ -14,40 +14,46 @@ maybe_down_cast(U t)
   return dynamic_cast<const T>(t);
 }
 
-struct down_caster
+namespace vcsn
 {
-  down_caster(const char* f, int l)
-    : file(f), line(l)
-  {}
-  const char* file;
-  int line;
-
-  template <typename T, typename U>
-  inline
-  T
-  cast(U t)
+  namespace details
   {
-    if (!t)
+    struct down_caster
+    {
+      down_caster(const char* f, int l)
+        : file(f), line(l)
+      {}
+      const char* file;
+      int line;
+    
+      template <typename T, typename U>
+      void
+      error(const char* msg)
       {
-        std::cerr << file << ":" << line << ": down_casting nullptr"
-                  << " from " << typeid(U).name() << " to " << typeid(T).name()
-                  << std::endl;
+        std::cerr
+          << file << ":" << line << ": " << msg
+          << " from " << typeid(U).name() << " to " << typeid(T).name()
+          << std::endl;
         abort();
       }
-    T res = maybe_down_cast<T>(t);
-    if (!res)
-      {
-        std::cerr << file << ":" << line << ": failed down_cast"
-                  << " from " << typeid(U).name() << " to " << typeid(T).name()
-                  << std::endl;
-        abort();
-      }
-    return res;
-  }
-};
 
+      template <typename T, typename U>
+      inline
+      T
+      cast(U t)
+      {
+        if (!t)
+          error<T, U>("down_casting nullptr");
+        T res = dynamic_cast<const T>(t);
+        if (!res)
+          error<T, U>("failed down_cast");
+        return res;
+      }
+    };
+  }
+}
 #define down_cast                               \
-  down_caster(__FILE__, __LINE__).cast
+  ::vcsn::details::down_caster(__FILE__, __LINE__).cast
 #endif
 
 #endif // !VCSN_MISC_CAST_HH
