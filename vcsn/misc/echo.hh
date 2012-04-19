1c0897c904bb72e91a23ccae1a7e89fd53eb5a05
Akim Demaille <akim@lrde.epita.fr>
2012-04-19 11:22:51 +0200

    ECHOV.
    * vcsn/misc/echo.hh (ECHOV): New.

--- vcsn/misc/echo.hh
+++ vcsn/misc/echo.hh
@@ -1,7 +1,8 @@
 #ifndef VCSN_MISC_ECHO_HH
 # define VCSN_MISC_ECHO_HH
 
-# define ECHO(S) std::cerr << S << std::endl
 # define V(S) #S ": " << S << " "
+# define ECHO(S) std::cerr << S << std::endl
+# define ECHOV(S) ECHO(V(S))
 
 #endif // !VCSN_MISC_ECHO_HH
