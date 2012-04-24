58165b7c0a36c3c6d406cecd95d0277f1c231aba
Akim Demaille <akim@lrde.epita.fr>
2012-04-24 13:44:08 +0200

    echo: more debug macros.
    * vcsn/misc/echo.hh (VV): New.

--- vcsn/misc/echo.hh
+++ vcsn/misc/echo.hh
@@ -1,8 +1,16 @@
 #ifndef VCSN_MISC_ECHO_HH
 # define VCSN_MISC_ECHO_HH
 
-# define V(S) #S ": " << S << " "
+/// Display S and its value.
+# define V(S)                                   \
+  #S ": " << S << " "
+
+/// Display S and its value in pseudo XML tags, in case of nesting.
+# define VV(S)                                                          \
+  "<" #S ":" __FILE__ ":" << __LINE__ << ">" << S << "</" << #S << ">"
+
 # define ECHO(S) std::cerr << S << std::endl
+
 # define ECHOV(S) ECHO(V(S))
 
 #endif // !VCSN_MISC_ECHO_HH
