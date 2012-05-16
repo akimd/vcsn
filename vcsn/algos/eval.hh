09de3dd1b560a5f1eefa16744d2031a7e83e1c98
David Moreira <moreira@lrde.epita.fr>
2012-05-16 11:15:36 +0200

    Rewrite evaluation algorithm for lal automaton.
    * vcsn/algos/eval.hh: rewrite in polynomial time.

--- vcsn/algos/eval.hh
+++ vcsn/algos/eval.hh
@@ -1,8 +1,8 @@
 #ifndef VCSN_ALGOS_EVAL_HH
 # define VCSN_ALGOS_EVAL_HH
 
-# include <vector>
-# include <utility>
+# include <algorithm>
+# include <map>
 # include <cassert>
 
 //# include <vcsn/core/crange.hh>
@@ -13,9 +13,11 @@ namespace vcsn {
   class eval_functor
   {
     using automaton_t = Aut;
+    using state_t = typename automaton_t::state_t;
     using word_t = typename automaton_t::genset_t::word_t;
     using weightset_t = typename automaton_t::weightset_t;
     using weight_t = typename weightset_t::value_t;
+    using weights = std::map<state_t, weight_t>;
 
   public:
     eval_functor(const automaton_t& a)
@@ -23,76 +25,45 @@ namespace vcsn {
     , w_(a_.weightset())
     {}
 
-    weight_t operator()(const word_t& w) const
+    weight_t operator()(const word_t& word) const
     {
-      // if the automaton isn't empty
-      // KLUDGE: We want to use `automaton_t::empty()'
-      if (a_.num_states() == 0)
-        return w_.zero();
-
       // Initialize
-      const size_t size = w.size() + 1;
-      weight_t res = w_.zero();
-      auto initial_transitions = a_.initial_transitions();
-      auto it = initial_transitions.begin();
-      auto end_it = initial_transitions.end();
+      const weight_t zero = w_.zero();
+      weights v1;
+      weights v2;
+      for (auto s : a_.states())
+        v1.insert(std::make_pair(s, zero));
 
-      if(it == end_it)
-        return res; // No initial states
+      for (auto init : a_.initial_transitions())
+        v1[a_.dst_of(init)] = w_.unit();
 
-      std::vector<std::pair<decltype(end_it), decltype(end_it)>> tr_stack;
-      tr_stack.reserve(size);
-      tr_stack.push_back(std::make_pair(a_.out(*it).begin(), a_.out(*it).end()));
-
-      // Create lambda
-      auto next_path = [&] {
-        while (!tr_stack.empty() && (++(tr_stack.back().first) == tr_stack.back().second))
-          {
-            tr_stack.pop_back();
-          }
-      };
-      auto reduce = [&] () -> weight_t {
-        weight_t accu = w_.unit();
-        // FIXME: Check if we apply `mul' function in the good direction
-        for (auto s : tr_stack)
-          {
-            // KLUDGE: operator *= or equivalent (temp var)
-            accu = w_.mul(accu, a_.weight_of(*s.first));
-          }
-        return accu;
-      };
-
-      while (!tr_stack.empty())
+      // Computation
+      for (auto l : word)
         {
-          auto out_container = a_.out(a_.dst_of(*(tr_stack.back().first)), w[tr_stack.size() - 1]);
-
-          auto it = out_container.begin();
-          auto end = out_container.end();
-          if (end == it)
-            // No path to continue
-            // Reduce the stack to find next path
-            next_path();
-          else
+          for (auto& w : v2)
+            w.second = zero;
+          for (auto w : v1)
             {
-              tr_stack.push_back(std::make_pair(it, end));
-              if (tr_stack.size() == size)
+              if (zero != w.second)
                 {
-                  // Each transitions read the whole word
-                  while (tr_stack.back().first != tr_stack.back().second)
+                  for (auto t : a_.out(w.first, l))
                     {
-                      // Get the weight for each path
-                      if (a_.is_final(a_.dst_of((*tr_stack.back().first))))
-                        res = w_.add(res, reduce()); // FIXME: The weight could be accumulate before
-                      ++(tr_stack.back().first);
+                      auto& weight = v2[a_.dst_of(t)];
+                      weight = w_.add(weight, w_.mul(w.second, a_.weight_of(t)));
                     }
-                  tr_stack.pop_back();
-                  next_path();
                 }
             }
+          std::swap(v1, v2);
         }
-
+      weight_t res = zero;
+      for (auto w : v1)
+        {
+          if (zero != w.second)
+            res = w_.add(res, w_.mul(w.second, a_.get_final_weight(w.first)));
+        }
       return res;
     }
+
   private:
     const automaton_t& a_;
     const weightset_t& w_;
