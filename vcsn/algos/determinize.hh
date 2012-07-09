154625c9dade5f7617c65bbee4bb36bf00800f40
Akim Demaille <akim@lrde.epita.fr>
2012-07-09 18:10:45 +0200

    determinize: simplify.
    * vcsn/algos/determinize.hh: here.

--- vcsn/algos/determinize.hh
+++ vcsn/algos/determinize.hh
@@ -7,77 +7,66 @@
 
 namespace vcsn
 {
-  namespace details
-  {
-    template <class Aut>
-    struct determinize_functor
-    {
-      using automaton_t = Aut;
-      using state_t = typename automaton_t::state_t;
-      using state_set = std::set<state_t>;
-      using stack = std::stack<state_set>;
-      using map = std::map<state_set, state_t>;
-
-      automaton_t operator()(const automaton_t& a)
-      {
-        state_set initial;
-        for (auto t : a.initial_transitions())
-          initial.insert(a.dst_of(t));
-
-        automaton_t out(a.context());
-        if (initial.empty())
-          return out;
-
-        stack st;
-        map m;
-        // Create a new state. Insert in the output automaton, in the
-        // map, and push in the stack.
-        auto push_new_state = [&] (state_set ss) -> state_t {
-          state_t res = out.new_state();
-          m[ss] = res;
-
-          for (auto e : ss)
-            if (a.is_final(e))
-              {
-                out.set_final(res);
-                break;
-              }
-          st.push(ss);
-          return res;
-        };
-
-
-        out.set_initial(push_new_state(initial));
-
-        while (!st.empty())
-          {
-            auto ss = st.top();
-            st.pop();
-            const auto& genset = *a.genset();
-            for (auto gen : genset)
-              {
-                state_set next;
-                for (auto s : ss)
-                  for (auto t : a.out(s, gen))
-                    next.insert(a.dst_of(t));
-                auto i = m.find(next);
-                state_t n = (i == m.end()) ? push_new_state(next) : i->second;
-
-                out.add_transition(m[ss], n, gen);
-              }
-          }
-        return out;
-      }
-    };
-  } // namespace details
 
   template <class Aut>
   inline
   Aut
-  determinize(const Aut& aut)
+  determinize(const Aut& a)
   {
-    details::determinize_functor<Aut> df;
-    return df(aut);
+    using automaton_t = Aut;
+    using state_t = typename automaton_t::state_t;
+    using state_set = std::set<state_t>;
+    using stack = std::stack<state_set>;
+    using map = std::map<state_set, state_t>;
+
+    state_set initial;
+    for (auto t : a.initial_transitions())
+      initial.insert(a.dst_of(t));
+
+    automaton_t res{a.context()};
+    if (initial.empty())
+      return res;
+
+    stack st;
+    map m;
+    // Create a new state. Insert in the output automaton, in the
+    // map, and push in the stack.
+    auto push_new_state =
+      [&res,&m,&a,&st] (state_set ss) -> state_t
+      {
+        state_t r = res.new_state();
+        m[ss] = r;
+
+        for (auto e : ss)
+          if (a.is_final(e))
+            {
+              res.set_final(r);
+              break;
+            }
+        st.push(ss);
+        return r;
+      };
+
+    res.set_initial(push_new_state(initial));
+
+    while (!st.empty())
+      {
+        auto ss = st.top();
+        st.pop();
+        const auto& genset = *a.genset();
+        for (auto gen : genset)
+          {
+            state_set next;
+            for (auto s : ss)
+              for (auto t : a.out(s, gen))
+                next.insert(a.dst_of(t));
+            auto i = m.find(next);
+            state_t n = (i == m.end()) ? push_new_state(next) : i->second;
+
+            res.add_transition(m[ss], n, gen);
+          }
+      }
+    return res;
   }
 } // namespace vcsn
 
