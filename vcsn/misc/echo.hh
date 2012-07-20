1715311342024b566a81c8107f1f77163a2100a4
Akim Demaille <akim@lrde.epita.fr>
2012-07-20 11:22:37 +0200

    echo: printers for vectors and sets.
    * vcsn/misc/echo.hh: here.

--- vcsn/misc/echo.hh
+++ vcsn/misc/echo.hh
@@ -1,6 +1,9 @@
 #ifndef VCSN_MISC_ECHO_HH
 # define VCSN_MISC_ECHO_HH
 
+# include <set>
+# include <vector>
+
 /// Display S and its value.
 # define V(S)                                   \
   #S ": " << S << " "
@@ -16,5 +19,38 @@
 # define __HERE__ __FILE__ ": " << __LINE__
 
 # define ECHOH(S) ECHO(__HERE__ << ": " << S)
+
+namespace std
+{
+  template <typename T>
+  ostream&
+  operator<<(ostream& o, set<T>& ts)
+  {
+    bool first = true;
+    for (const auto& t: ts)
+      {
+        if (!first)
+          o << ' ';
+        o << t;
+        first = false;
+      }
+    return o;
+  }
+
+  template <typename T>
+  ostream&
+  operator<<(ostream& o, vector<T>& ts)
+  {
+    bool first = true;
+    for (const auto& t: ts)
+      {
+        if (!first)
+          o << ' ';
+        o << t;
+        first = false;
+      }
+    return o;
+  }
+}
 
 #endif // !VCSN_MISC_ECHO_HH
