cdd8da32adc51cc6997898bc0fe60770349a5937
Akim Demaille <akim@lrde.epita.fr>
2012-07-25 09:30:42 +0200

    tests: poly: make the test more self-contained.
    We really need to move to either gtest or Boost.Tests.
    * tests/unit/poly.cc: Check for string equalities here, instead of...
    * tests/unit/poly.chk: here.

--- tests/unit/poly.cc
+++ tests/unit/poly.cc
@@ -1,20 +1,22 @@
 #include <cassert>
 #include <vcsn/ctx/char_z_lal.hh>
 #include <vcsn/weights/poly.hh>
+#include <tests/unit/test.hh>
 
 int main()
 {
+  bool res = true;
   using context_t = vcsn::ctx::char_z_lal;
   context_t ctx {{'a', 'b', 'c', 'd'}};
   using poly_t = vcsn::polynomials<context_t>;
-  poly_t poly(ctx);
+  poly_t poly{ctx};
 
   poly_t::value_t u = poly.unit();
   poly.assoc(u, "ab", 12);
-  poly.print(std::cout, u) << std::endl;
+  ASSERT_EQ(poly.format(u), "\\e + {12}ab");
 
   u = poly.add(u, u);
-  poly.print(std::cout, u) << std::endl;
+  ASSERT_EQ(poly.format(u), "{2}\\e + {24}ab");
 
   assert(!poly.is_zero(u));
   assert(poly.is_zero(poly.zero()));
@@ -25,10 +27,12 @@ int main()
   assert(u == v);
 
   u = poly.mul(u, u);
-  poly.print(std::cout, u) << std::endl;
+  ASSERT_EQ(poly.format(u), "{4}\\e + {96}ab + {576}abab");
 
   poly.add_assoc(v, "ab", 96 - 24);
   poly.assoc(v, "", 4);
   poly.assoc(v, "abab", 576);
   assert(u == v);
+
+  return !res;
 }
