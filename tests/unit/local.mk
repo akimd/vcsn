ebf7edf1fc81f4e906f3c26c720d9b7819735abd
Akim Demaille <akim@lrde.epita.fr>
2012-07-31 16:10:53 +0200

    tests: rename.
    * tests/unit/simpleaut.cc: Rename as...
    * tests/unit/aut_char_z_lal.cc: this.
    * tests/unit/simpleaut.chk: Rename as...
    * tests/unit/aut_char_z_lal.chk: this.
    * tests/unit/simplaw.cc: Rename as...
    * tests/unit/aut_char_z_law.cc: this.
    * tests/unit/simplaw.chk: Rename as...
    * tests/unit/aut_char_z_law.chk: this.
    * tests/unit/local.mk: Adjust.

--- tests/unit/local.mk
+++ tests/unit/local.mk
@@ -12,23 +12,27 @@
 # The Vaucanson Group consists of people listed in the `AUTHORS' file.
 
 # Not check_PROGRAMS, see below why.
-EXTRA_PROGRAMS +=				\
-  unit/determinize				\
-  unit/eval					\
-  unit/ladybird-b 				\
-  unit/ladybird-z				\
-  unit/ladybird-zmin				\
-  unit/lift					\
-  unit/parse-dot				\
-  unit/poly					\
-  unit/product					\
-  unit/simplaw					\
-  unit/simpleaut				\
+EXTRA_PROGRAMS +=                               \
+  unit/aut_char_z_lal                           \
+  unit/aut_char_z_law                           \
+  unit/determinize                              \
+  unit/eval                                     \
+  unit/ladybird-b                               \
+  unit/ladybird-z                               \
+  unit/ladybird-zmin                            \
+  unit/lift                                     \
+  unit/parse-dot                                \
+  unit/poly                                     \
+  unit/product                                  \
   unit/transpose
 
 AM_LDADD = $(libvcsn)
 
 noinst_HEADERS = unit/test.hh
+
+unit_aut_char_z_lal_LDADD = $(libchar_z_lal) $(AM_LDADD)
+
+unit_aut_char_z_law_LDADD = $(libchar_z_law) $(AM_LDADD)
 
 unit_determinize_LDADD = $(libchar_b_lal) $(AM_LDADD)
 
@@ -54,29 +58,27 @@ unit_poly_LDADD = $(libchar_z_lal) $(AM_LDADD)
 
 unit_product_LDADD = $(libchar_z_lal) $(AM_LDADD)
 
-unit_simpleaut_LDADD = $(libchar_z_lal) $(AM_LDADD)
-
-unit_simplaw_LDADD = $(libchar_z_law) $(AM_LDADD)
-
 unit_transpose_LDADD = $(libchar_b_lal) $(libchar_z_lal) $(AM_LDADD)
 
-unit_TESTS =					\
-  unit/determinize.chk				\
-  unit/eval.chk					\
-  unit/ladybird.chk				\
-  unit/lift.chk					\
-  unit/parse-dot.chk				\
-  unit/poly.chk					\
-  unit/product.chk				\
-  unit/simplaw.chk				\
-  unit/simpleaut.chk				\
-  unit/standard_of.chk				\
+unit_TESTS =                                    \
+  unit/aut_char_z_lal.chk                       \
+  unit/aut_char_z_law.chk                       \
+  unit/determinize.chk                          \
+  unit/eval.chk                                 \
+  unit/ladybird.chk                             \
+  unit/lift.chk                                 \
+  unit/parse-dot.chk                            \
+  unit/poly.chk                                 \
+  unit/product.chk                              \
+  unit/standard_of.chk                          \
   unit/transpose.chk
 dist_TESTS += $(unit_TESTS)
 
 # Instead of using check_PROGRAMS, use EXTRA_PROGRAMS, but spell out
 # the dependencies, so that the test suite does not make useless
 # compilations.
+unit/aut_char_z_lal.log: unit/aut_char_z_lal
+unit/aut_char_z_law.log: unit/aut_char_z_law
 unit/aut_to_exp.log: rat/pprat
 unit/determinize.log: unit/determinize
 unit/eval.log: unit/eval
@@ -85,8 +87,6 @@ unit/lift.log: unit/lift
 unit/parse-dot.log: unit/parse-dot
 unit/poly.log: unit/poly
 unit/product.log: unit/product
-unit/simplaw.log: unit/simplaw
-unit/simpleaut.log: unit/simpleaut
 unit/standard_of.log: rat/pprat
 unit/transpose.log: unit/transpose
 
