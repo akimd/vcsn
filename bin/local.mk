c73844c8ddfe6abd3b3908f44c54fb490986578a
David Moreira <moreira@lrde.epita.fr>
2012-12-26 18:51:22 +0100

    Delete vcsn-exp-to-xml.
    * bin/local.mk, bin/vcsn-exp-to-xml.cc: Deleted, the feature is
      already included in vcsn-cat binaries.

--- bin/local.mk
+++ bin/local.mk
@@ -16,7 +16,6 @@ bin_PROGRAMS +=                                 \
   bin/vcsn-de-bruijn                            \
   bin/vcsn-determinize                          \
   bin/vcsn-evaluate                             \
-  bin/vcsn-exp-to-xml				\
   bin/vcsn-lift                                 \
   bin/vcsn-standard-of                          \
   bin/vcsn-transpose
@@ -28,6 +27,5 @@ bin_vcsn_evaluate_LDADD    = $(all_libctx) $(libvcsn)
 bin_vcsn_lift_LDADD        = $(all_libctx) $(libvcsn)
 bin_vcsn_standard_of_LDADD = $(all_libctx) $(libvcsn)
 bin_vcsn_transpose_LDADD   = $(all_libctx) $(libvcsn)
-bin_vcsn_exp_to_xml_LDADD  = $(all_libctx) $(libvcsn)
 
 bin_vcsn_cat_SOURCES = bin/parse-args.cc bin/vcsn-cat.cc
