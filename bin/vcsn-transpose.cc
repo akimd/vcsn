9429cafabd614931c1877b9efadccc3deb0c2957
Akim Demaille <akim@lrde.epita.fr>
2012-12-29 09:21:23 +0100

    bin: really make them filters
    * bin/vcsn-cat.cc, bin/vcsn-standard-of.cc, bin/vcsn-transpose.cc:
    Read the expression from a file instead of as a command line argument.
    * tests/rat/cat.chk, tests/rat/vcsn-transpose.chk: Adjust.
    * tests/unit/transpose.chk: Comment changes.

--- bin/vcsn-transpose.cc
+++ bin/vcsn-transpose.cc
@@ -8,15 +8,15 @@
 
 int main(const int argc, char *const argv[])
 {
-  assert(argc == 4);
+  assert(3 <= argc);
   std::string ctx_str = argv[1];
   std::string labelset_str = argv[2];
-  std::string ratexp_str = argv[3];
+  std::string ratexp_file = argv[3];
 
   // Input.
   using namespace vcsn::dyn;
   vcsn::dyn::context* ctx = make_context(ctx_str, labelset_str);
-  vcsn::dyn::ratexp exp = read_ratexp_string(ratexp_str, *ctx);
+  vcsn::dyn::ratexp exp = read_ratexp_file(ratexp_file, *ctx);
 
   // Process.
   exp = transpose (exp);
