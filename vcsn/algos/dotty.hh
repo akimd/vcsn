7676e12450b663b6d1a6cb82b0fa4a338970bb4b
Akim Demaille <akim@lrde.epita.fr>
2012-12-29 14:41:23 +0100

    dotty: sort the output to ensure idempotence
    * vcsn/algos/dotty.hh: Sort the output by state number, and then by
    destination number.
    
    * tests/unit/aut_char_z_lal.chk, tests/unit/aut_char_z_lau.chk,
    * tests/unit/aut_char_z_law.chk, tests/unit/determinize.chk,
    * tests/unit/eval.chk, tests/unit/ladybird.chk,
    * tests/unit/lift.chk, tests/unit/parse-dot.chk,
    * tests/unit/product.chk, tests/unit/standard_of.chk,
    * tests/unit/transpose.chk: Adjust.

--- vcsn/algos/dotty.hh
+++ vcsn/algos/dotty.hh
@@ -1,10 +1,13 @@
 #ifndef VCSN_ALGOS_DOTTY_HH
 # define VCSN_ALGOS_DOTTY_HH
 
+# include <algorithm>
 # include <iostream>
 # include <set>
 # include <sstream>
 # include <unordered_map>
+# include <unordered_set>
+# include <vector>
 
 # include <vcsn/core/fwd.hh>
 
@@ -22,6 +25,7 @@ namespace vcsn
   dotty(const A& aut, std::ostream& out)
   {
     using state_t = typename A::state_t;
+    using transition_t = typename A::transition_t;
     std::unordered_map<state_t, unsigned> names;
 
     out <<
@@ -39,55 +43,65 @@ namespace vcsn
     // Output the pre-initial and post-final states.
     out <<
       "  {\n"
-      "    node [style = invis, shape = none, label = \"\", width = 0, height = 0]\n";
+      "    node [style = invis, shape = none, label = \"\""
+      ", width = 0, height = 0]\n";
     for (auto t : aut.initial_transitions())
       out << "    I" << names[aut.dst_of(t)] << '\n';
-    // Output the "post" final states.
     for (auto t : aut.final_transitions())
       out << "    F" << names[aut.src_of(t)] << '\n';
     out << "  }\n";
 
-    // Output the states that are not part of an entry.
-    {
-      std::set<state_t> reachable;
-      for (auto t : aut.all_entries())
-        {
-          reachable.insert(aut.src_of(t));
-          reachable.insert(aut.dst_of(t));
-        }
-      for (auto s : aut.states())
-        if (reachable.find(s) == reachable.end())
-          out << "  " << names[s] << "\n";
-    }
+    // Output all the states to make "print | read" idempotent.
+    out << "  {";
+    for (auto s : aut.states())
+      out << ' ' << names[s];
+    out << " }\n";
 
-    for (auto t : aut.all_entries())
+    for (auto src : aut.all_states())
       {
-        auto src = aut.src_of(t);
-        auto dst = aut.dst_of(t);
-
-        if (src == aut.pre())
+        // Transitions from src, ordered by destination.
+        // Keep a single destination as we use entries below.
+        // FIXME: A heap might be nice.
+        std::unordered_set<state_t> ds;
+        std::vector<transition_t> ts;
+        for (auto t: aut.all_out(src))
+          if (ds.find(aut.dst_of(t)) == end(ds))
+            {
+              ds.insert(aut.dst_of(t));
+              ts.push_back(t);
+            }
+        std::sort(begin(ts), end(ts),
+                  [&aut](transition_t a, transition_t b)
+                  {
+                    return aut.dst_of(a) < aut.dst_of(b);
+                  });
+        for (const auto& t: ts)
           {
-            unsigned n = names[dst];
-            out << "  I" << n << " -> " << n;
+            auto dst = aut.dst_of(t);
+            if (src == aut.pre())
+              {
+                unsigned n = names[dst];
+                out << "  I" << n << " -> " << n;
+              }
+            else if (dst == aut.post())
+              {
+                unsigned n = names[src];
+                out << "  " << n << " -> F" << n;
+              }
+            else
+              {
+                unsigned ns = names[src];
+                unsigned nd = names[dst];
+                out << "  " << ns << " -> " << nd;
+              }
+            std::string s = aut.entryset().format(aut.entry_at(t));
+            if (!s.empty())
+              {
+                out << " [label = \"";
+                str_escape(out, s) << "\"]";
+              }
+            out << "\n";
           }
-        else if (dst == aut.post())
-          {
-            unsigned n = names[src];
-            out << "  " << n << " -> F" << n;
-          }
-        else
-          {
-            unsigned ns = names[src];
-            unsigned nd = names[dst];
-            out << "  " << ns << " -> " << nd;
-          }
-        std::string s = aut.entryset().format(aut.entry_at(t));
-        if (!s.empty())
-          {
-            out << " [label = \"";
-            str_escape(out, s) << "\"]";
-          }
-        out << "\n";
       }
     out << "}" << std::endl;
   }
