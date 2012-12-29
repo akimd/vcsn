99b3955c0e8ff188931570fcfed00cbf0dd1ffe3
Akim Demaille <akim@lrde.epita.fr>
2012-12-29 09:50:29 +0100

    vcsn-cat, vcsn-transpose: work on automata and expressions
    This is while expecting for a better interface to be provided by
    David's work.
    
    * bin/vcsn-cat.cc, bin/vcsn-transpose.cc: Support automaton and
    expression input.
    * tests/rat/cat.chk, tests/rat/vcsn-transpose.chk: Adjust.

--- bin/vcsn-transpose.cc
+++ bin/vcsn-transpose.cc
@@ -6,21 +6,58 @@
 
 #include <lib/vcsn/rat/driver.hh>
 
-int main(const int argc, char *const argv[])
+void
+work_aut(const std::string& file)
 {
-  assert(3 <= argc);
-  std::string ctx_str = argv[1];
-  std::string labelset_str = argv[2];
-  std::string ratexp_file = argv[3];
-
   // Input.
   using namespace vcsn::dyn;
-  vcsn::dyn::context* ctx = make_context(ctx_str, labelset_str);
-  vcsn::dyn::ratexp exp = read_ratexp_file(ratexp_file, *ctx);
+  vcsn::dyn::automaton aut = read_automaton_file(file);
+
+  // Process.
+  //
+  // Beware that the result is a wrapper around the original
+  // automaton, so aut = transpose (aut) is wrong.
+  auto res = transpose (aut);
+
+  // Output.
+  print(res, std::cout) << std::endl;
+}
+
+void
+work_exp(const std::string& ctx_name, const std::string& labelset,
+         const std::string& file)
+{
+  // Input.
+  using namespace vcsn::dyn;
+  vcsn::dyn::context* ctx = make_context(ctx_name, labelset);
+  vcsn::dyn::ratexp exp = read_ratexp_file(file, *ctx);
 
   // Process.
   exp = transpose (exp);
 
   // Output.
   print(exp, std::cout) << std::endl;
+}
+
+int main(int argc, char *const argv[])
+{
+  assert(2 < argc);
+  std::string opt = argv[1];
+  argc -= 2;
+  argv += 2;
+  if (opt == "-a")
+    {
+      assert(1 == argc);
+      work_aut(argv[0]);
+    }
+  else if (opt == "-e")
+    {
+      assert(3 == argc);
+      work_exp(argv[0], argv[1], argv[2]);
+    }
+  else
+    {
+      std::cerr << argv[0] << ": invalid option: " << opt << std::endl;
+      exit(EXIT_FAILURE);
+    }
 }
