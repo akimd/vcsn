0199b9712d735ee2c0e904e0ba5c41a3fa428dc0
Akim Demaille <akim@lrde.epita.fr>
2013-02-18 17:55:14 +0100

    empty: make it useless in unordered containers
    * vcsn/empty.hh: Add support for hash and ==.

--- vcsn/empty.hh
+++ vcsn/empty.hh
@@ -1,6 +1,8 @@
 #ifndef VCSN_EMPTY_HH
 # define VCSN_EMPTY_HH
 
+# include <functional> // std::hash
+
 namespace vcsn
 {
   /// Empty labels, for LAU.
@@ -14,6 +16,26 @@ namespace vcsn
   {
     return false;
   }
+
+  // For unordered containers.
+  inline
+  bool operator==(empty_t, empty_t)
+  {
+    return true;
+  }
+}
+
+namespace std
+{
+  // For unordered containers.
+  template <>
+  struct hash<vcsn::empty_t>
+  {
+    size_t operator()(vcsn::empty_t) const
+    {
+      return 0;
+    }
+  };
 }
 
 #endif // !VCSN_EMPTY_HH
