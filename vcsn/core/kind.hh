c412d728ab540d451ad0446a81100f158ee02623
Akim Demaille <akim@lrde.epita.fr>
2013-03-09 10:34:26 +0100

    kinds: remove now useless code
    It it still unclear if it makes sense to keep the kinds, as opposed
    to depending on the LetterSet itself.  But we can remove what is now
    useless.
    
    * vcsn/core/kind.hh: The "kind" name (lal, etc.) is provided by the
    LabelSets themselves, as it the label_t.

--- vcsn/core/kind.hh
+++ vcsn/core/kind.hh
@@ -5,53 +5,9 @@
 
 namespace vcsn
 {
-  struct labels_are_unit
-  {
-    static std::string sname()
-    {
-      return "lau";
-    }
-  };
-
-  struct labels_are_letters
-  {
-    static std::string sname()
-    {
-      return "lal";
-    }
-  };
-
-  struct labels_are_words
-  {
-    static std::string sname()
-    {
-      return "law";
-    }
-  };
-
-  template <class Kind, class LabelSet>
-  struct label_trait
-  {
-  };
-
-  template <class LabelSet>
-  struct label_trait<labels_are_unit, LabelSet>
-  {
-    using label_t = typename LabelSet::empty_t;
-  };
-
-  template <class LabelSet>
-  struct label_trait<labels_are_letters, LabelSet>
-  {
-    using label_t = typename LabelSet::letter_t;
-  };
-
-  template <class LabelSet>
-  struct label_trait<labels_are_words, LabelSet>
-  {
-    using label_t = typename LabelSet::word_t;
-  };
-
+  struct labels_are_letters  {};
+  struct labels_are_unit     {};
+  struct labels_are_words    {};
 }
 
 #endif // !VCSN_CORE_KIND_HH
