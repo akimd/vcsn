fb304db8ecaf394773a69ca6baca404fd003385a
Akim Demaille <akim@lrde.epita.fr>
2013-03-18 14:47:26 +0100

    tests: is-deterministic does not check for is-realtime
    * tests/tafkit/is-deterministic-fail.chk: New.
    * tests/tafkit/local.mk: XFAIL this test.
    * TODO.txt: Fix this failure.

--- tests/tafkit/local.mk
+++ tests/tafkit/local.mk
@@ -11,21 +11,24 @@
 #
 # The Vaucanson Group consists of people listed in the `AUTHORS' file.
 
-tafkit_TESTS =					\
-  tafkit/accessible.chk				\
-  tafkit/aut_to_exp.chk				\
-  tafkit/cat.chk				\
-  tafkit/de-bruijn.chk				\
-  tafkit/determinize.chk			\
-  tafkit/eps-removal.chk			\
-  tafkit/eval.chk				\
-  tafkit/is-deterministic.chk			\
-  tafkit/is_complete.chk			\
-  tafkit/ladybird.chk				\
-  tafkit/lift.chk				\
-  tafkit/product.chk				\
+tafkit_TESTS =                                  \
+  tafkit/accessible.chk                         \
+  tafkit/aut_to_exp.chk                         \
+  tafkit/cat.chk                                \
+  tafkit/de-bruijn.chk                          \
+  tafkit/determinize.chk                        \
+  tafkit/eps-removal.chk                        \
+  tafkit/eval.chk                               \
+  tafkit/is-deterministic.chk                   \
+  tafkit/is-deterministic-fail.chk              \
+  tafkit/is_complete.chk                        \
+  tafkit/ladybird.chk                           \
+  tafkit/lift.chk                               \
+  tafkit/product.chk                            \
   tafkit/standard_of.chk
 dist_TESTS += $(tafkit_TESTS)
+
+XFAIL_TESTS += tafkit/is-deterministic-fail.chk
 
 .PHONY: check-tafkit
 check-tafkit:
