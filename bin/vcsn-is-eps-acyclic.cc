dd2cc683404bee8d548086654583bd180eab413a
Akim Demaille <akim@lrde.epita.fr>
2013-03-19 17:44:04 +0100

    tafkit: use the is-proper structure for is-eps-acyclic
    * bin/vcsn-is-eps-acyclic.cc: here.

--- bin/vcsn-is-eps-acyclic.cc
+++ bin/vcsn-is-eps-acyclic.cc
@@ -1,29 +1,47 @@
 #include <iostream>
+#include <stdexcept>
 
 #include <vcsn/dyn/algos.hh>
+
 #include "parse-args.hh"
 
-int main(int argc, char * const argv[])
-try
-  {
-    options opts;
-    opts.is_automaton = false;
-    opts.input_format = vcsn::dyn::FileType::text;
-    parse_args(opts, argc, argv);
+static int
+work_aut(const options& opts)
+{
+  using namespace vcsn::dyn;
+  // Input.
+  auto aut = read_automaton(opts);
 
-    // Input.
-    using namespace vcsn::dyn;
-    automaton aut = read_automaton(opts);
+  // Process.
+  bool res = is_eps_acyclic(aut);
 
-    // Process.
-    bool res = is_eps_acyclic(aut);
+  // Output.
+  std::cout << (res ? "true" : "false") << std::endl;
+  return res ? 0 : 2;
+}
 
-    // Output.
-    std::cout << (res ? "true" : "false") << std::endl;
-    return res ? 0 : 2;
-  }
-catch (const std::exception& e)
-  {
-    std::cerr << e.what() << std::endl;
-    exit(EXIT_FAILURE);
-  }
+static int
+work_exp(const options&)
+{
+  throw std::runtime_error("not implemented for rational expressions");
+}
+
+int main(int argc, char* const argv[])
+{
+  options opts;
+  try
+    {
+      opts = parse_args(argc, argv);
+      return opts.is_automaton ? work_aut(opts) : work_exp(opts);
+    }
+  catch (const std::exception& e)
+    {
+      std::cerr << opts.program << ": " << e.what() << std::endl;
+      exit(EXIT_FAILURE);
+    }
+  catch (...)
+    {
+      std::cerr << opts.program << ": unknown exception caught" << std::endl;
+      exit(EXIT_FAILURE);
+    }
+}
