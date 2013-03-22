bd30fef86828587b821ef40c384c8eaaa746eed8
Akim Demaille <akim@lrde.epita.fr>
2013-03-22 10:11:06 +0100

    tafkit: use factored main for vcsn-cat
    * bin/vcsn-cat.cc: here.

--- bin/vcsn-cat.cc
+++ bin/vcsn-cat.cc
@@ -1,39 +1,34 @@
 #include <iostream>
+#include <stdexcept>
 
 #include <vcsn/dyn/algos.hh>
+
 #include "parse-args.hh"
 
-static void
-work_aut(const options& opts)
+struct cat: vcsn_function
 {
-  using namespace vcsn::dyn;
-  // Input.
-  auto aut = read_automaton(opts);
-  // Output.
-  print(opts, aut);
-}
+  virtual int work_aut(const options& opts) const override
+  {
+    using namespace vcsn::dyn;
+    // Input.
+    auto aut = read_automaton(opts);
+    // Output.
+    print(opts, aut);
+    return 0;
+  }
 
-static void
-work_exp(const options& opts)
-{
-  using namespace vcsn::dyn;
-  // Input.
-  auto exp = read_ratexp(opts);
-  // Output.
-  print(opts, exp);
-}
+  virtual int work_exp(const options& opts) const override
+  {
+    using namespace vcsn::dyn;
+    // Input.
+    auto exp = read_ratexp(opts);
+    // Output.
+    print(opts, exp);
+    return 0;
+  }
+};
 
 int main(int argc, char* const argv[])
-try
-  {
-    auto opts = parse_args(argc, argv);
-    if (opts.is_automaton)
-      work_aut(opts);
-    else
-      work_exp(opts);
-  }
-catch (const std::exception& e)
-  {
-    std::cerr << e.what() << std::endl;
-    exit(EXIT_FAILURE);
-  }
+{
+  return vcsn_main(argc, argv, cat{});
+}
