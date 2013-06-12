5cd5a247f157a5f1ae6dea08af9209a1286c044a
Akim Demaille <akim@lrde.epita.fr>
2013-06-12 11:57:50 +0200

    escape: improve to support the result of istream::get
    * vcsn/misc/escape.hh, lib/vcsn/misc/escape.cc: Add overloads for
    int as input, and std::string as output.
    Be sure to display _all_ the nasty characters, even those that are
    not isprint.

--- lib/vcsn/misc/escape.cc
+++ lib/vcsn/misc/escape.cc
@@ -1,27 +1,56 @@
-#include <iostream>
 #include <vcsn/misc/escape.hh>
+
+#include <cctype>
+#include <iomanip>
+#include <iostream>
+#include <sstream>
 
 namespace vcsn
 {
   std::ostream&
+  str_escape(std::ostream& os, const int c)
+  {
+    std::ios_base::fmtflags flags = os.flags(std::ios_base::hex);
+    char fill = os.fill('0');
+    switch (c)
+      {
+      case -1:   os << "<end-of-file>"; break;
+      case '\\': os << "\\\\";          break;
+      case '"':  os << "\\\"";          break;
+      case '\n': os << "\\n";           break;
+      default:
+        if (0 <= c && c <= 0177 && std::isprint(c))
+          os << char(c);
+        else
+          os << "\\0x" << std::setw(3) << c;
+        break;
+      }
+    os.fill(fill);
+    os.flags(flags);
+    return os;
+  }
+
+  std::string
+  str_escape(const int c)
+  {
+    std::ostringstream o;
+    str_escape(o, c);
+    return o.str();
+  }
+
+  std::ostream&
   str_escape(std::ostream& os, const std::string& str)
   {
     for (auto c: str)
-      switch (c)
-        {
-        case '\\':
-          os << "\\\\";
-          break;
-        case '"':
-          os << "\\\"";
-          break;
-        case '\n':
-          os << "\\n";
-          break;
-        default:
-          os << c;
-          break;
-        }
+      str_escape(os, c);
     return os;
+  }
+
+  std::string
+  str_escape(const std::string& s)
+  {
+    std::ostringstream o;
+    str_escape(o, s);
+    return o.str();
   }
 }
--- vcsn/misc/escape.hh
+++ vcsn/misc/escape.hh
@@ -8,6 +8,16 @@ namespace vcsn
 {
   /// Output a string, escaping special characters.
   std::ostream& str_escape(std::ostream& os, const std::string& str);
+
+  /// Likewise, but produces a string.
+  std::string str_escape(const std::string& c);
+
+  /// Output a character, escaping special characters.
+  /// -1 denotes end-of-file.
+  std::ostream& str_escape(std::ostream& os, int c);
+
+  /// Likewise, but produces a string.
+  std::string str_escape(int c);
 }
 
 #endif // !VCSN_MISC_ESCAPE_HH
