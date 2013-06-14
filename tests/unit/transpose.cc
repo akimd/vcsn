aa7d6142ad84c3c86d7d93425808b29d8e083184
Akim Demaille <akim@lrde.epita.fr>
2013-06-14 16:26:51 +0200

    tests: improve readability
    * tests/unit/transpose.cc: Don't use the same alphabet for labels
    and (rational) weights.

--- tests/unit/transpose.cc
+++ tests/unit/transpose.cc
@@ -2,7 +2,6 @@
 #include <vcsn/core/rat/ratexpset.hh>
 #include <vcsn/ctx/wordset.hh>
 #include <vcsn/ctx/lal_char_b.hh>
-#include <vcsn/ctx/lal_char_z.hh>
 #include <vcsn/core/mutable_automaton.hh>
 #include <tests/unit/test.hh>
 
@@ -11,10 +10,10 @@ static bool
 check_mutable_automaton()
 {
   bool res = true;
-  // labels_are_letters for weights.
-  auto ctx_b = vcsn::ctx::lal_char_b{{'a', 'b', 'c', 'd'}};
+  // labels_are_letters (w, x, y, z) for weights.
+  auto ctx_b = vcsn::ctx::lal_char_b{{'w', 'x', 'y', 'z'}};
   auto ks_b = ctx_b.make_ratexpset();
-  // labels_are_words for labels.
+  // labels_are_words (a, b, c, d) for labels.
   auto ctx_br =
     vcsn::ctx::context<vcsn::ctx::wordset<vcsn::set_alphabet<vcsn::char_letters>>,
                        decltype(ks_b)>
@@ -27,11 +26,11 @@ check_mutable_automaton()
   auto s1 = aut1.new_state();
   auto s2 = aut1.new_state();
 
-  aut1.set_initial(s0, ks_b.conv("abcd"));
-  aut1.add_transition(s0, s0, "cd", ks_b.conv("ab"));
-  aut1.add_transition(s0, s1, "cd", ks_b.conv("abcd"));
+  aut1.set_initial(s0, ks_b.conv("wxyz"));
+  aut1.add_transition(s0, s0, "cd", ks_b.conv("wx"));
+  aut1.add_transition(s0, s1, "cd", ks_b.conv("wxyz"));
   aut1.add_transition(s1, s2, "cd");
-  aut1.set_initial(s2, ks_b.conv("abcd"));
+  aut1.set_initial(s2, ks_b.conv("wxyz"));
   std::cout << vcsn::dot(aut1) << '\n';
 
   auto aut2 = vcsn::transpose(aut1);
@@ -45,14 +44,14 @@ check_mutable_automaton()
   // Check has_transition and get_transition.
   assert(aut1.has_transition(s0, s1, "cd"));
   // FIXME: we would really like to have equality here on ratexp.
-  ASSERT_WEIGHT(aut1, s0, s1, "cd", "a.b.c.d");
+  ASSERT_WEIGHT(aut1, s0, s1, "cd", "w.x.y.z");
   assert(aut2.has_transition(s1, s0, "dc"));
-  ASSERT_WEIGHT(aut2, s1, s0, "dc", "d.c.b.a");
+  ASSERT_WEIGHT(aut2, s1, s0, "dc", "z.y.x.w");
 
   // Now change the transposed automaton, and check the modifications
   // on the original.
-  aut2.set_transition(s1, s0, "dc", ks_b.conv("bbaa"));
-  ASSERT_WEIGHT(aut1, s0, s1, "cd", "a.a.b.b");
+  aut2.set_transition(s1, s0, "dc", ks_b.conv("yyxx"));
+  ASSERT_WEIGHT(aut1, s0, s1, "cd", "x.x.y.y");
   aut2.del_state(s2);
   aut2.set_initial(s1);
 
