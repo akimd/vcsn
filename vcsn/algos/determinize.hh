89f61d9c8ab2ca307a8b871b75c82579012c1739
Akim Demaille <akim@lrde.epita.fr>
2013-06-14 17:32:18 +0200

    determinize: turn into a functor
    * vcsn/algos/determinize.hh: here.
    (map_): Now a member instead of a local variable.  This will
    allow to retrieve the mapping from determinized states to
    original state sets.

--- vcsn/algos/determinize.hh
+++ vcsn/algos/determinize.hh
@@ -13,91 +13,113 @@
 
 namespace vcsn
 {
+  namespace detail
+  {
+    template <class Aut>
+    class determinizer
+    {
+      static_assert(Aut::context_t::is_lal,
+                    "requires labels_are_letters");
+      static_assert(std::is_same<typename Aut::weight_t, bool>::value,
+                    "requires Boolean weights");
+
+      using automaton_t = Aut;
+      using label_t = typename automaton_t::label_t;
+      using state_t = typename automaton_t::state_t;
+
+      // Set of (input) states.
+      using state_set = dynamic_bitset;
+
+      // Set of input states -> output state.
+      using map = std::unordered_map<state_set, state_t>;
+      map map_;
+
+    public:
+      void clear()
+      {
+        map_.clear();
+      }
+
+      /// Determinize \a a, and return the result.
+      automaton_t operator()(const automaton_t& a)
+      {
+        clear();
+
+        const auto& letters = *a.labelset();
+        automaton_t res{a.context()};
+
+        // successors[SOURCE-STATE][LABEL] = DEST-STATESET.
+        using successors_t = std::vector<std::unordered_map<label_t, state_set> >;
+        successors_t successors{a.num_all_states()};
+        for (auto st : a.all_states())
+          for (auto l : letters)
+            {
+              state_set& ss = successors[st][l];
+              ss.resize(a.num_all_states());
+              for (auto tr : a.out(st, l))
+                ss.set(a.dst_of(tr));
+            }
+
+        // Set of final states.
+        state_set finals;
+        finals.resize(a.num_all_states());
+        for (auto t : a.final_transitions())
+          finals.set(a.src_of(t));
+
+        // The (input) sets of states waiting to be processed.
+        using stack = std::stack<state_set>;
+        stack todo;
+
+        // Create a new output state from SS. Insert in the output
+        // automaton, in the map, and push in the stack.
+        auto push_new_state =
+          [this,&res,&todo,&finals] (const state_set& ss) -> state_t
+          {
+            state_t r = res.new_state();
+            map_[ss] = r;
+
+            if (ss.intersects(finals))
+              res.set_final(r);
+
+            todo.push(ss);
+            return r;
+          };
+
+        // The input initial states.
+        state_set next;
+        next.resize(a.num_all_states());
+        for (auto t : a.initial_transitions())
+          next.set(a.dst_of(t));
+        res.set_initial(push_new_state(next));
+
+        while (!todo.empty())
+          {
+            auto ss = std::move(todo.top());
+            todo.pop();
+            for (auto l: letters)
+              {
+                next.reset();
+                for (auto s = ss.find_first(); s != ss.npos;
+                     s = ss.find_next(s))
+                  next |= successors[s][l];
+                auto i = map_.find(next);
+                state_t n = (i == map_.end()) ? push_new_state(next) : i->second;
+
+                res.add_transition(map_[ss], n, l);
+              }
+          }
+        return res;
+      }
+    };
+  }
+
   template <class Aut>
   inline
   Aut
   determinize(const Aut& a)
   {
-    static_assert(Aut::context_t::is_lal,
-                  "requires labels_are_letters");
-    static_assert(std::is_same<typename Aut::weight_t, bool>::value,
-                  "requires Boolean weights");
-
-    using automaton_t = Aut;
-    using label_t = typename automaton_t::label_t;
-    using state_t = typename automaton_t::state_t;
-
-    const auto& letters = *a.labelset();
-    automaton_t res{a.context()};
-
-    // Set of states.
-    using state_set = dynamic_bitset;
-
-    // successors[SOURCE-STATE][LABEL] = DEST-STATESET.
-    using successors_t = std::vector<std::unordered_map<label_t, state_set> >;
-    successors_t successors{a.num_all_states()};
-    for (auto st : a.all_states())
-      for (auto l : letters)
-        {
-          state_set& ss = successors[st][l];
-          ss.resize(a.num_all_states());
-          for (auto tr : a.out(st, l))
-            ss.set(a.dst_of(tr));
-        }
-
-    // Set of final states.
-    state_set finals;
-    finals.resize(a.num_all_states());
-    for (auto t : a.final_transitions())
-      finals.set(a.src_of(t));
-
-    // The (input) sets of states waiting to be processed.
-    using stack = std::stack<state_set>;
-    stack todo;
-
-    // set of input states -> output state.
-    using map = std::unordered_map<state_set, state_t>;
-    map m;
-
-    // Create a new output state from SS. Insert in the output
-    // automaton, in the map, and push in the stack.
-    auto push_new_state =
-      [&res,&m,&a,&todo,&finals] (const state_set& ss) -> state_t
-      {
-        state_t r = res.new_state();
-        m[ss] = r;
-
-        if (ss.intersects(finals))
-          res.set_final(r);
-
-        todo.push(ss);
-        return r;
-      };
-
-    // The input initial states.
-    state_set next;
-    next.resize(a.num_all_states());
-    for (auto t : a.initial_transitions())
-      next.set(a.dst_of(t));
-    res.set_initial(push_new_state(next));
-
-    while (!todo.empty())
-      {
-        auto ss = std::move(todo.top());
-        todo.pop();
-        for (auto l: letters)
-          {
-            next.reset();
-            for (auto s = ss.find_first(); s != ss.npos;
-                 s = ss.find_next(s))
-              next |= successors[s][l];
-            auto i = m.find(next);
-            state_t n = (i == m.end()) ? push_new_state(next) : i->second;
-
-            res.add_transition(m[ss], n, l);
-          }
-      }
-    return res;
+    detail::determinizer<Aut> determinize;
+    return determinize(a);
   }
 
   /*-------------------.
