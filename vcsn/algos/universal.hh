617d16bda16b4ee04d8a77c7f91b089119ce52a4
Akim Demaille <akim@lrde.epita.fr>
2013-06-20 16:58:02 +0200

    universal: use a functor to avoid stupid bugs due to temporary variables
    Recently (see d798139ed68fd5e6cf72bdaf0bb60afef84081f7), universal was
    buggy because two different automata coexisted in universal(): the
    "raw" automaton, and the one which is complete and deterministic.
    At some point, one was used instead of the other.
    
    Instead of having both coexist in the same routine, make this a
    functor with two routines: one, private, which _requires_ a complete
    deterministic automaton, and another, public, which takes in charge
    the needed preconditions.  The core routine then deals with only one
    automaton.
    
    The memory management is also simplified.
    
    * vcsn/algos/universal.hh: Do that.

--- vcsn/algos/universal.hh
+++ vcsn/algos/universal.hh
@@ -3,7 +3,6 @@
 
 # include <map>
 
-# include <vcsn/misc/attributes.hh>
 # include <vcsn/misc/set.hh>
 # include <vcsn/ctx/lal_char_b.hh>
 # include <vcsn/core/mutable_automaton.hh>
@@ -12,112 +11,127 @@
 
 namespace vcsn
 {
-  template <typename Aut>
-  Aut
-  universal(const Aut& automaton)
+  namespace detail
   {
-    static_assert(Aut::context_t::is_lal,
-                  "requires labels_are_letters");
-    static_assert(std::is_same<typename Aut::weight_t, bool>::value,
-                  "requires Boolean weights");
+    /// Functor for universal.
+    template <typename Aut>
+    class universaler
+    {
+    public:
+      static_assert(Aut::context_t::is_lal,
+                    "requires labels_are_letters");
+      static_assert(std::is_same<typename Aut::weight_t, bool>::value,
+                    "requires Boolean weights");
 
-    using automaton_t = Aut;
+      using automaton_t = Aut;
+      using state_t = typename automaton_t::state_t;
+      using state_set_t = std::set<state_t>;
+      using pstate_t = std::set<state_set_t>;
+      using map_t = std::map<state_t, state_set_t>;
 
-    // Start from a complete, deterministic automaton.
-    const automaton_t* aut = &automaton;
-    std::unique_ptr<automaton_t> complete_deterministic;
-    if (!is_deterministic(automaton))
+      /// The universal automaton of \a automaton.
+      automaton_t operator()(const Aut& automaton)
       {
-        complete_deterministic.reset(new automaton_t{determinize(automaton)});
-        aut = complete_deterministic.get();
+        if (!is_deterministic(automaton))
+          return work_(determinize(automaton));
+        else if (!is_complete(automaton))
+          return work_(complete(automaton));
+        else
+          return work_(automaton);
       }
-    else if (!is_complete(automaton))
+
+    private:
+      /// Work on \a aut, which is complete and deterministic.
+      /// \pre is_complete(aut)
+      /// \pre is_deterministic(aut)
+      automaton_t work_(const automaton_t& aut)
       {
-        complete_deterministic.reset(new automaton_t{complete(automaton)});
-        aut = complete_deterministic.get();
-      }
+        // The initial state of automaton.
+        state_t i = aut.dst_of(aut.initial_transitions().front());
 
-    using state_t = typename automaton_t::state_t;
-    using state_set_t = std::set<state_t>;
-    using pstate_t = std::set<state_set_t>;
-    using map_t = std::map<state_t, state_set_t>;
+        // compute the co-determinized of the minimal automaton
+        // and retrieve the origin of each state.
+        // FIXME: Get rid of the const_cast.
+        // FIXME: Make transposed const.
+        auto transposed = transpose(const_cast<automaton_t&>(aut));
+        detail::determinizer<decltype(transposed)> determinize;
+        auto const co_det = determinize(transposed);
+        map_t origin = determinize.origins();
 
-    // The initial state of automaton.
-    state_t i = aut->dst_of(aut->initial_transitions().front());
+        // the 'origin' is a map from co_det's state_t to
+        // minimal's state_set_t.
+        // let 'transp_states' be the image of 'origin'.
+        pstate_t transp_states = image(origin);
 
-    // compute the co-determinized of the minimal automaton
-    // and retrieve the origin of each state.
-    // FIXME: Get rid of the const_cast.
-    // FIXME: Make transposed const.
-    auto transposed = transpose(const_cast<Aut&>(*aut));
-    detail::determinizer<decltype(transposed)> determinize;
-    auto const co_det = determinize(transposed);
-    map_t origin = determinize.origins();
+        // the universal automaton's state set is its intersection closure.
+        pstate_t univers_states(intersection_closure(transp_states));
 
-    // the 'origin' is a map from co_det's state_t to
-    // minimal's state_set_t.
-    // let 'transp_states' be the image of 'origin'.
-    pstate_t transp_states = image(origin);
+        // The universal automaton.
+        automaton_t res{aut.context()};
 
-    // the universal automaton's state set is its intersection closure.
-    pstate_t univers_states(intersection_closure(transp_states));
+        // The final states of aut.
+        std::set<state_t> automaton_finals;
+        for (auto t: aut.final_transitions())
+          automaton_finals.insert(aut.src_of(t));
 
-    // The universal automaton.
-    automaton_t res{aut->context()};
+        // we have to save the state set associated to each automaton.
+        map_t subset_label;
 
-    // The final states of aut.
-    std::set<state_t> automaton_finals;
-    for (auto t: aut->final_transitions())
-      automaton_finals.insert(aut->src_of(t));
+        // X = univers_states \ {}.
+        for (const auto s: univers_states)
+          if (!s.empty())
+            {
+              state_t new_s = res.new_state();
+              subset_label[new_s] = s;
+              // J = { X | i in X }
+              if (has(s, i))
+                res.set_initial(new_s);
+              // U = { X | X \subset T }
+              if (subset(s, automaton_finals))
+                res.set_final(new_s);
+            }
 
-    // we have to save the state set associated to each automaton.
-    map_t subset_label;
-
-    // X = univers_states \ {}.
-    for (const auto s: univers_states)
-      if (!s.empty())
-        {
-          state_t new_s = res.new_state();
-          subset_label[new_s] = s;
-          // J = { X | i in X }
-          if (has(s, i))
-            res.set_initial(new_s);
-          // U = { X | X \subset T }
-          if (subset(s, automaton_finals))
-            res.set_final(new_s);
-        }
-
-    // Finally, the transition set.
-    for (const auto x: res.states())
-      for (const auto y: res.states())
-        for (const auto a: *res.labelset())
-          {
-            bool cont = false;
-            state_set_t delta_ret;
-            for (auto s: subset_label[x])
+        // Finally, the transition set.
+        for (const auto x: res.states())
+          for (const auto y: res.states())
+            for (const auto a: *res.labelset())
               {
-                bool empty = true;
-                for (auto t: aut->out(s, a))
+                bool cont = false;
+                state_set_t delta_ret;
+                for (auto s: subset_label[x])
                   {
-                    empty = false;
-                    delta_ret.insert(aut->dst_of(t));
+                    bool empty = true;
+                    for (auto t: aut.out(s, a))
+                      {
+                        empty = false;
+                        delta_ret.insert(aut.dst_of(t));
+                      }
+                    if (empty)
+                      {
+                        cont = true;
+                        break;
+                      }
                   }
-                if (empty)
-                  {
-                    cont = true;
-                    break;
-                  }
+                // case 1: \exists p \in X, p.a = {}
+                if (cont)
+                  continue;
+                // case 2: X.a \subset Y?
+                if (subset(delta_ret, subset_label[y]))
+                  res.add_transition(x, y, a);
               }
-            // case 1: \exists p \in X, p.a = {}
-            if (cont)
-              continue;
-            // case 2: X.a \subset Y?
-            if (subset(delta_ret, subset_label[y]))
-              res.add_transition(x, y, a);
-          }
-    return res;
+        return res;
+      }
+    };
   }
 
+  template <class Aut>
+  inline
+  Aut
+  universal(const Aut& a)
+  {
+    detail::universaler<Aut> universal;
+    return universal(a);
+  }
 
   /*-----------------.
   | dyn::universal.  |
