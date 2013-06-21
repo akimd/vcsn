33367ae1b75d5a32adf76517b99225985d7f5e49
Akim Demaille <akim@lrde.epita.fr>
2013-06-21 10:11:09 +0200

    tests: check that the empty string generates the unit ratexp
    * tests/unit/concat.cc: Here.
    * tests/unit/local.mk: Adjust.

--- tests/unit/concat.cc
+++ tests/unit/concat.cc
@@ -19,11 +19,26 @@ check_concat(const Ctx& ctx)
 #undef CHECK
   return nerrs;
 }
+
+template <typename Ctx>
+static size_t
+check_conv(const Ctx& ctx)
+{
+  size_t nerrs = 0;
+  auto ks = ctx.make_ratexpset();
+  auto ls = *ctx.labelset();
+
+  // Check that the empty word is really recognized as \e.
+  ASSERT_EQ(int(ks.atom(ls.conv(""))->type()),
+            int(vcsn::rat::exp::type_t::one));
+
+  return nerrs;
 }
 
 int main()
 {
   size_t nerrs = 0;
   nerrs += check_concat(vcsn::ctx::law_char_z{{'a', 'b'}});
+  nerrs += check_conv(vcsn::ctx::law_char_z{{'a', 'b'}});
   return !!nerrs;
 }
