399a3c92293e9a9bd83a7d121241698e57b225b8
Akim Demaille <akim@lrde.epita.fr>
2013-06-25 17:44:00 +0200

    standard: be sure to compare weights appropriately
    * vcsn/algos/standard.hh: Use the WeightSet to this end.

--- vcsn/algos/standard.hh
+++ vcsn/algos/standard.hh
@@ -94,13 +94,13 @@ namespace vcsn
       {
         {
           weight_t w = e.left_weight();
-          if (w != ws_.one())
+          if (!ws_.is_one(w))
             for (auto t: res_.all_out(initial_))
               res_.lmul_weight(t, w);
         }
         {
           weight_t w = e.right_weight();
-          if (w != ws_.one())
+          if (!ws_.is_one(w))
             for (auto t: res_.final_transitions())
               if (!has(other_finals, res_.src_of(t)))
                 res_.rmul_weight(t, w);
