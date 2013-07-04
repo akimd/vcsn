5f53a32beffb6da961469736546c86217e607500
Alexandre Lewkowicz <alexandre.lewkowicz@epita.fr>
2013-07-04 14:49:43 +0200

    style: sort
    * vcsn/misc/attributes.hh: here.

--- vcsn/misc/attributes.hh
+++ vcsn/misc/attributes.hh
@@ -3,18 +3,18 @@
 
 # if defined __clang__
 
-#  define ATTRIBUTE_PURE
 #  define ATTRIBUTE_NORETURN [[noreturn]]
+#  define ATTRIBUTE_PURE
 
 # elif defined __GNUC__
 
-#  define ATTRIBUTE_PURE __attribute__((pure))
 #  define ATTRIBUTE_NORETURN __attribute__((noreturn))
+#  define ATTRIBUTE_PURE __attribute__((pure))
 
 # elif defined _MSC_VER
 
-#  define ATTRIBUTE_PURE
 #  define ATTRIBUTE_NORETURN [[noreturn]]
+#  define ATTRIBUTE_PURE
 
 # endif
 
