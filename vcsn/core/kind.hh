3d8273d8c605a89a4a201c515ca5971a4b6967ed
Akim Demaille <akim@lrde.epita.fr>
2013-07-10 10:36:06 +0200

    kinds: factor their definition.
    * vcsn/core/kind.hh (DEFINE): New.
    Use it.

--- vcsn/core/kind.hh
+++ vcsn/core/kind.hh
@@ -5,61 +5,39 @@
 
 namespace vcsn
 {
-  struct labels_are_letters  {};
-  struct labels_are_nullable {};
-  struct labels_are_one      {};
-  struct labels_are_words    {};
 
+  /// Define the kinds, and auxialary tools.
+  ///
+  /// is_ABBREV<Kinded>: Whether Kinded has a specific kind_t
+  ///
+  /// if_ABBREV<Kinded, R = Kinded>:
+  /// Provide "overloading" on parameter types, for instance to require
+  /// letter_t when labels_are_letters, and word_t when
+  /// labels_are_words.  See ratexpset::atom and ratexpset::atom_ for
+  /// an example.
+  ///
+  /// It is very tempting to turns these guys into members of
+  /// ctx::context, but then, instead of "(if_lal<Ctx, letter_t> v)",
+  /// one must write "(typename Cxx::template if_lal<letter_t> v)".
 
-  /*---------------------------------------.
-  | Whether Kinded has a specific kind_t.  |
-  `---------------------------------------*/
+# define DEFINE(Abbrev, Name)                                           \
+  struct labels_are_ ## Name  {};                                       \
+                                                                        \
+  template <typename Kinded>                                            \
+  struct is_ ## Abbrev                                                  \
+    : std::is_same<typename Kinded::kind_t, labels_are_ ## Name>::type  \
+  {};                                                                   \
+                                                                        \
+  template <typename Kinded, typename R = Kinded>                       \
+  using if_ ## Abbrev                                                   \
+    = typename std::enable_if<is_ ## Abbrev<Kinded>::value, R>::type
 
-  template <typename Kinded>
-  struct is_lal
-    : std::is_same<typename Kinded::kind_t, labels_are_letters>::type
-  {};
+  DEFINE(lal, letters);
+  DEFINE(lan, nullable);
+  DEFINE(lao, one);
+  DEFINE(law, words);
 
-  template <typename Kinded>
-  struct is_lan
-    : std::is_same<typename Kinded::kind_t, labels_are_nullable>::type
-  {};
-
-  template <typename Kinded>
-  struct is_lao
-    : std::is_same<typename Kinded::kind_t, labels_are_one>::type
-  {};
-
-  template <typename Kinded>
-  struct is_law
-    : std::is_same<typename Kinded::kind_t, labels_are_words>::type
-  {};
-
-
-  // Provide "overloading" on parameter types, for instance to require
-  // letter_t when labels_are_letters, and word_t when
-  // labels_are_words.  See ratexpset::atom and ratexpset::atom_ for
-  // an example.
-  //
-  // It is very tempting to turns these guys into members of
-  // ctx::context, but then, instead of "(if_lal<Ctx, letter_t> v)",
-  // one must write "(typename Cxx::template if_lal<letter_t> v)".
-  template <typename Kinded, typename R = Kinded>
-  using if_lal
-    = typename std::enable_if<is_lal<Kinded>::value, R>::type;
-
-  template <typename Kinded, typename R = Kinded>
-  using if_lan
-    = typename std::enable_if<is_lan<Kinded>::value, R>::type;
-
-  template <typename Kinded, typename R = Kinded>
-  using if_lao
-    = typename std::enable_if<is_lao<Kinded>::value, R>::type;
-
-  template <typename Kinded, typename R = Kinded>
-  using if_law
-    = typename std::enable_if<is_law<Kinded>::value, R>::type;
-
+# undef DEFINE
 }
 
 #endif // !VCSN_CORE_KIND_HH
