b9caf3e29753db79fffd3c30f9ac289057f3f815
Akim Demaille <akim@lrde.epita.fr>
2013-07-26 13:34:46 +0200

    automata: catch up with V1
    * share/vcsn/lal_char_b/b1.gv: New.
    * share/vcsn/lal_char_b/evena.gv: New.
    * share/vcsn/lal_char_b/oddb.gv: New.
    * share/vcsn/lal_char_z/b1.gv: New.
    * share/vcsn/lal_char_z/d1.gv: New.
    * share/vcsn/lal_char_zmin/minab.gv: New.
    * share/vcsn/lal_char_zmin/minblocka.gv: New.
    * share/vcsn/lal_char_zmin/slowgrow.gv: New.
    * NEWS.txt, share/local.mk: Adjust.

--- share/local.mk
+++ share/local.mk
@@ -11,7 +11,15 @@
 #
 # The Vaucanson Group consists of people listed in the `AUTHORS' file.
 
-dist_pkgdata_DATA =				\
-  share/vcsn/lal_char_b/a1.gv			\
-  share/vcsn/lal_char_z/binary.gv		\
-  share/vcsn/lal_char_z/c1.gv
+dist_pkgdata_DATA =                             \
+  share/vcsn/lal_char_b/a1.gv                   \
+  share/vcsn/lal_char_b/b1.gv                   \
+  share/vcsn/lal_char_b/evena.gv                \
+  share/vcsn/lal_char_b/oddb.gv                 \
+  share/vcsn/lal_char_z/b1.gv                   \
+  share/vcsn/lal_char_z/binary.gv               \
+  share/vcsn/lal_char_z/c1.gv                   \
+  share/vcsn/lal_char_z/d1.gv                   \
+  share/vcsn/lal_char_zmin/minab.gv             \
+  share/vcsn/lal_char_zmin/minblocka.gv         \
+  share/vcsn/lal_char_zmin/slowgrow.gv
