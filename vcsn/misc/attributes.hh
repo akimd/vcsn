863e511107a1c2900c03e75e6bfd197ffd1ed227
Luca Saiu <positron@gnu.org>
2013-10-10 15:38:19 +0200

    attributes: add macros for hot and always_inline

--- vcsn/misc/attributes.hh
+++ vcsn/misc/attributes.hh
@@ -4,18 +4,24 @@
 # if defined __clang__
 
 #  define ATTRIBUTE_CONST __attribute__((const))
+#  define ATTRIBUTE_ALWAYS_INLINE __attribute__((always_inline))
+#  define ATTRIBUTE_HOT __attribute__((hot))
 #  define ATTRIBUTE_NORETURN [[noreturn]]
 #  define ATTRIBUTE_PURE __attribute__((pure))
 
 # elif defined __GNUC__
 
 #  define ATTRIBUTE_CONST __attribute__((const))
+#  define ATTRIBUTE_ALWAYS_INLINE __attribute__((always_inline))
+#  define ATTRIBUTE_HOT __attribute__((hot))
 #  define ATTRIBUTE_NORETURN __attribute__((noreturn))
 #  define ATTRIBUTE_PURE __attribute__((pure))
 
 # elif defined _MSC_VER
 
 #  define ATTRIBUTE_CONST __declspec(const)
+#  define ATTRIBUTE_ALWAYS_INLINE
+#  define ATTRIBUTE_HOT
 #  define ATTRIBUTE_NORETURN [[noreturn]]
 #  define ATTRIBUTE_PURE
 
