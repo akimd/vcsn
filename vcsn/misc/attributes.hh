f71c0f3d032454085e1598358846768db6d3b65f
Akim Demaille <akim@lrde.epita.fr>
2013-10-15 09:07:28 +0200

    attributes: visibility
    * vcsn/misc/attributes.hh: Sort.
    Improve layout.
    (ATTRIBUTE_DLLEXPORT, ATTRIBUTE_DLLIMPORT): New.

--- vcsn/misc/attributes.hh
+++ vcsn/misc/attributes.hh
@@ -3,26 +3,32 @@
 
 # if defined __clang__
 
-#  define ATTRIBUTE_CONST __attribute__((const))
 #  define ATTRIBUTE_ALWAYS_INLINE __attribute__((always_inline))
-#  define ATTRIBUTE_HOT __attribute__((hot))
-#  define ATTRIBUTE_NORETURN [[noreturn]]
-#  define ATTRIBUTE_PURE __attribute__((pure))
+#  define ATTRIBUTE_CONST         __attribute__((const))
+#  define ATTRIBUTE_DLLEXPORT
+#  define ATTRIBUTE_DLLIMPORT
+#  define ATTRIBUTE_HOT           __attribute__((hot))
+#  define ATTRIBUTE_NORETURN      [[noreturn]]
+#  define ATTRIBUTE_PURE          __attribute__((pure))
 
 # elif defined __GNUC__
 
-#  define ATTRIBUTE_CONST __attribute__((const))
 #  define ATTRIBUTE_ALWAYS_INLINE __attribute__((always_inline))
-#  define ATTRIBUTE_HOT __attribute__((hot))
-#  define ATTRIBUTE_NORETURN __attribute__((noreturn))
-#  define ATTRIBUTE_PURE __attribute__((pure))
+#  define ATTRIBUTE_CONST         __attribute__((const))
+#  define ATTRIBUTE_DLLEXPORT     __attribute__((visibility("default")))
+#  define ATTRIBUTE_DLLIMPORT     ATTRIBUTE_DLLEXPORT
+#  define ATTRIBUTE_HOT           __attribute__((hot))
+#  define ATTRIBUTE_NORETURN      __attribute__((noreturn))
+#  define ATTRIBUTE_PURE          __attribute__((pure))
 
 # elif defined _MSC_VER
 
-#  define ATTRIBUTE_CONST __declspec(const)
 #  define ATTRIBUTE_ALWAYS_INLINE
+#  define ATTRIBUTE_CONST         __declspec(const)
+#  define ATTRIBUTE_DLLEXPORT     __declspec(dllexport)
+#  define ATTRIBUTE_DLLIMPORT     __declspec(dllimport)
 #  define ATTRIBUTE_HOT
-#  define ATTRIBUTE_NORETURN [[noreturn]]
+#  define ATTRIBUTE_NORETURN      [[noreturn]]
 #  define ATTRIBUTE_PURE
 
 # endif
