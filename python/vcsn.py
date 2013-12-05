1bf37bb5ef35e0a42a44f3d78b7d10357cae8f14
Akim Demaille <akim@lrde.epita.fr>
2013-12-05 14:52:03 +0100

    python: style changes and __eq__ operators
    * python/vcsn.py: here.
    However, unused and untested so far.

--- python/vcsn.py
+++ python/vcsn.py
@@ -6,6 +6,19 @@ def dot_to_svg(dot):
     check_call(['dot','-Tsvg','/tmp/a.gv','-o','/tmp/a.svg'])
     return open("/tmp/a.svg", "r").read()
 
+## --------- ##
+## context.  ##
+## --------- ##
+
+def make_ratexp(ctx, re):
+    return ratexp(ctx, re)
+context.ratexp = make_ratexp
+
+
+## ----------- ##
+## automaton.  ##
+## ----------- ##
+
 def automaton_to_svg(aut):
     return dot_to_svg(str(aut))
 
@@ -13,6 +26,25 @@ automaton._repr_svg_ = automaton_to_svg
 
 automaton.__mul__ = lambda lhs, rhs: lhs.product(rhs).trim()
 
-def make_ratexp(ctx, re):
-    return ratexp(ctx, re)
-context.ratexp = make_ratexp
+
+## -------- ##
+## ratexp.  ##
+## -------- ##
+
+def vcsn_ratexp__eq__(lhs, rhs):
+    print isinstance(rhs, lhs.__class__)
+    print str(lhs) == str(rhs)
+    return isinstance(rhs, lhs.__class__) and str(lhs) == str(rhs)
+
+ratexp.__eq__ = vcsn_ratexp__eq__
+
+
+## ------------ ##
+## polynomial.  ##
+## ------------ ##
+def vcsn_polynomial__eq__(lhs, rhs):
+    print isinstance(rhs, lhs.__class__)
+    print str(lhs) == str(rhs)
+    return isinstance(rhs, lhs.__class__) and str(lhs) == str(rhs)
+
+polynomial.__eq__ = vcsn_polynomial__eq__
