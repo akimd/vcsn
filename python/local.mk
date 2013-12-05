fd2b65620b27e18dfca21fe675606ec14357f420
Akim Demaille <akim@lrde.epita.fr>
2013-12-05 16:29:15 +0100

    python: fix distcheck
    * configure.ac, Makefile.am, python/local.mk: Properly set up the Autotools
    for Python.

--- python/local.mk
+++ python/local.mk
@@ -1,4 +1,6 @@
-pkglib_LTLIBRARIES += %D%/vcsn_python.la
+python_PYTHON = %D%/vcsn.py
+
+pyexec_LTLIBRARIES = %D%/vcsn_python.la
 %C%_vcsn_python_la_CPPFLAGS = $(AM_CPPFLAGS) $(BOOST_PYTHON_CPPFLAGS)
-%C%_vcsn_python_la_LDFLAGS = -module $(BOOST_PYTHON_LDFLAGS)
+%C%_vcsn_python_la_LDFLAGS = -avoid-version -module $(BOOST_PYTHON_LDFLAGS)
 %C%_vcsn_python_la_LIBADD = $(BOOST_PYTHON_LIBS) $(all_libctx) lib/libvcsn.la
