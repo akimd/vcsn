859fdd57e1fcb21993354eb33df2dd71e2b2d7ed
Akim Demaille <akim@lrde.epita.fr>
2013-12-19 09:40:28 +0100

    build: fix install-time dependency
    * python/local.mk: Be sure to first complete the installation of the
    libraries we depend.

--- python/local.mk
+++ python/local.mk
@@ -4,3 +4,9 @@ pyexec_LTLIBRARIES = %D%/vcsn_python.la
 %C%_vcsn_python_la_CPPFLAGS = $(AM_CPPFLAGS) $(BOOST_PYTHON_CPPFLAGS)
 %C%_vcsn_python_la_LDFLAGS = -avoid-version -module $(BOOST_PYTHON_LDFLAGS)
 %C%_vcsn_python_la_LIBADD = $(BOOST_PYTHON_LIBS) $(all_libctx) lib/libvcsn.la
+
+# We must wait for the libraries againt which this one is linked, to
+# be installed.  Then we can install this one.  Otherwise libtool will
+# complain that this Python library has dependencies that are not
+# installed.
+install-pyexecLTLIBRARIES: install-pkglibLTLIBRARIES install-libLTLIBRARIES
