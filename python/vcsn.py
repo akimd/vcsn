c3d455a4fe26ddc29f6deb63ae14606046be7d02
Akim Demaille <akim@lrde.epita.fr>
2013-12-31 15:53:54 +0100

    python: provide dummy, but effective, versions of __eq__
    This reverts commit afeacea6470f320b41760d7937508257cfd3f342.
    
    These functions needed.  However, of course, they should be
    implemented more seriously, using is_equal, but there is no such thing
    in dyn::.

--- python/vcsn.py
+++ python/vcsn.py
@@ -5,21 +5,32 @@ def dot_to_svg(dot):
     from subprocess import check_output
     return check_output(['dot','-Tsvg','/tmp/a.gv'])
 
+def is_equal(lhs, rhs):
+    return isinstance(rhs, lhs.__class__) and str(lhs) == str(rhs)
+
 ## --------- ##
 ## context.  ##
 ## --------- ##
 
+context.__eq__ = is_equal
+context._repr_latex_ = lambda c: '$' + c.format('latex') + '$'
+
+def make_polynomial(ctx, p):
+    return polynomial(ctx, p)
+context.polynomial = make_polynomial
+
 def make_ratexp(ctx, re):
     return ratexp(ctx, re)
 context.ratexp = make_ratexp
-context._repr_latex_ = lambda c: '$' + c.format('latex') + '$'
+
 
 ## ----------- ##
 ## automaton.  ##
 ## ----------- ##
 
-automaton._repr_svg_ = lambda a: dot_to_svg(str(a))
+automaton.__eq__ = is_equal
 automaton.__mul__ = lambda lhs, rhs: lhs.product(rhs).trim()
+automaton._repr_svg_ = lambda a: dot_to_svg(str(a))
 
 def automaton_fst(aut, cmd):
     open("/tmp/in.efsm", "w").write(aut.format("efsm"))
@@ -31,3 +42,15 @@ def automaton_fst(aut, cmd):
 
 automaton.fstdeterminize = lambda aut: automaton_fst(aut, "fstdeterminize")
 automaton.fstminimize = lambda aut: automaton_fst(aut, "fstminimize")
+
+## ------------ ##
+## polynomial.  ##
+## ------------ ##
+
+polynomial.__eq__ = is_equal
+
+## -------- ##
+## ratexp.  ##
+## -------- ##
+
+ratexp.__eq__ = is_equal
