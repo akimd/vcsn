1c30d072d84c9d94ab978cf6a3d484a5d754344a
Akim Demaille <akim@lrde.epita.fr>
2013-12-31 09:40:18 +0100

    tests: derivation: more ratexp operators
    * tests/python/derivation.py (check): Improve to provide location of
    errors.
    Check the complement operator.

--- tests/python/derivation.py
+++ tests/python/derivation.py
@@ -1,21 +1,27 @@
 #! /usr/bin/env python
 
+import inspect
 import vcsn
 
 count = 0
 c = vcsn.context("lal_char(abc)_ratexpset<lal_char(xyz)_z>")
 
-def check(re, letter, res):
+def check(re, letter, exp):
     global count
     count += 1
+    finfo = inspect.getframeinfo(inspect.currentframe().f_back)
+    here = finfo.filename + ":" + str(finfo.lineno)
     if re[:3] <> "(?@":
         re = "(?@lal_char(abc)_ratexpset<lal_char(xyz)_z>)" + re
     r = c.ratexp(re)
-    print r, "/d",letter, " = ", r.derivation(letter)
-    if str(r.derivation(letter)) == res:
-        print 'ok ', count;
+    eff = str(r.derivation(letter))
+    print r, "/d",letter, " = ", eff
+    if eff == exp:
+        print 'ok ', count
     else:
-        print 'not ok ', count, r.derivation(letter), "!=", res;
+        msg = exp + " != " + eff
+        print here + ":", msg
+        print 'not ok ', count, msg
 
 ## ---------------------------- ##
 ## Derive wrt a single letter.  ##
@@ -68,6 +74,32 @@ check('(<x>a)*', 'a', '<x>(<x>a)*')
 check('(<x>a)*', 'b', '\z')
 check('<x>a*',   'a', '<x>a*')
 check('<x>(<y>a)*', 'a', '<x.y>(<y>a)*')
+
+# Complement.
+check('\z{c}', 'a', '\z{c}')
+check('\e{c}', 'a', '\z{c}')
+
+check('a{c}', 'a', '\e{c}')
+check('a{c}', 'b', '\z{c}')
+
+check('(a+b){c}', 'a', '\e{c}')
+check('(a+b){c}', 'c', '\z{c}')
+
+check('(a.b){c}', 'a', 'b{c}')
+check('(a.b){c}', 'b', '\z{c}')
+
+check('(a:b){c}', 'a', '(\e:b){c}')
+check('(a:b){c}', 'b', '(a:\e){c}')
+check('(a:b){c}', 'c', '\z{c}')
+
+check('(a*&a*){c}', 'a', '(a*&a*){c}')
+check('(a*&a*){c}', 'b', '\z{c}')
+
+check('(<x>(<y>a)*<z>){c}', 'a', '((<y>a)*){c}')
+check('(<x>(<y>a)*<z>){c}', 'b', '\z{c}')
+
+check('a{c}{c}', 'a', '\e{c}{c}')
+check('a{c}{c}', 'b', '\z{c}{c}')
 
 
 ## ------------------------------- ##
