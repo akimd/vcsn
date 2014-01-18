0edee497d1585ec7a35ef7d9e1b1b43ca4864bd9
Akim Demaille <akim@lrde.epita.fr>
2014-01-18 12:44:33 +0100

    style: simplify the definition of less-than for ratexp
    * vcsn/core/rat/less-than.hh: Avoid cascades of typedefs.
    Rename the binary methods to avoid warnings from clang++ that complains
    about overloads on inherited virtual functions.

--- vcsn/core/rat/less-than.hh
+++ vcsn/core/rat/less-than.hh
@@ -30,20 +30,8 @@ namespace vcsn
       using unary_t = typename super_type::template unary_t<Type>;
       template <rat::exp::type_t Type>
       using nary_t = typename super_type::template nary_t<Type>;
-      using prod_t = typename super_type::prod_t;
-      using shuffle_t = typename super_type::shuffle_t;
-      using intersection_t = typename super_type::intersection_t;
-      using sum_t = typename super_type::sum_t;
       template <rat::exp::type_t Type>
       using weight_node_t = typename super_type::template weight_node_t<Type>;
-      using leaf_t = typename super_type::leaf_t;
-      using star_t = typename super_type::star_t;
-      using complement_t = typename super_type::complement_t;
-      using zero_t = typename super_type::zero_t;
-      using one_t = typename super_type::one_t;
-      using atom_t = typename super_type::atom_t;
-      using lweight_t = typename super_type::lweight_t;
-      using rweight_t = typename super_type::rweight_t;
 
       /// Whether \a lhs < \a rhs.
       bool
@@ -69,28 +57,52 @@ namespace vcsn
           }
       }
 
+      /*-----------------------------------------------------------.
+      | Unary visit functions than bounces to their binary peers.  |
+      `-----------------------------------------------------------*/
+
+#define VISIT(Type)                                                     \
+      using Type ## _t = typename super_type::Type ## _t;               \
+      virtual void                                                      \
+      visit(const Type ## _t& lhs)                                      \
+      {                                                                 \
+        res_ = less_than_(lhs, *down_pointer_cast<const Type ## _t>(rhs_)); \
+      }
+
+      VISIT(atom);
+      VISIT(complement);
+      VISIT(intersection);
+      VISIT(lweight);
+      VISIT(one);
+      VISIT(prod);
+      VISIT(rweight);
+      VISIT(shuffle);
+      VISIT(star);
+      VISIT(sum);
+      VISIT(zero);
+#undef VISIT
 
       /*-------------------------------------------------------.
       | Binary functions that compare two nodes of same type.  |
       `-------------------------------------------------------*/
 
-      bool visit(const zero_t&, const zero_t&)
+      bool less_than_(const zero_t&, const zero_t&)
       {
         return false;
       }
 
-      bool visit(const one_t&, const one_t&)
+      bool less_than_(const one_t&, const one_t&)
       {
         return false;
       }
 
-      bool visit(const atom_t& lhs, const atom_t& rhs)
+      bool less_than_(const atom_t& lhs, const atom_t& rhs)
       {
         return labelset_t::less_than(lhs.value(), rhs.value());
       }
 
       template <rat::exp::type_t Type>
-      bool visit(const nary_t<Type>& lhs, const nary_t<Type>& rhs)
+      bool less_than_(const nary_t<Type>& lhs, const nary_t<Type>& rhs)
       {
         auto ls = lhs.size();
         auto rs = rhs.size();
@@ -108,13 +120,13 @@ namespace vcsn
       }
 
       template <rat::exp::type_t Type>
-      bool visit(const unary_t<Type>& lhs, const unary_t<Type>& rhs)
+      bool less_than_(const unary_t<Type>& lhs, const unary_t<Type>& rhs)
       {
         return ratexpset_t::less_than(lhs.sub(), rhs.sub());
       }
 
       template <rat::exp::type_t Type>
-      bool visit(const weight_node_t<Type>& lhs, const weight_node_t<Type>& rhs)
+      bool less_than_(const weight_node_t<Type>& lhs, const weight_node_t<Type>& rhs)
       {
         // Lexicographic comparison on sub-expression, and then weight.
         if (ratexpset_t::less_than(lhs.sub(), rhs.sub()))
@@ -124,30 +136,6 @@ namespace vcsn
         else
           return weightset_t::less_than(lhs.weight(), rhs.weight());
       }
-
-      /*-----------------------------------------------------------.
-      | Unary visit functions than bounces to their binary peers.  |
-      `-----------------------------------------------------------*/
-
-#define VISIT(Type)                                                     \
-      virtual void                                                      \
-      visit(const Type ## _t& lhs)                                      \
-      {                                                                 \
-        res_ = visit(lhs, *down_pointer_cast<const Type ## _t>(rhs_));  \
-      }
-
-      VISIT(zero);
-      VISIT(one);
-      VISIT(atom);
-      VISIT(prod);
-      VISIT(sum);
-      VISIT(shuffle);
-      VISIT(intersection);
-      VISIT(star);
-      VISIT(lweight);
-      VISIT(rweight);
-      VISIT(complement);
-#undef VISIT
 
    private:
       ratexp_t rhs_;
