4e1f564c761deb5e4a725b0ad956653b9ea21e0a
Akim Demaille <akim@lrde.epita.fr>
2014-03-21 09:46:36 +0100

    style: use Automake style for copyright header
    To avoid having multiple copies in the generated Makefile.in.  Keep
    one single-dash copyright notice in Makefile.am though, to have a
    single one generated.

--- tests/python/local.mk
+++ tests/python/local.mk
@@ -1,15 +1,15 @@
-# Vaucanson 2, a generic library for finite state machines.
-# Copyright (C) 2013, 2014 Vaucanson Group.
-#
-# This program is free software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License
-# as published by the Free Software Foundation; either version 2
-# of the License, or (at your option) any later version.
-#
-# The complete GNU General Public Licence Notice can be found as the
-# `COPYING' file in the root directory.
-#
-# The Vaucanson Group consists of people listed in the `AUTHORS' file.
+## Vaucanson 2, a generic library for finite state machines.
+## Copyright (C) 2013, 2014 Vaucanson Group.
+##
+## This program is free software; you can redistribute it and/or
+## modify it under the terms of the GNU General Public License
+## as published by the Free Software Foundation; either version 2
+## of the License, or (at your option) any later version.
+##
+## The complete GNU General Public Licence Notice can be found as the
+## `COPYING' file in the root directory.
+##
+## The Vaucanson Group consists of people listed in the `AUTHORS' file.
 
 TEST_EXTENSIONS += .py
 PY_LOG_DRIVER = $(TAP_DRIVER)
