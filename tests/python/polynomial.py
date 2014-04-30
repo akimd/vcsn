8941cc58056a1e30ba09e46c7ebd54ac3e47d7d1
Akim Demaille <akim@lrde.epita.fr>
2014-04-30 10:57:39 +0200

    tests: exercize order on tuple of nullable labels
    * tests/python/polynomial.py: here.

--- tests/python/polynomial.py
+++ tests/python/polynomial.py
@@ -31,5 +31,10 @@ check('law_char(ab)_z', r'ba+ab+bb+aa+a+b+\e+bb+aa',
 check('lal_char(abc)_ratexpset<lal_char(xyz)_z>',
       r'a + a + <x>b + <y>b', r'<\e+\e>a + <x+y>b')
 
+# Be sure to have proper ordering on tuples with LAN.
+check('lat<lan_char(abc), lan_char(xyz)>_z',
+      r'(a,\e) + (\e,x) + (a,\e) + (a, x) + (\e,y)',
+      r'(\e, x) + (\e, y) + <2>(a, \e) + (a, x)')
+
 # Check that we don't ignore trailing characters.
 XFAIL(lambda: vcsn.context('lal_char(ab)_z').polynomial('<123>a*'))
