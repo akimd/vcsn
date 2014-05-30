ab2f8034fff6d0e49f13ceeaae6c785ca7ce90e1
Luca Saiu <positron@gnu.org>
2014-05-30 11:43:38 +0200

    edit (static, dyn, Python); add the alternative Python API preferred by Jacques
    Implement an automaton edit functionality (static, dyn, Python), in a
    dirtyish, but actually quite simple and minimal way: only one function
    taking many parameters, the first being an opcode and the others not
    being all used at the same time.
    
    * vcsn/algos/edit.hh: New.
    * lib/vcsn/algos/edit.cc: New.
    * vcsn/ctx/instantiate.hh: Here.
    
    * python/vcsn_cxx.cc
    
    * lib/vcsn/local.mk: Here.
    * vcsn/dyn/algos.hh: Here.
    * vcsn/local.mk: Here.

--- vcsn/ctx/instantiate.hh
+++ vcsn/ctx/instantiate.hh
@@ -12,6 +12,7 @@
 # include <vcsn/algos/derivation.hh>
 # include <vcsn/algos/derived-term.hh>
 # include <vcsn/algos/dot.hh>
+# include <vcsn/algos/edit.hh>
 # include <vcsn/algos/edit-automaton.hh>
 # include <vcsn/algos/efsm.hh>
 # include <vcsn/algos/enumerate.hh>
@@ -316,6 +317,7 @@ namespace vcsn
         REGISTER(context_of_ratexp, rs_t);
         REGISTER(copy, aut_t);
         REGISTER(dot, aut_t, std::ostream, bool);
+        REGISTER(edit, aut_t, int, int, int, const std::string, const std::string);
         REGISTER(efsm, aut_t, std::ostream);
         REGISTER(expand, rs_t);
         REGISTER(first_order, rs_t);
