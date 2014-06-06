e0a18af20d635e9c4e2568ccebddb02780f30fc2
Akim Demaille <akim@lrde.epita.fr>
2014-06-06 17:05:50 +0200

    tests: improve faithfulness of eliminate-state tests
    * tests/python/eliminate-state.dir/lao-elim-0.gv,
    * tests/python/eliminate-state.dir/lao-elim-1.gv,
    * tests/python/eliminate-state.py: Check the unaltered output.

--- tests/python/eliminate-state.py
+++ tests/python/eliminate-state.py
@@ -6,5 +6,5 @@ from test import *
 # Eliminate state 0, or 1, or 2.
 a = vcsn.automaton.load(medir + '/lao.gv')
 for i in [0, 1, 2]:
-    CHECK_EQ(vcsn.automaton.load(medir + '/lao-elim-{}.gv'.format(i)).sort(),
-             a.eliminate_state(i).sort())
+    CHECK_EQ(open(medir + '/lao-elim-{}.gv'.format(i)).read().strip(),
+             str(a.eliminate_state(i)))
