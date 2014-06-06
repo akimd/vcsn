fc923db7faad0b85afa3e575cb4c011ebc0f99d4
Akim Demaille <akim@lrde.epita.fr>
2014-06-06 16:38:40 +0200

    style: factor the definition of traits
    * vcsn/ctx/traits.hh: here.

--- vcsn/ctx/traits.hh
+++ vcsn/ctx/traits.hh
@@ -11,121 +11,43 @@ namespace vcsn
   using base_t
     = typename std::remove_cv<typename std::remove_reference<T>::type>::type;
 
-  /*---------------.
-  | context_t_of.  |
-  `---------------*/
-  namespace detail
-  {
-    template <typename ValueSet>
-    struct context_t_of_impl
-    {
-      using type = typename ValueSet::context_t;
-    };
-  }
+  /*------------------.
+  | Computing types.  |
+  `------------------*/
 
-  template <typename ValueSet>
-  using context_t_of = typename detail::context_t_of_impl<base_t<ValueSet>>::type;
+# define DEFINE(Type)                                                   \
+  namespace detail                                                      \
+  {                                                                     \
+    template <typename ValueSet>                                        \
+    struct Type ## _of_impl                                             \
+    {                                                                   \
+      using type = typename ValueSet::Type;                             \
+    };                                                                  \
+  }                                                                     \
+                                                                        \
+  template <typename ValueSet>                                          \
+  using Type ## _of                                                     \
+    = typename detail::Type ## _of_impl<base_t<ValueSet>>::type
 
+  DEFINE(context_t);
+  DEFINE(label_t);
+  DEFINE(labelset_t);
+  DEFINE(state_t);
+  DEFINE(transition_t);
+  DEFINE(weight_t);
+  DEFINE(weightset_t);
 
-  /*-------------.
-  | label_t_of.  |
-  `-------------*/
-  namespace detail
-  {
-    template <typename ValueSet>
-    struct label_t_of_impl
-    {
-      using type = typename ValueSet::label_t;
-    };
-  }
-
-  template <typename ValueSet>
-  using label_t_of = typename detail::label_t_of_impl<base_t<ValueSet>>::type;
-
-
-  /*----------------.
-  | labelset_t_of.  |
-  `----------------*/
-  namespace detail
-  {
-    template <typename ValueSet>
-    struct labelset_t_of_impl
-    {
-      using type = typename ValueSet::labelset_t;
-    };
-  }
-
-  template <typename ValueSet>
-  using labelset_t_of = typename detail::labelset_t_of_impl<base_t<ValueSet>>::type;
-
-
-  /*-------------.
-  | state_t_of.  |
-  `-------------*/
-  namespace detail
-  {
-    template <typename ValueSet>
-    struct state_t_of_impl
-    {
-      using type = typename ValueSet::state_t;
-    };
-  }
-
-  template <typename ValueSet>
-  using state_t_of = typename detail::state_t_of_impl<base_t<ValueSet>>::type;
+# undef DEFINE
 
 
   /*------------------.
-  | transition_t_of.  |
+  | shared pointers.  |
   `------------------*/
+
+  // Forward traits about shared_ptr<T> to T.
   namespace detail
   {
-    template <typename ValueSet>
-    struct transition_t_of_impl
-    {
-      using type = typename ValueSet::transition_t;
-    };
-  }
-
-  template <typename ValueSet>
-  using transition_t_of = typename detail::transition_t_of_impl<base_t<ValueSet>>::type;
-
-
-  /*--------------.
-  | weight_t_of.  |
-  `--------------*/
-  namespace detail
-  {
-    template <typename ValueSet>
-    struct weight_t_of_impl
-    {
-      using type = typename ValueSet::weight_t;
-    };
-  }
-
-  template <typename ValueSet>
-  using weight_t_of = typename detail::weight_t_of_impl<base_t<ValueSet>>::type;
-
-
-  /*-----------------.
-  | weightset_t_of.  |
-  `-----------------*/
-  namespace detail
-  {
-    template <typename ValueSet>
-    struct weightset_t_of_impl
-    {
-      using type = typename ValueSet::weightset_t;
-    };
-  }
-
-  template <typename ValueSet>
-  using weightset_t_of = typename detail::weightset_t_of_impl<base_t<ValueSet>>::type;
-
-
-  namespace detail
-  {
-#define DEFINE(Traits)                                          \
+# define DEFINE(Traits)                                         \
     template <typename ValueSet>                                \
     struct Traits ## _t_of_impl<std::shared_ptr<ValueSet>>      \
       : Traits ## _t_of_impl<base_t<ValueSet>>                  \
@@ -138,7 +60,7 @@ namespace vcsn
     DEFINE(transition);
     DEFINE(weight);
     DEFINE(weightset);
-#undef DEFINE
+# undef DEFINE
   }
 }
 
