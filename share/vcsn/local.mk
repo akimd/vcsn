a8b51a8bdc9979ffc1db84052ed16628c107dc9a
Akim Demaille <akim@lrde.epita.fr>
2014-06-12 11:36:42 +0200

    notebooks: install them and start some developer doc
    * share/vcsn/notebooks/Some_developer_documentation.ipynb: New.
    * share/vcsn/notebooks/local.mk: New.

---
+++ share/vcsn/local.mk
@@ -0,0 +1,34 @@
+## Vaucanson 2, a generic library for finite state machines.
+## Copyright (C) 2013-2014 Vaucanson Group.
+##
+## This program is free software; you can redistribute it and/or
+## modify it under the terms of the GNU General Public License
+## as published by the Free Software Foundation; either version 2
+## of the License, or (at your option) any later version.
+##
+## The complete GNU General Public Licence Notice can be found as the
+## `COPYING' file in the root directory.
+##
+## The Vaucanson Group consists of people listed in the `AUTHORS' file.
+
+include %D%/notebooks/local.mk
+
+lal_char_bdir = $(datadir)/vcsn/lal_char_b
+dist_lal_char_b_DATA =				\
+  %D%/lal_char_b/a1.gv				\
+  %D%/lal_char_b/b1.gv				\
+  %D%/lal_char_b/evena.gv			\
+  %D%/lal_char_b/oddb.gv
+
+lal_char_zdir = $(datadir)/vcsn/lal_char_z
+dist_lal_char_z_DATA =				\
+  %D%/lal_char_z/b1.gv				\
+  %D%/lal_char_z/binary.gv			\
+  %D%/lal_char_z/c1.gv				\
+  %D%/lal_char_z/d1.gv
+
+lal_char_zmindir = $(datadir)/vcsn/lal_char_zmin
+dist_lal_char_zmin_DATA =			\
+  %D%/lal_char_zmin/minab.gv			\
+  %D%/lal_char_zmin/minblocka.gv		\
+  %D%/lal_char_zmin/slowgrow.gv
--- share/vcsn/notebooks/local.mk
+++ share/vcsn/notebooks/local.mk
@@ -1,5 +1,5 @@
 ## Vaucanson 2, a generic library for finite state machines.
-## Copyright (C) 2013-2014 Vaucanson Group.
+## Copyright (C) 2014 Vaucanson Group.
 ##
 ## This program is free software; you can redistribute it and/or
 ## modify it under the terms of the GNU General Public License
@@ -11,4 +11,10 @@
 ##
 ## The Vaucanson Group consists of people listed in the `AUTHORS' file.
 
-include %D%/vcsn/local.mk
+notebooksdir = $(datadir)/vcsn/notebooks
+# Unfortunately, we can't deal with space in file names in Automake.
+dist_notebooks_DATA =                           \
+  %D%/00_-_Welcome_to_Vaucanson.ipynb           \
+  %D%/01_-_Playing_with_contexts.ipynb          \
+  %D%/02_-_Basic_operations_on_automata.ipynb   \
+  %D%/Some_developer_documentation.ipynb
