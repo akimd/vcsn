fdbbfc5cf0ab35acf6a4dce4ecdffe6d106dea05
Akim Demaille <akim@lrde.epita.fr>
2014-07-21 11:56:06 +0200

    notebooks: do use space in file names
    The limitation is not acceptable.  It particular, it forbids
    automatically generating lists of notebooks by globbing.  FWIW,
    IPython itself uses notebooks with spaces in their names.
    
    $ ls ipython/examples/Notebook
    Animations Using clear_output.ipynb	Raw Input.ipynb
    Basic Output.ipynb			Running Code.ipynb
    Connecting with the Qt Console.ipynb	SymPy.ipynb
    Custom Display Logic.ipynb		Trapezoid Rule.ipynb
    Display System.ipynb			Typesetting Math Using MathJax.ipynb
    Importing Notebooks.ipynb		User Interface.ipynb
    Index.ipynb				images
    Markdown Cells.ipynb			nbpackage
    Plotting with Matplotlib.ipynb
    
    * share/vcsn/notebooks/00_-_Welcome_to_Vaucanson.ipynb: Rename as...
    * share/vcsn/notebooks/00 - Welcome to Vaucanson.ipynb: this.
    * share/vcsn/notebooks/01_-_Playing_with_contexts.ipynb: Rename as...
    * share/vcsn/notebooks/01 - Playing with contexts.ipynb: this.
    * share/vcsn/notebooks/02_-_Basic_operations_on_automata.ipynb: Rename as...
    * share/vcsn/notebooks/02 - Basic operations on automata.ipynb: this.
    * share/vcsn/notebooks/Some_developer_documentation.ipynb: Rename as...
    * share/vcsn/notebooks/Some developer documentation.ipynb: this.
    * share/vcsn/notebooks/local.mk: Adjust.

--- share/vcsn/notebooks/local.mk
+++ share/vcsn/notebooks/local.mk
@@ -12,9 +12,53 @@
 ## The Vaucanson Group consists of people listed in the `AUTHORS' file.
 
 notebooksdir = $(datadir)/vcsn/notebooks
-# Unfortunately, we can't deal with space in file names in Automake.
-dist_notebooks_DATA =                           \
-  %D%/00_-_Welcome_to_Vaucanson.ipynb           \
-  %D%/01_-_Playing_with_contexts.ipynb          \
-  %D%/02_-_Basic_operations_on_automata.ipynb   \
-  %D%/Some_developer_documentation.ipynb
+
+# Unfortunately, we can't deal with space in file names in Automake,
+# and it is really unacceptable to forbid them.  So work around
+# Automake.
+dist_notebooks_data =				\
+  '%D%/00 - Welcome to Vaucanson.ipynb'		\
+  '%D%/01 - Playing with contexts.ipynb'	\
+  '%D%/02 - Basic operations on automata.ipynb'	\
+  '%D%/Some developer documentation.ipynb'
+
+dist-hook: dist-hook-%C%
+dist-hook-%C%:
+	@$(MKDIR_P) $(distdir)/%D%/algos
+	@for file in $(dist_notebooks_data);			\
+	do							\
+	  test -f "$(distdir)/$$file"				\
+	  || cp -p "$(srcdir)/$$file" "$(distdir)/$$file"	\
+	  || exit 1;						\
+	done
+
+# $f: share/vcsn/notebooks/algos/Index.ipynb
+# $b: algos/Index.ipynb
+install-data-hook: install-hook-%C%
+install-hook-%C%:
+	@$(NORMAL_INSTALL)
+	@dir="$(DESTDIR)$(notebooksdir)";			\
+	for f in $(dist_notebooks_data);			\
+	do							\
+	  b=$$(echo "$$f" | sed -e 's,^%D%/,,');		\
+	  if test ! -f "$$dir/$$b"; then			\
+	    dest=$$(dirname "$$dir/$$b");			\
+	    echo " $(MKDIR_P) '$$dest'";			\
+	    $(MKDIR_P) "$$dest" || exit 1;			\
+	    echo " $(INSTALL_DATA) '$(srcdir)/$$f' '$$dest'";	\
+	    $(INSTALL_DATA) "$(srcdir)/$$f" "$$dest" || exit 1;	\
+	  fi							\
+	done
+
+uninstall-hook: uninstall-hook-%C%
+uninstall-hook-%C%:
+	@$(NORMAL_UNINSTALL)
+	@dir="$(DESTDIR)$(notebooksdir)";		\
+	for f in $(dist_notebooks_data);		\
+	do						\
+	  b=$$(echo "$$f" | sed -e 's,^%D%/,,');	\
+	  if test -f "$$dir/$$b"; then			\
+	    echo " rm -f '$$dir/$$b'";			\
+	    rm -f "$$dir/$$b" || exit 1;		\
+	  fi						\
+	done
