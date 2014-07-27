6ba514764251ef6c0ad42866fc8e7d853ebae45d
Akim Demaille <akim@lrde.epita.fr>
2014-07-27 10:40:52 +0200

    tests: check F2 and operations on weights
    * tests/python/weight.py: here.

--- tests/python/weight.py
+++ tests/python/weight.py
@@ -17,8 +17,17 @@ def check(ctx, w, output):
 check('lal_char(a)_b', '0', '0')
 check('lal_char(a)_b', '1', '1')
 check('lal_char(a)_z', '123', '123')
+check('lal_char(a)_f2', '1', '1')
 check('lal_char(x)_ratexpset<lal_char(abc)_z>', 'a*<23>bc', 'a*<23>bc')
 check('lal_char(x)_seriesset<lal_char(abc)_z>', 'a*<23>bc', '<23>(a*bc)')
 
+
 # Check that we don't ignore trailing characters.
 XFAIL(lambda: vcsn.context('lal_char(ab)_z').weight('123a*'))
+
+# Check + and *.
+c = vcsn.context('lal_char(x)_seriesset<lal_char(abc)_z>')
+CHECK_EQ(c.weight('<5>a+<5>b'),
+         c.weight('<2>a+<3>b') + c.weight('<3>a+<2>b'))
+CHECK_EQ(c.weight('<4>aa+<6>ab+<6>ba+<9>bb'),
+         c.weight('<2>a+<3>b') * c.weight('<2>a+<3>b'))
