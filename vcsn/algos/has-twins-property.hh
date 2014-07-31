84bc48b1b783bc259dc5419c30365d38d2eea51b
Canh Luu <ldcanh@gmail.com>
2014-07-31 03:44:31 +0200

    has_twins_property: use Tarjan's strongly connected components algorithm
    The Tarjan's algorithm is better than Kosaraju's algorithm because it
    visit all vertexs by dfs just only one time and it doesn't do to
    transposing the automaton.
    The score of two alogorithms with two automates:
    r1 = "<3>a((<2>c<3>b<4>a)*){50}<7>c+<2>a((<3>c<2>b<4>a)*){50}<5>a"
    a1 = vcsn.context("lal_char(a-c)_q").ratexp(r1).standard()
    r2 = "<3>a((<2>c<3>b<4>a)*){50}<7>c+<2>a((<3>c<5>b<4>a)*){50}<5>a"
    a1 = vcsn.context("lal_char(a-c)_q").ratexp(r2).standard()
       Kosaraju Tarjan
    a1 1.81s    1.51s
    a2 1.57s    1.36s
    
    * vcsn/algos/has_twins_property.hh: Here.

--- vcsn/algos/has-twins-property.hh
+++ vcsn/algos/has-twins-property.hh
@@ -59,89 +59,28 @@ namespace vcsn
   }
 
 
-  /*--------------------.
-  | reverse_postorder.  |
-  `--------------------*/
+  /*-------------.
+  | scc_tarjan.  |
+  `-------------*/
 
   namespace detail
   {
-    /// Get all vertexs in reverse postorder
-    /// by using depth first search.
-    template <typename Aut>
-    class reverse_postorder_impl
-    {
-    public:
-      using state_t = state_t_of<Aut>;
-
-      reverse_postorder_impl(const Aut& aut)
-      {
-        for (auto s : aut->all_states())
-          if (!has(marked_, s))
-            dfs(s, aut);
-      }
-
-      std::stack<state_t>& reverse_post()
-      {
-        return rvp_;
-      }
-
-      private:
-        void dfs(state_t s, const Aut& aut)
-        {
-          marked_.emplace(s);
-          for (auto t : aut->out(s))
-            {
-              auto dst = aut->dst_of(t);
-              if (!has(marked_, dst))
-                dfs(dst, aut);
-            }
-          rvp_.push(s);
-        }
-      std::stack<state_t> rvp_;
-      std::unordered_set<state_t> marked_;
-      std::stack<state_t> todo_;
-    };
-
-  }
-
-  /// Get all vertices in reverse postorder.
-  template <typename Aut>
-  std::stack<state_t_of<Aut>>
-  reverse_postorder(const Aut& aut)
-  {
-    detail::reverse_postorder_impl<Aut> dv(aut);
-    return dv.reverse_post();
-  }
-
-  /*---------------.
-  | scc_kosaraju.  |
-  `---------------*/
-
-  namespace detail
-  {
-    /// Use Kosajaju algorithm for finding all of strongly
+    /// Use Tarjan's algorithm to find all strongly
     /// connected components.
     template <typename Aut>
-    class scc_kosaraju
+    class scc_tarjan
     {
     public:
       using state_t = state_t_of<Aut>;
       using component_t = std::unordered_set<state_t>;
       using components_t = std::vector<component_t>;
 
-      scc_kosaraju(const Aut& aut)
+      scc_tarjan(const Aut& aut)
       {
-        auto trans = ::vcsn::transpose(aut);
-        auto todo = reverse_postorder(trans);
-        while (!todo.empty())
+        for (auto s : aut->states())
           {
-            auto s = todo.top();
-            todo.pop();
             if (!has(marked_, s))
-              {
-                dfs(s, aut);
-                ++num_;
-              }
+              dfs(s, aut);
           }
       }
 
@@ -153,26 +92,52 @@ namespace vcsn
     private:
       void dfs(state_t s, const Aut& aut)
       {
+        int min = curr_vertex_num_++;
+        low_.emplace(s, min);
         marked_.emplace(s);
-        if (num_ == components_.size())
-          components_.emplace_back(component_t{s});
-        else
-          components_[num_].emplace(s);
+        stack_.push(s);
 
         for (auto t : aut->out(s))
           {
             auto dst = aut->dst_of(t);
             if (!has(marked_, dst))
               dfs(dst, aut);
+            if (low_[dst] < min)
+              min = low_[dst];
           }
+        if (min < low_[s])
+          {
+            low_[s] = min;
+            return;
+          }
+        state_t w;
+        components_.emplace_back(component_t{});
+        do
+          {
+            w = stack_.top();
+            stack_.pop();
+            components_[curr_comp_num_].emplace(w);
+            // This vertex belong only one component
+            // so remove it by update low value to max size.
+            low_[w] = aut->num_states() + 1;
+          }
+        while (w != s);
+        curr_comp_num_++;
       }
 
       /// The current component number.
-      int num_ = 0;
+      int curr_comp_num_ = 0;
+      /// The current visited vertex
+      int curr_vertex_num_ = 0;
+      /// All compnents
       components_t components_;
+      /// List visited vertices
       std::unordered_set<state_t> marked_;
+      /// low_[s] is minimum of vertex that it can go
+      std::unordered_map<state_t, int> low_;
+      /// Contains list vertices same the component
+      std::stack<state_t> stack_;
     };
-
   }
 
   /// Find all strongly connected components of \a aut.
@@ -180,7 +145,7 @@ namespace vcsn
   const std::vector<std::unordered_set<state_t_of<Aut>>>
   components(const Aut& aut)
   {
-    detail::scc_kosaraju<Aut> scc(aut);
+    detail::scc_tarjan<Aut> scc(aut);
     return scc.components();
   }
 
