78acb3cda3c77bb177ae22631c8ed0db01abe11c
Akim Demaille <akim@lrde.epita.fr>
2014-09-15 11:53:49 +0200

    transition-map: be ready to record original transition ids
    * vcsn/core/transition-map.hh: here.

--- vcsn/core/transition-map.hh
+++ vcsn/core/transition-map.hh
@@ -22,21 +22,49 @@ namespace vcsn
     /// \tparam AllOut
     ///    Whether even the transitions to post() (via the
     ///    special label) are to be included.
+    /// \tparam KeepTransitions
+    ///    If enabled, add a "transition" field to the cache that
+    ///    allows to recover the transition id.
     template <typename Aut,
               typename WeightSet = weightset_t_of<Aut>,
               bool Deterministic = false,
-              bool AllOut = false>
+              bool AllOut = false,
+              bool KeepTransitions = false>
     class transition_map
     {
     public:
+      using state_t = state_t_of<Aut>;
+      using transition_t = transition_t_of<Aut>;
       using weightset_t = WeightSet;
       using weight_t = typename weightset_t::value_t;
-      struct transition
+
+      /// Outgoing signature: weight, destination.
+      template <bool KeepTransitions_ = false, typename Dummy = void>
+      struct transition_
+      {
+        transition_(weight_t w, state_t d, transition_t)
+          : wgt(w)
+          , dst(d)
+        {}
+        /// The (converted) weight.
+        weight_t wgt;
+        state_t dst;
+      };
+
+      /// Outgoing signature: weight, destination, transition
+      /// identifier.
+      template <typename Dummy>
+      struct transition_<true, Dummy>
       {
         /// The (converted) weight.
         weight_t wgt;
-        state_t_of<Aut> dst;
+        state_t dst;
+        transition_t_of<Aut> transition;
       };
+
+      /// Outgoing signature: weight, destination, and possibly
+      /// transition identifier.
+      using transition = transition_<KeepTransitions>;
 
       using transitions_t
         = typename std::conditional<Deterministic,
@@ -54,7 +82,7 @@ namespace vcsn
       {}
 
       /// Outgoing transitions of state \a s, sorted by label.
-      map_t& operator[](state_t_of<Aut> s)
+      map_t& operator[](state_t s)
       {
         auto lb = maps_.lower_bound(s);
         if (lb == maps_.end() || maps_.key_comp()(s, lb->first))
@@ -64,7 +92,7 @@ namespace vcsn
       }
 
     private:
-      using maps_t = std::map<state_t_of<Aut>, map_t>;
+      using maps_t = std::map<state_t, map_t>;
 
       /// Insert l -> t in map.
       template <bool Deterministic_>
@@ -89,7 +117,7 @@ namespace vcsn
       /// Build and return the transition map for state s, store at res.
       /// Insert it in the cache.
       map_t&
-      build_map_(typename maps_t::iterator lb, state_t_of<Aut> s)
+      build_map_(typename maps_t::iterator lb, state_t s)
       {
         auto& res = maps_.emplace_hint(lb, s, map_t{})->second;
         for (auto t: aut_->all_out(s))
@@ -98,7 +126,7 @@ namespace vcsn
               auto w = ws_.conv(*aut_->weightset(), aut_->weight_of(t));
               insert_<Deterministic>(res,
                                      aut_->label_of(t),
-                                     transition{w, aut_->dst_of(t)});
+                                     transition{w, aut_->dst_of(t), t});
             }
         return res;
       }
