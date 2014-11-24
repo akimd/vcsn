b8de9f50ca986f5a481aa5c163d53b6c7da36804
Akim Demaille <akim@lrde.epita.fr>
2014-11-24 22:04:11 +0100

    to_string: work for more that Value/ValueSet
    * vcsn/misc/stream.hh: here.

--- vcsn/misc/stream.hh
+++ vcsn/misc/stream.hh
@@ -75,10 +75,10 @@ namespace vcsn LIBVCSN_API
   }
 
   /// Format v via vs.print.
-  template <typename ValueSet, typename... Args>
+  template <typename ValueSet, typename Value = typename ValueSet::value_t,
+            typename... Args>
   auto
-  to_string(const ValueSet& vs,
-            const typename ValueSet::value_t& v, Args&&... args)
+  to_string(const ValueSet& vs, const Value& v, Args&&... args)
     -> std::string
   {
     std::ostringstream o;
