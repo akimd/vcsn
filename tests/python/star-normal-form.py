086066b12f4333567eb2c4691d0b5512a853f2e3
Akim Demaille <akim@lrde.epita.fr>
2015-02-17 09:13:35 +0100

    coverage: star-normal-form
    * tests/python/star-normal-form.py (check): Fix obviously stupid
    checks.
    Add more cases reported by coverage.
    * doc/notebooks/expression.star_normal_form.ipynb: Show some of these
    cases.

--- tests/python/star-normal-form.py
+++ tests/python/star-normal-form.py
@@ -5,16 +5,21 @@ from test import *
 
 ctx = vcsn.context('lal_char(ab), b')
 
-# check INPUT OUTPUT
-# ------------------
-def check(re, exp):
+# check INPUT [OUTPUT = INPUT]
+# ----------------------------
+def check(re, exp = None):
+    if exp is None:
+        exp = re
     exp = ctx.expression(exp)
-    snf = ctx.expression(re).star_normal_form()
+    re  = ctx.expression(re)
+    snf = re.star_normal_form()
+    # Check is equal to the expected result.
     CHECK_EQ(exp, snf)
-    CHECK_EQ(exp, exp)
-    CHECK_EQUIV(exp, snf)
+    # Check that is indeed equivalent to the original expression.
+    CHECK_EQUIV(re, snf)
+    # Check that an expression and its SNF have the same standard.
     # Should be "only" isomorphic, let's try "equal".
-    CHECK_EQ(exp.standard(), snf.standard())
+    CHECK_EQ(re.standard(), snf.standard())
 
 # fail INPUT
 # ----------
@@ -22,11 +27,17 @@ def fail(re):
     re = ctx.expression(re)
     XFAIL(lambda: re.star_normal_form())
 
-check('a', 'a')
-check('a*', 'a*')
+check('\z')
+check('a')
+check('a*')
 check('\e*', '\e')
 check('(a*b*)*', '(a+b)*')
 check('(a*b*)**', '(a+b)*')
+check('(a*b*)**+b**', '(a+b)*+b*')
+
+# Check the dot operator: when one member of a product has a null
+# constant term.
+check('(ab?)*')
 
 # We don't support extended operators.
 fail('a{c}')
@@ -34,3 +45,7 @@ fail('a*&b*')
 fail('a:b')
 fail('a{\}b')
 fail('a*{T}')
+
+# We don't support weighted expressions.
+fail('(?@lal_char, z)<2>a*')
+fail('(?@lal_char, z)a*<2>')
