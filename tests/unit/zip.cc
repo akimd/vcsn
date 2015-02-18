41137abf36faf3ac9ae35e9e450090896358e096
Valentin Tolmer <valentin.tolmer@gmail.com>
2015-02-18 16:40:59 +0100

    zip: add zip_with_padding
    When zipping over several lists, zip_with_padding uses the given default values
    to continue the iteration until every list comes to an end, instead of stopping
    when the first list reaches the end.
    
    * vcsn/misc/zip.hh: Implement.
    * tests/unit/zip.cc, tests/unit/zip.chk: Update test.

--- tests/unit/zip.cc
+++ tests/unit/zip.cc
@@ -20,16 +20,23 @@ int main()
   std::vector<int> ints
     = { 1, 20, 30, 40 };
   std::list<double> floats
-    = { 1.1, 2.22, 3.3, 4.4 };
+    = { 1.1, 2.22, 3.3, 4.4, 5.5, 6.6 };
   std::vector<std::string> strings
-    = { "one", "deux", "three", "four" };
+    = { "one", "deux", "three", "four", "five" };
 
   using vcsn::zip;
+  using vcsn::zip_with_padding;
   for (auto i: zip(ints))
     std::cout << i << std::endl;
 
   for (auto i: zip(ints, floats, strings))
     std::cout << i << std::endl;
+
+  auto pad = std::make_tuple (0, 0.0, "zero", 'a');
+  std::string msg = "test";
+  for (auto i : vcsn::zip_with_padding(pad, ints, floats, strings, msg))
+    std::cout << i << std::endl;
+
 
   for (auto i: zip(zip(ints), zip(floats, strings)))
     std::cout << i << std::endl;
--- vcsn/misc/zip.hh
+++ vcsn/misc/zip.hh
@@ -1,10 +1,10 @@
-#ifndef VCSN_MISC_ZIP_HH
-# define VCSN_MISC_ZIP_HH
+#pragma once
 
 # include <boost/iterator/iterator_facade.hpp>
 
 # include <vcsn/misc/raise.hh> // pass
 # include <vcsn/misc/tuple.hh>
+# include <vcsn/misc/type_traits.hh> // vcsn::enable_if_t
 
 namespace vcsn
 {
@@ -86,12 +86,33 @@ namespace vcsn
         return *this;
       }
 
+      void increment()
+      {
+        (*this)++;
+      }
+
+      void advance(std::size_t n)
+      {
+        for (size_t i = 0; i < n; ++i)
+          increment();
+      }
+
       value_type operator*() const
       {
         return dereference_(indices_t{});
       }
 
-    private:
+      const iterators_type& iterators() const
+      {
+        return is_;
+      }
+
+      const iterators_type& end() const
+      {
+        return ends_;
+      }
+
+    protected:
       friend class boost::iterator_core_access;
 
       /// We have reached the end, move all the cursors to this end.
@@ -218,6 +239,191 @@ namespace vcsn
     sequences_t sequences_;
   };
 
+  template <typename ZipSequences>
+  struct zip_sequences_padded
+  {
+    /// Type of the wrapped zip sequence
+    using zip_sequences_t = ZipSequences;
+
+    /// Type of the tuples of all the maps
+    using sequences_t = typename zip_sequences_t::sequences_t;
+
+    /// Type of index sequences.
+    template <std::size_t... I>
+    using seq = vcsn::detail::index_sequence<I...>;
+
+    /// Index sequence for our sequences.
+    using indices_t = typename zip_sequences_t::indices_t;
+
+    /// The type of the members.
+    using value_type = typename zip_sequences_t::value_type;
+
+    /// Tuple of const_iterators.
+    using const_iterators_t = typename zip_sequences_t::const_iterators_t;
+
+    /// Tuple of iterators.
+    using iterators_t = typename zip_sequences_t::iterators_t;
+
+    using padding_t = value_type;
+
+    template <typename... S>
+    zip_sequences_padded(const value_type& pad, const std::tuple<S...>& sequences)
+      : sequences_{sequences}, paddings_{pad}
+    {}
+
+    template <typename... S>
+    zip_sequences_padded(const value_type& pad, S... sequences)
+      : sequences_(sequences...), paddings_{pad}
+    {}
+
+    template <typename ValueType, typename IteratorsType>
+    struct zip_iterator
+      : public zip_sequences_t::template zip_iterator<ValueType, IteratorsType>
+    {
+
+      using parent_t =
+        typename zip_sequences_t::template zip_iterator<ValueType, IteratorsType>;
+
+      /// Underlying iterators.
+      using iterators_type = IteratorsType;
+
+      zip_iterator(const iterators_type& is, const iterators_type& ends,
+                   const padding_t& pad)
+        : parent_t(is, ends)
+        , pad_(pad)
+      {}
+
+      template <typename OtherValue, typename OtherIterators>
+      zip_iterator(zip_iterator<OtherValue, OtherIterators> const& that,
+                   const padding_t& pad)
+        : parent_t(that.is_, that.ends_)
+        , pad_(pad)
+      {}
+      value_type operator*() const
+      {
+        return dereference_(indices_t{});
+      }
+
+      /// Advance to next position.
+      zip_iterator& operator++()
+      {
+        if (!next_())
+          this->done_();
+        return *this;
+      }
+
+
+    private:
+
+      /// Move to the next position.  Return the index of the lastest
+      /// iterator that could move, -1 if we reached the end.
+      bool next_()
+      {
+        return next_(indices_t{});
+      }
+
+      template <std::size_t... I>
+      bool next_(seq<I...>)
+      {
+        bool res = false;
+        using swallow = int[];
+        (void) swallow
+          {
+            (std::get<I>(this->is_) == std::get<I>(this->ends_)
+             ? false
+             : ++std::get<I>(this->is_) == std::get<I>(this->ends_)
+             ? false
+             : res = true)
+            ...
+          };
+        return res;
+      }
+
+      /// Tuple of values.
+      template <std::size_t... I>
+      value_type dereference_(seq<I...>) const
+      {
+        return value_type{(std::get<I>(this->is_) == std::get<I>(this->ends_) ? std::get<I>(pad_) : *std::get<I>(this->is_))...};
+      }
+
+      padding_t pad_;
+    };
+
+    /// Mutable iterator.
+    using iterator = zip_iterator<value_type, iterators_t>;
+
+    /// Const iterator.
+    using const_iterator = zip_iterator<const value_type, const_iterators_t>;
+
+    const_iterator cbegin() const
+    {
+      return cbegin_(indices_t{});
+    }
+
+    const_iterator cend() const
+    {
+      return cend_(indices_t{});
+    }
+
+    const_iterator begin() const
+    {
+      return cbegin();
+    }
+
+    const_iterator end() const
+    {
+      return cend();
+    }
+
+    iterator begin()
+    {
+      return begin_(indices_t{});
+    }
+
+    iterator end()
+    {
+      return end_(indices_t{});
+    }
+
+  private:
+    template <std::size_t... I>
+    const_iterator cbegin_(seq<I...>) const
+    {
+      return {const_iterators_t{std::get<I>(sequences_).cbegin()...},
+              const_iterators_t{std::get<I>(sequences_).cend()...},
+              paddings_};
+    }
+
+    template <std::size_t... I>
+    const_iterator cend_(seq<I...>) const
+    {
+      return {const_iterators_t{std::get<I>(sequences_).cend()...},
+              const_iterators_t{std::get<I>(sequences_).cend()...},
+              paddings_};
+    }
+
+
+    template <std::size_t... I>
+    iterator begin_(seq<I...>)
+    {
+      return {iterators_t{std::get<I>(sequences_).begin()...},
+              iterators_t{std::get<I>(sequences_).end()...},
+              paddings_};
+    }
+
+    template <std::size_t... I>
+    iterator end_(seq<I...>)
+    {
+      return {iterators_t{std::get<I>(sequences_).end()...},
+              iterators_t{std::get<I>(sequences_).end()...},
+              paddings_};
+    }
+
+  private:
+    sequences_t sequences_;
+    padding_t paddings_;
+  };
+
   template <typename... Sequences>
   zip_sequences<Sequences...>
   zip(Sequences&&... seqs)
@@ -231,6 +437,11 @@ namespace vcsn
   {
     return {seqs};
   }
-}
 
-#endif // !VCSN_MISC_ZIP_HH
+  template <typename... Sequences>
+  zip_sequences_padded<zip_sequences<Sequences...>>
+  zip_with_padding(const std::tuple<typename Sequences::value_type...>& paddings, const Sequences&... seq)
+  {
+    return {paddings, std::make_tuple(seq...)};
+  }
+}
