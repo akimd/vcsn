a2032ea3396b7753c9ad589d09f06767685dc23e
Akim Demaille <akim@lrde.epita.fr>
2015-03-10 20:00:56 +0100

    quotient: style: minor issues
    * vcsn/algos/quotient.hh: Improve style.

--- vcsn/algos/quotient.hh
+++ vcsn/algos/quotient.hh
@@ -44,14 +44,15 @@ namespace vcsn
       /// their state ids.
       void sort_classes_()
       {
-        /* For each class, put its smallest numbered state first.  We
-           don't need to fully sort.  */
+        // For each class, put its smallest numbered state first
+        // (set_t is not a std::set, it's a vector!).  We don't need
+        // to fully sort.
         for (unsigned c = 0; c < num_classes_; ++c)
             std::swap(class_to_set_[c][0],
                       *std::min_element(begin(class_to_set_[c]),
                                         end(class_to_set_[c])));
 
-        /* Sort class numbers by smallest state number.  */
+        // Sort class numbers by smallest state number.
         std::sort(begin(class_to_set_), end(class_to_set_),
                   [](const set_t& lhs, const set_t& rhs) -> bool
                   {
@@ -60,21 +61,21 @@ namespace vcsn
       }
 
       /// Build the resulting automaton.
-      partition_automaton_t build_result_(const automaton_t& aut)
+      /// \param aut   the input automaton to quotient
+      partition_automaton_t operator()(const automaton_t& aut)
       {
         state_to_class_t state_to_class;
         for (unsigned c = 0; c < num_classes_; ++c)
           for (auto s: class_to_set_[c])
             state_to_class[s] = c;
 
-        partition_automaton_t res
-          = make_shared_ptr<partition_automaton_t>(aut);
-        class_to_res_state_.resize(num_classes_);
+        auto res = make_shared_ptr<partition_automaton_t>(aut);
+        auto class_to_res_state = class_to_state_t(num_classes_);
         for (unsigned c = 0; c < num_classes_; ++c)
           {
             const std::vector<state_t>& set = class_to_set_[c];
             state_t s = set[0];
-            class_to_res_state_[c]
+            class_to_res_state[c]
               = s == aut->pre()  ? res->pre()
               : s == aut->post() ? res->post()
               : res->new_state(set);
@@ -84,28 +85,21 @@ namespace vcsn
             // Copy the transitions of the first state of the class in
             // the result.
             state_t s = class_to_set_[c][0];
-            state_t src = class_to_res_state_[c];
+            state_t src = class_to_res_state[c];
             for (auto t : aut->all_out(s))
               {
                 state_t d = aut->dst_of(t);
-                state_t dst = class_to_res_state_[state_to_class[d]];
+                state_t dst = class_to_res_state[state_to_class[d]];
                 res->add_transition(src, dst,
                                     aut->label_of(t), aut->weight_of(t));
               }
           }
-        return std::move(res);
-      }
-
-      /// The minimized automaton.
-      partition_automaton_t operator()(const automaton_t& aut)
-      {
-        return build_result_(aut);
+        return res;
       }
 
     private:
       class_to_set_t& class_to_set_;
       unsigned num_classes_;
-      class_to_state_t class_to_res_state_;
     };
   } // detail::
 
@@ -113,10 +107,10 @@ namespace vcsn
   inline
   auto
   quotient(const Aut& a,
-           typename detail::quotienter<Aut>::class_to_set_t& classes)
+           typename detail::quotienter<Aut>::class_to_set_t& cs)
     -> partition_automaton<Aut>
   {
-    detail::quotienter<Aut> quotient(classes);
+    detail::quotienter<Aut> quotient(cs);
     return quotient(a);
   }
 
