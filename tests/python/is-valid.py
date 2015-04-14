1f8ef5bf3c3735b9151cb6f2dddd0cf5c8405b65
Akim Demaille <akim@lrde.epita.fr>
2015-04-14 11:42:31 +0200

    tests: use fewer context types
    * tests/python/is-valid.py: here.

--- tests/python/is-valid.py
+++ tests/python/is-valid.py
@@ -3,8 +3,7 @@
 import vcsn
 from test import *
 
-lal = vcsn.context('lal_char(abc), r')
-lan = vcsn.context('lan_char(abc), r')
+ctx = vcsn.context('lal_char(abc), r')
 
 # check(OUTPUT, RAT_EXP)
 # ----------------------
@@ -13,16 +12,16 @@ lan = vcsn.context('lan_char(abc), r')
 #
 # Use a context with expression weights to check the order of products.
 def check (exp, re):
-    r = lal.expression(re)
+    r = ctx.expression(re)
     CHECK_EQ(exp, r.is_valid())
     # Check that we are consistent with the validity of the Thompson
     # of RAT_EXP.
-    a = lan.expression(re).thompson()
+    a = ctx.expression(re).thompson()
     CHECK_EQ(exp, a.is_valid())
 
-check(True, 'a*')
+check(True,  'a*')
 check(False, '(<42>b*)*')
-check(True, '<64>a+<4>b+(<0.6>b*)*')
+check(True,  '<64>a+<4>b+(<0.6>b*)*')
 check(False, '(<0.6>a*+<0.4>b*)*')
-check(True, '(<0.6>a*+<0.3>b*)*<42>')
-check(True, '(<0.6>a*.<0.4>b*)*')
+check(True,  '(<0.6>a*+<0.3>b*)*<42>')
+check(True,  '(<0.6>a*.<0.4>b*)*')
