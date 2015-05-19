da55bb8cfdaedfcab2f67a8a9246ed7f0a6fb855
Akim Demaille <akim@lrde.epita.fr>
2015-05-19 17:07:30 +0200

    score: monitor trie
    On erebus, Clang 3.6, -O3 -DNDEBUG
    
      0.99 p.trie()             # p = [a-j]{6}
    
    * python/vcsn/polynomial.py (__pow__): New.
    * bin/vcsn-score (trie): New.

--- python/vcsn/polynomial.py
+++ python/vcsn/polynomial.py
@@ -10,3 +10,12 @@ polynomial.__add__ = polynomial.sum
 polynomial.__mul__ = _right_mult
 polynomial.__repr__ = lambda self: self.format('text')
 polynomial._repr_latex_ = lambda self: '$' + self.format('latex') + '$'
+
+def _polynomial_pow(p, k):
+    # FIXME: cannot generate the unit polynomial with the current API.
+    assert 0 < k
+    res = p
+    for i in range(1, k):
+        res *= p
+    return res
+polynomial.__pow__ = _polynomial_pow
