3f9369dfb18f105c4e7247d99caf931696028dcd
Antoine Pietri <antoine.pietri@lrde.epita.fr>
2015-05-28 17:37:10 +0200

    start status: add descriptions of all the statuses
    * vcsn/misc/star_status.hh: Here.

--- vcsn/misc/star_status.hh
+++ vcsn/misc/star_status.hh
@@ -2,5 +2,21 @@
 
 namespace vcsn
 {
-  enum star_status_t { STARRABLE, NON_STARRABLE, TOPS, ABSVAL };
+  enum star_status_t
+    {
+      /// valid iff proper succeeds on the "absolute value" of
+      /// the automaton
+      ABSVAL,
+
+      // valid iff there is no epsilon-circuit with weight zero. Warning: the
+      // property tested here is the acyclicity, which is equivalent only in
+      // zero divisor free semirings.
+      NON_STARRABLE,
+
+      /// always valid.
+      STARRABLE,
+
+      /// valid iff proper succeeds.
+      TOPS,
+    };
 };
