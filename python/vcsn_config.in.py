20576a98a36618145ff16cb6c2a930e60aa061f6
Akim Demaille <akim@lrde.epita.fr>
2015-08-13 08:42:11 +0200

    python: fix and complete the installation config
    Currently C++ and Python don't see the same config (CPPFLAGS etc.).
    C++ is right, Python misses some bits.  In particular
    vcsn.config.cppflags is lacking the -I we need to find our headers
    when we are installed, so distcheck's installcheck would fail if the
    runtime compilations were fired from Python.
    
    * configure.ac: Be sure to define the same symbols to the same values.
    * python/vcsn_config.in.py: Provive the missing place holders.

--- python/vcsn_config.in.py
+++ python/vcsn_config.in.py
@@ -1,17 +1,35 @@
 import os
 
 def env(key, default):
-    return os.environ[key] if key in os.environ else default
+    res = os.environ[key] if key in os.environ else default
+    res = res.strip()
+    return res
+
+def integer(key):
+    if key in os.environ:
+        try:
+            val = int(os.environ[key])
+        except ValueError:
+            val = 1
+    else:
+        val = 0
+    return val
 
 config = {
     'version': '@PACKAGE_VERSION@',
 
-    'ccache':   env('VCSN_CCACHE', r'''@CCACHE@'''),
-    'cppflags': env('VCSN_CPPFLAGS', r'''@CPPFLAGS@'''),
-    'cxx':      env('VCSN_CXX', r'''@CXX_NO_CCACHE@'''),
+    'ccache':   env('VCSN_CCACHE',   r'''@CCACHE@'''),
+    'cppflags': env('VCSN_CPPFLAGS', r'''@VCSN_CPPFLAGS@'''),
+    'cxx':      env('VCSN_CXX',      r'''@VCSN_CXX@'''),
     'cxxflags': env('VCSN_CXXFLAGS', r'''@CXXFLAGS@'''),
-    'ldflags':  env('VCSN_LDFLAGS', r'''@LDFLAGS@'''),
+    'ldflags':  env('VCSN_LDFLAGS',  r'''@VCSN_LDFLAGS@'''),
 
-    # Where the automaton library is stored.
-    'datadir':  env('VCSN_DATA_PATH', r'''@DATADIR@'''),
+    # Where out files are installed.
+    'datadir':    env('VCSN_DATA_PATH',  r'''@DATADIR@'''),
+    'includedir': env('VCSN_INCLUDEDIR', r'''@INCLUDEDIR@'''),
+    'libdir':     env('VCSN_LIBDIR',     r'''@LIBDIR@'''),
+    'pyexecdir':  env('VCSN_PYEXECDIR',  r'''@PYEXECDIR@'''),
+
+    # Various flags.
+    'verbose':  integer('VCSN_VERBOSE'),
 }
