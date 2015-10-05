akimd/vcsn

commit 9483607d284bcc11343f4441f5654cc59ab966dc

automata: beware of special characters
We are still fragile on I/O of automata when the labels are a bit
special (say `'`, or `|`, or `<`, etc.).

We need to support more escapes in input, and escape more in output.

* lib/vcsn/algos/edit-automaton.cc
* vcsn/alphabets/char.hh (get_char): When a symbol is backslashed, always
strip the backslash.
(get_letter): Support quoted letters.
(print): Escape special characters.
* vcsn/alphabets/setalpha.hh (get_word): Support quoted words.
* vcsn/labelset/nullableset.hh (conv): Be ready to pass escaped character
to the inner labelset.

And we also need to be able to not escape, in the case of OpenFST.  To
this end, format::raw is introduced.  But it might be a mistake: maybe
the way things are printed should be dealt with in OpenFST itself.
Maybe the future will tell which approach is better.
* vcsn/misc/format.hh (raw): New format.

* tests/python/efsm.dir/ascii-to-one.efsm, tests/python/efsm.dir/ascii-to-one.gv:
New.
* tests/python/efsm.py: Use them to check such I/O for transducers.

vcsn/labelset/nullableset.hh
@@ -404,18 +404,19 @@ namespace vcsn
     value_t
     conv(std::istream& i, bool quoted = true) const
     {
-      value_t res;
       int c = i.peek();
+      // Check for '\e', otherwise pass it to the inner labelset.
       if (c == '\\')
         {
           i.ignore();
           c = i.get();
-          require(c == 'e', "invalid label: unexpected \\", str_escape(c));
-          res = one();
+          if (c == 'e')
+            return one();
+          else
+            i.putback('\\');
         }
-      else
-        res = value(ls_->conv(i, quoted));
-      return res;
+
+      return value(ls_->conv(i, quoted));
     }
 
     template <typename Fun>
@@ -478,6 +479,10 @@ namespace vcsn
           o <<  "nullableset<";
           ls_->print_set(o, fmt);
           o << '>';
+          break;
+
+        case format::raw:
+          assert(0);
           break;
         }
 

lib/vcsn/algos/edit-automaton.cc
@@ -1,3 +1,6 @@
+#include <cctype>
+#include <regex>
+
 #include <lib/vcsn/algos/registry.hh>
 #include <vcsn/algos/edit-automaton.hh>
 #include <vcsn/dyn/algos.hh>
@@ -65,6 +68,22 @@ namespace vcsn
         }
       BUILTIN_UNREACHABLE();
     }
+
+    /// Turn a label into a parsable label: escape special characters.
+    std::string quote(lazy_automaton_editor::string_t s)
+    {
+      if (s == "\\e"
+          || s.get().size() == 1 && std::isalnum(s.get()[0]))
+        return s;
+      else
+        {
+          // Backslash backslashes and quotes.
+          static auto re = std::regex{"['\\\\]"};
+          return ("'"
+                  + std::regex_replace(s.get(), re, "\\$&")
+                  + "'");
+        }
+    }
   }
 
   /// Add transitions from \a src to \a dst, labeled by \a entry.
@@ -76,10 +95,14 @@ namespace vcsn
                                         string_t weight)
   {
     input_type_ = std::max(input_type_, type(lbl1));
-    if (!lbl2.get().empty())
+
+    if (lbl2.get().empty())
+      lbl1 = quote(lbl1);
+    else
       {
+        // Turn into a multiple-tape label.
         output_type_ = std::max(output_type_, type(lbl2));
-        lbl1 = "(" + lbl1.get() + "," + lbl2.get() + ")";
+        lbl1 = quote(lbl1) + "|" + quote(lbl2);
       }
     transitions_.emplace_back(src, dst, lbl1, weight);
 

lib/vcsn/dot/scan.ll
@@ -81,7 +81,7 @@ NUM     [-]?("."{digit}+|{digit}+("."{digit}*)?)
     return parser::make_ID(string_t{s}, loc);
   }
 
-  \\.       s.push_back(yytext[1]);
+  "\\".     s.push_back(yytext[1]);
   [^\\""]+  s.append(yytext, yyleng);
 
   <<EOF>> {

lib/vcsn/misc/format.cc
@@ -17,6 +17,7 @@ namespace vcsn
         {"auto",    deflt},
         {"default", deflt},
         {"latex",   latex},
+        {"raw",     raw},
         {"text",    text},
       };
     format_ = getargs("format", map, f);
@@ -28,6 +29,8 @@ namespace vcsn
       {
       case format::latex:
         return "latex";
+      case format::raw:
+        return "raw";
       case format::text:
         return "text";
       }

tests/python/efsm.dir/ascii-to-one.efsm
@@ -1,0 +1,96 @@
+#! /bin/sh
+
+me=$(basename "$0")
+medir=$(mktemp -d "/tmp/$me.XXXXXX") || exit 1
+
+arc_type=standard
+
+cat >$medir/isymbols.txt <<\EOFSM
+\e	0
+!	1
+"	2
+#	3
+$	4
+%	5
+&	6
+'	7
+(	8
+)	9
+*	10
++	11
+,	12
+-	13
+.	14
+/	15
+:	16
+;	17
+<	18
+=	19
+>	20
+?	21
+@	22
+[	23
+\	24
+]	25
+^	26
+_	27
+`	28
+{	29
+|	30
+}	31
+~	32
+<space>	33
+EOFSM
+
+cat >$medir/osymbols.txt <<\EOFSM
+\e	0
+EOFSM
+
+cat >$medir/transitions.fsm <<\EOFSM
+0	1	\e	\e
+0	1	!	\e
+0	1	"	\e
+0	1	#	\e
+0	1	$	\e
+0	1	%	\e
+0	1	&	\e
+0	1	'	\e
+0	1	(	\e
+0	1	)	\e
+0	1	*	\e
+0	1	+	\e
+0	1	,	\e
+0	1	-	\e
+0	1	.	\e
+0	1	/	\e
+0	1	:	\e
+0	1	;	\e
+0	1	<	\e
+0	1	=	\e
+0	1	>	\e
+0	1	?	\e
+0	1	@	\e
+0	1	[	\e
+0	1	\	\e
+0	1	]	\e
+0	1	^	\e
+0	1	_	\e
+0	1	`	\e
+0	1	{	\e
+0	1	|	\e
+0	1	}	\e
+0	1	~	\e
+0	1	<space>	\e
+0
+1
+EOFSM
+
+fstcompile \
+  --arc_type=$arc_type \
+  --keep_isymbols --isymbols=$medir/isymbols.txt \
+  --keep_osymbols --osymbols=$medir/osymbols.txt \
+  $medir/transitions.fsm "$@"
+sta=$?
+
+rm -rf $medir
+exit $sta

tests/python/efsm.dir/ascii-to-one.gv
@@ -1,0 +1,21 @@
+digraph
+{
+  vcsn_context = "lat<wordset<char_letters(!\"#$%&\\'\\(\\)*+,-./:;\\<=\\>?@\\[\\\\\\]^_`aceps{\\|}~)>, nullableset<letterset<char_letters()>>>, b"
+  rankdir = LR
+  edge [arrowhead = vee, arrowsize = .6]
+  {
+    node [shape = point, width = 0]
+    I0
+    F0
+    F1
+  }
+  {
+    node [shape = circle, style = rounded, width = 0.5]
+    0
+    1
+  }
+  I0 -> 0
+  0 -> F0
+  0 -> 1 [label = "\\e|\\e, !|\\e, \\\"|\\e, #|\\e, $|\\e, %|\\e, &|\\e, \\'|\\e, \\(|\\e, \\)|\\e, *|\\e, \\+|\\e, \\,|\\e, -|\\e, .|\\e, /|\\e, :|\\e, ;|\\e, \\<|\\e, =|\\e, \\>|\\e, ?|\\e, @|\\e, \\[|\\e, \\\\|\\e, \\]|\\e, ^|\\e, _|\\e, `|\\e, {|\\e, \\||\\e, }|\\e, ~|\\e, \\<space\\>|\\e"]
+  1 -> F1
+}

tests/python/efsm.py
@@ -52,7 +52,7 @@ check(a, 'a1.efsm')
 a = load('lal_char_zmin/slowgrow.gv')
 check(a, 'slowgrow.efsm')
 
-for f in ["lal-char-zmin", "lat-zmin"]:
+for f in ["lal-char-zmin", "lat-zmin", "ascii-to-one"]:
   print("f:", f)
   a = vcsn.automaton(filename = medir + "/" + f + '.gv')
   check(a, f + '.efsm')

tests/python/local.mk
@@ -168,6 +168,8 @@ EXTRA_DIST +=                                   \
   %D%/efsm.dir/a2x.efsm                         \
   %D%/efsm.dir/a2xyz.efsm                       \
   %D%/efsm.dir/abs.efsm                         \
+  %D%/efsm.dir/ascii-to-one.efsm                \
+  %D%/efsm.dir/ascii-to-one.gv                  \
   %D%/efsm.dir/lal-char-log.gv                  \
   %D%/efsm.dir/lal-char-zmin.efsm               \
   %D%/efsm.dir/lal-char-zmin.gv                 \

commit a1487303760697e24540cebe8677548f87dc3934

nullableset: fix conv
Commit 9483607d284bcc11343f4441f5654cc59ab966dc broke the
implementation of conv:

  int c = i.peek();
  // Check for '\e', otherwise pass it to the inner labelset.
  if (c == '\\')
    {
      i.ignore();
      c = i.get();
      if (c == 'e')
        return one();
      else
        i.putback('\\');
    }

obviously, when a backslash is processed and not followed by an `e`,
we putback the character we read, but not the backslash.

No change in the test suite, as some forthcoming tests use this
feature anyway.

* vcsn/labelset/nullableset.hh (conv): Fix.
Based on setalpha::get_word.

vcsn/labelset/nullableset.hh
@@ -404,18 +404,19 @@ namespace vcsn
     value_t
     conv(std::istream& i, bool quoted = true) const
     {
-      int c = i.peek();
       // Check for '\e', otherwise pass it to the inner labelset.
-      if (c == '\\')
+      if (i.good() && i.peek() == '\\')
         {
           i.ignore();
-          c = i.get();
+          int c = i.peek();
           if (c == 'e')
-            return one();
+            {
+              i.ignore();
+              return {};
+            }
           else
-            i.putback('\\');
+            i.unget();
         }
-
       return value(ls_->conv(i, quoted));
     }
 