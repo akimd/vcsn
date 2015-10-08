c5654d38dd299bed9b4169d46c674f13e571fff9
Akim Demaille <akim@lrde.epita.fr>
2015-10-08 15:43:55 +0200

    notebook: make it work when installed
    For some reason, when `migrated` does not exist, running ipython
    notebook with our profile triggers the creation of this file.  If the
    installation was run by root, of course, Jupyter fails to create this
    `migrated` file, and the whole process fails.  If migrated is
    installed, it works.
    
    Let's do that until we know better.
    
    * share/vcsn/jupyter_profile/migrated: New.
    * share/vcsn/local.mk: Install it.
    Fix the installation directory for Jupyter/IPython files.

--- share/vcsn/local.mk
+++ share/vcsn/local.mk
@@ -12,42 +12,46 @@
 ## The Vaucanson Group consists of people listed in the `AUTHORS' file.
 
 jsdir = $(pkgdatadir)/js
-dist_js_DATA =					\
+dist_js_DATA =                                  \
   %D%/js/AutomatonD3Widget.js
 
-jupyter_profiledir = $(pkgdatadir)/jupyter_profile
-dist_jupyter_profile_DATA =			\
-  %D%/jupyter_profile/custom/custom.css		\
-  %D%/jupyter_profile/custom/custom.js		\
+jupyterdir = $(pkgdatadir)/jupyter_profile
+dist_jupyter_DATA =                             \
+  %D%/jupyter_profile/migrated
+
+jupyter_customdir = $(jupyterdir)/custom
+dist_jupyter_custom_DATA =                      \
+  %D%/jupyter_profile/custom/custom.css         \
+  %D%/jupyter_profile/custom/custom.js          \
   %D%/jupyter_profile/custom/logo.png
 
-notebook_profiledir = $(pkgdatadir)/notebook_profile
-dist_notebook_profile_DATA =			\
-  %D%/notebook_profile/static/custom/custom.css	\
-  %D%/notebook_profile/static/custom/custom.js	\
+notebook_profiledir = $(pkgdatadir)/notebook_profile/static/custom
+dist_notebook_profile_DATA =                    \
+  %D%/notebook_profile/static/custom/custom.css \
+  %D%/notebook_profile/static/custom/custom.js  \
   %D%/notebook_profile/static/custom/logo.png
 
 figsdir = $(pkgdatadir)/figs
-dist_figs_DATA =				\
-  %D%/figs/vcsn.png				\
+dist_figs_DATA =                                \
+  %D%/figs/vcsn.png                             \
   %D%/figs/vcsn.mini.png
 
 lal_char_bdir = $(pkgdatadir)/lal_char_b
-dist_lal_char_b_DATA =				\
-  %D%/lal_char_b/a1.gv				\
-  %D%/lal_char_b/b1.gv				\
-  %D%/lal_char_b/evena.gv			\
+dist_lal_char_b_DATA =                          \
+  %D%/lal_char_b/a1.gv                          \
+  %D%/lal_char_b/b1.gv                          \
+  %D%/lal_char_b/evena.gv                       \
   %D%/lal_char_b/oddb.gv
 
 lal_char_zdir = $(pkgdatadir)/lal_char_z
-dist_lal_char_z_DATA =				\
-  %D%/lal_char_z/b1.gv				\
-  %D%/lal_char_z/binary.gv			\
-  %D%/lal_char_z/c1.gv				\
+dist_lal_char_z_DATA =                          \
+  %D%/lal_char_z/b1.gv                          \
+  %D%/lal_char_z/binary.gv                      \
+  %D%/lal_char_z/c1.gv                          \
   %D%/lal_char_z/d1.gv
 
 lal_char_zmindir = $(pkgdatadir)/lal_char_zmin
-dist_lal_char_zmin_DATA =			\
-  %D%/lal_char_zmin/minab.gv			\
-  %D%/lal_char_zmin/minblocka.gv		\
+dist_lal_char_zmin_DATA =                       \
+  %D%/lal_char_zmin/minab.gv                    \
+  %D%/lal_char_zmin/minblocka.gv                \
   %D%/lal_char_zmin/slowgrow.gv
