d54c425a33326630056c270af0e90cdbb341a171
Akim Demaille <akim@lrde.epita.fr>
2015-10-15 15:18:12 +0200

    demangle: improve sugaring
    * python/vcsn/demangle.py: Fix the regex vs. re issue.
    Fix funky indentation.
    (param1, param2): Fuse into...
    (param): this.
    (sub): Adjust.
    Use it.
    * tests/python/demangle.py: New.

--- tests/python/demangle.py
+++ tests/python/demangle.py
@@ -1,17 +1,35 @@
 #! /usr/bin/env python
+# -*- coding: utf-8 -*-
 
 import vcsn
 from test import *
 from vcsn.demangle import sugar
 
-def check_sugar(input, exp):
+try:
+    import regex
+    has_regex = True
+except ImportError:
+    has_regex = False
+
+
+## ------- ##
+## sugar.  ##
+## ------- ##
+
+def check(input, exp_regex, exp_re):
+    '''Check that `sugar(input)` is `exp1` when regex is available,
+    otherwise `exp2`.'''
+    exp = exp_regex if has_regex else exp_re
     CHECK_EQ(exp, sugar(input))
 
-check_sugar('''
-context:       context<letterset<char_letters>, expressionset<context<letterset<char_letters>, b>>>
-is_out_sorted: transpose_automaton<mutable_automaton<context<letterset<char_letters>, b>>>
-strip:         name_automaton<mutable_automaton<context<letterset<char_letters>, expressionset<context<letterset<char_letters>, b>>>>>''',
-            '''
-context:       chars → expressionset<chars → b>
-is_out_sorted: transpose<mutable<chars → b>>
-strip:         name<mutable<chars → expressionset<chars → b>>>''')
+check('context<letterset<char_letters>, expressionset<context<letterset<char_letters>, b>>>',
+      'chars → expressionset<chars → b>',
+      'letterset<chars>, expressionset<context<letterset<chars>, b>>')
+
+check('transpose_automaton<mutable_automaton<context<letterset<char_letters>, b>>>',
+      'transpose<mutable<chars → b>>',
+      'transpose<mutable<context<letterset<chars>, b>>>')
+
+check('name_automaton<mutable_automaton<context<letterset<char_letters>, expressionset<context<letterset<char_letters>, b>>>>>',
+      'name<mutable<chars → expressionset<chars → b>>>',
+      'name<mutable<context<letterset<chars>, expressionset<context<letterset<chars>, b>>>>>')
