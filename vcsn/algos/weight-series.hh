d3a9fa52c9e61b21411ce236e5669b18a379ffd4
Sébastien Piat <spiat@lrde.epita.fr>
2015-12-04 13:48:19 +0100

    weight-series: use proper on spontaneous version of automaton
    Implement this version for weightset different from Nmin.
    With these weightset the computation does not end using state_distancer.
    0.38s : a.weight_series() # a = std([a-z]{4000}), c = [a-z] -> Nmin, 20x
    1.58s : a.weight_series() # a = std([a-z]{200}),  c = [a-z] -> Z,    20x
    Nmin implementation is far more efficient though.
    (Closes #19)
    
    * vcsn/algos/weight-series.hh: Here.
    * libexec/vcsn-score: Bench it.

--- vcsn/algos/weight-series.hh
+++ vcsn/algos/weight-series.hh
@@ -1,9 +1,13 @@
 #pragma once
 
+#include <vcsn/algos/eval.hh>
 #include <vcsn/algos/distance.hh>
+#include <vcsn/algos/to-spontaneous.hh>
+#include <vcsn/algos/proper.hh>
 #include <vcsn/dyn/fwd.hh>
 #include <vcsn/dyn/automaton.hh>
 #include <vcsn/dyn/weight.hh>
+#include <vcsn/weightset/fwd.hh> // nmin
 
 namespace vcsn
 {
@@ -11,7 +15,19 @@ namespace vcsn
   inline
   auto
   weight_series(const Aut& a)
-    -> weight_t_of<Aut>
+    -> vcsn::enable_if_t<!std::is_same<weightset_t_of<Aut>, nmin>::value,
+                         weight_t_of<Aut>>
+  {
+    auto aut = proper(to_spontaneous(a));
+    return eval(aut);
+  }
+
+  template <typename Aut>
+  inline
+  auto
+  weight_series(const Aut& a)
+    -> vcsn::enable_if_t<std::is_same<weightset_t_of<Aut>, nmin>::value,
+                         weight_t_of<Aut>>
   {
     state_distancer<Aut, weightset_t_of<Aut>> d(a);
     return d(a->pre(), a->post());
