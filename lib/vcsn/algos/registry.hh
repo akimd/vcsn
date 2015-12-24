akimd/vcsn

commit ff43b0faeb6c51f9810d29485dceaebdcf0266cf

automaton: improve the conversion support
* vcsn/labelset/letterset.hh, vcsn/labelset/wordset.hh (conv): Improve:
make sure the result is valid (check the alphabet), and support
letterset and nullableset to wordset conversion.
* tests/python/automaton.py: Test lal to law conversion, valid and
invalid.

vcsn/labelset/letterset.hh
@@ -161,9 +161,11 @@ namespace vcsn
       return hash_value(v);
     }
 
-    static value_t
-    conv(self_type, value_t v)
+    value_t
+    conv(self_type, value_t v) const
     {
+      require(is_valid(v) || is_special(v),
+              sname(), ": conv: invalid label: ", str_escape(v));
       return v;
     }
 

vcsn/labelset/wordset.hh
@@ -168,10 +168,37 @@ namespace vcsn
       return hash_value(v);
     }
 
-    static value_t
-    conv(self_type, value_t v)
+    value_t
+    conv(self_type, value_t v) const
     {
       return v;
+    }
+
+    template <typename GenSet_>
+    value_t
+    conv(const letterset<GenSet_>& ls,
+         typename letterset<GenSet_>::value_t v) const
+    {
+      if (ls.is_special(v))
+        return special();
+      else
+        {
+          auto res = value(v);
+          require(is_valid(res),
+                  sname(), ": conv: invalid label: ", str_escape(v));
+          return res;
+        }
+    }
+
+    template <typename LabelSet_>
+    value_t
+    conv(const nullableset<LabelSet_>& ls,
+         typename nullableset<LabelSet_>::value_t v) const
+    {
+      if (ls.is_one(v))
+        return one();
+      else
+        return conv(*ls->labelset(), ls->get_value(v));
     }
 
     value_t

tests/python/automaton.py
@@ -490,3 +490,34 @@ for fn in glob.glob(os.path.join(medir, '*.grail')):
     grail  = open(fn).read().strip()
     CHECK_EQ(grail, a.format('grail'))
     check_grail(a)
+
+
+## ------------ ##
+## Conversion.  ##
+## ------------ ##
+
+# Convert an automaton from lal_char, b to law_char, z.
+CHECK_EQ(vcsn.automaton('''context = "law_char, z"
+  $ -> 0
+  0 -> 1 a, b
+  1 -> 1 c
+  1 -> $''', 'daut'),
+         vcsn.automaton('''context = "lal_char(abc), b"
+  $ -> 0
+  0 -> 1 a, b
+  1 -> 1 c
+  1 -> $''', 'daut').automaton(vcsn.context("law_char(abc), z")))
+
+# Convert an automaton to a smaller, valid, alphabet.
+CHECK_EQ(vcsn.automaton('''context = "law_char(abc), z"
+  0 -> 1 a, b''', 'daut'),
+         vcsn.automaton('''context = "lal_char(a-z), b"
+  0 -> 1 a, b''', 'daut').automaton(vcsn.context("law_char(abc), z")))
+
+# Convert an automaton to a smaller, invalid, alphabet.
+XFAIL(lambda: vcsn.automaton('''context = "lal_char(abc), b"
+  0 -> 1 a, b''', 'daut').automaton(vcsn.context("law_char(xy), z")))
+
+# Convert to an invalid smaller weightset.
+XFAIL(lambda: vcsn.automaton('''context = "lal_char(abc), z"
+  0 -> 1 <3>a, b''', 'daut').automaton(vcsn.context("lal_char(xy), b")))

vcsn/core/mutable-automaton.hh
@@ -504,11 +504,10 @@ namespace vcsn
     ///
     /// \param src  source state
     /// \param dst  destination state
-    /// \param aut  the automaton whose transition will be copied.
+    /// \param aut  the automaton whose transition will be copied
     /// \param t    transition of \a aut whose label and weight are to copy
     /// \param transpose   whether label and weight should be transposed
     ///
-    /// \pre the label is _not checked_, for efficiency.
     /// \pre ! has_transition(src, dst, label_of(l)).
     template <typename A>
     transition_t
@@ -619,9 +618,6 @@ namespace vcsn
     /// \param aut  the automaton whose transition will be copied.
     /// \param t    transition of \a aut whose label and weight are to copy
     /// \param transpose   whether label and weight should be transposed
-    ///
-    /// \pre the label is _not checked_, for efficiency.  Letters out
-    /// of the alphabet will be accepted.
     template <typename A>
     weight_t
     add_transition_copy(state_t src, state_t dst,

commit f1ae7a62a2b1b5053331e05133aacfe6ec530fe9

misc: beware that all the arguments of `require` are evaluated
The introduction of the syntactic sugar for exponents in the
expression pretty-printer (i.e., `a{5}` instead of `aaaaa`) introduced
a huge and illogical performance regression on `to-expression` (see
issue #24):

    $ v score-compare +scores/36s/v2.1-259-g77470c7 +scores/36s/v2.1-260-gb9820c1
    0.11  0.27 a.expression("associative", "delgado") # a = ladybird(2000)
    0.09  3.56 a.expression("associative", "naive") # a = ladybird(8000)
    0.59  0.60 a.expression("associative", "naive") # a = std([a-d]?{100})
    0.21  0.38 a.expression("linear", "delgado") # a = ladybird(2000)
    0.23  0.21 a.expression("linear", "delgado") # a = std([a-d]?{15})
    0.42  1.16 a.expression("linear", "naive") # a = ladybird(4000)
    0.20  0.43 r.format("text")     # r = b.expression("(\e+a)" * 500), 1000x

The problem is that `expressionset::star` contains:

    res = std::make_shared<star_t>(e);
    require(!ids_.is_distributive() || is_valid(*this, res),
            "star: not starrable: ", to_string(self(), e));

and the `to_string` call is expensive!

So the real problem is that `require` looks inoffensive, but it can be
very costly.  To mitigate with this problem, each call to `require`
was audited, and when arguments are costly, we rather use a macro
`VCSN_REQUIRE` to avoid the unnecessary evaluation of the arguments.

The results are _much_ better.  We recovered the same performances for
`to_expression`, and even `sort`, for which a performance regression
was introduced by commit ff43b0f, recovers parts of its performances.

     (1)   (2)   (3)   (4)
    0.16  0.16  0.16  0.13 a.eval("a"*151)      # a = de_bruijn(150), 1000x
    0.07  0.07  0.07  0.04 a.eval("abcxyz"*25)  # a = std([a-z]*), c = [a-z] -> Z, 1000x
    0.11  0.27  0.29  0.10 a.expression("associative", "delgado") # a = ladybird(2000)
    0.09  3.56  3.75  0.07 a.expression("associative", "naive")   # a = ladybird(8000)

lib/vcsn/algos/registry.hh
@@ -83,11 +83,11 @@ namespace vcsn
             {
               vcsn::dyn::compile(name_, sig);
               auto fun = get0(sig);
-              require(fun,
-                      name_,
-                      ": compilation succeeded, "
-                      "but function is unavailable\n",
-                      signatures(sig));
+              VCSN_REQUIRE(fun,
+                           name_,
+                           ": compilation succeeded, "
+                           "but function is unavailable\n",
+                           signatures(sig));
               return fun;
             }
           catch (const jit_error& e)

lib/vcsn/dyn/translate.cc
@@ -307,7 +307,7 @@ namespace vcsn
               first = false;
             }
           lt_dlhandle lib = lt_dlopen((base + ".so").c_str());
-          require(lib, "cannot load lib: ", base, ".so: ", lt_dlerror());
+          VCSN_REQUIRE(lib, "cannot load lib: ", base, ".so: ", lt_dlerror());
         }
 
         /// Compile, and load, a DSO with instantiations for \a ctx.

lib/vcsn/misc/stream.cc
@@ -55,8 +55,9 @@ namespace vcsn
         s.append(1, c);
         --cnt;
       }
-    require(s == expect,
-            "unexpected: ", str_escape(s), ": expected ", str_escape(expect));
+    VCSN_REQUIRE(s == expect,
+                 "unexpected: ", str_escape(s),
+                 ": expected ", str_escape(expect));
     return expect;
   }
 
@@ -65,7 +66,7 @@ namespace vcsn
   get_file_contents(const std::string& file)
   {
     std::ifstream in(file.c_str(), std::ios::in | std::ios::binary);
-    require(in.good(), "cannot read file: ", file, ": ", strerror(errno));
+    VCSN_REQUIRE(in.good(), "cannot read file: ", file, ": ", strerror(errno));
 
     std::string res;
     in.seekg(0, std::ios::end);
@@ -85,8 +86,8 @@ namespace vcsn
     else
       {
         res.reset(new std::ifstream(file.c_str()));
-        require(res->good(),
-                "cannot open ", file, " for reading: ", strerror(errno));
+        VCSN_REQUIRE(res->good(),
+                     "cannot open ", file, " for reading: ", strerror(errno));
       }
     return res;
   }
@@ -100,8 +101,8 @@ namespace vcsn
     else
       {
         res.reset(new std::ofstream(file.c_str()));
-        require(res->good(),
-                "cannot open ", file, " for writing: ", strerror(errno));
+        VCSN_REQUIRE(res->good(),
+                     "cannot open ", file, " for writing: ", strerror(errno));
       }
     return res;
   }

vcsn/algos/divkbaseb.hh
@@ -30,10 +30,10 @@ namespace vcsn
             "divkbaseb: divisor cannot be 0");
     require(2 <= base,
             "divkbaseb: base (", base, ") must be at least 2");
-    require(base <= letters.size(),
-            "divkbaseb: base (", base,
-            ") must be less than or equal to the alphabet size (",
-            letters.size(), ')');
+    VCSN_REQUIRE(base <= letters.size(),
+                 "divkbaseb: base (", base,
+                 ") must be less than or equal to the alphabet size (",
+                 letters.size(), ')');
 
     automaton_t res = make_shared_ptr<automaton_t>(ctx);
 

vcsn/algos/edit-automaton.hh
@@ -179,12 +179,12 @@ namespace vcsn
               // one.
               auto e = conv(ps_, entry, sep_);
               auto i = begin(e);
-              require(e.size() == 1
-                      && (res_->labelset()->is_special(label_of(*i))
-                          || res_->labelset()->is_one(label_of(*i))),
-                      "edit_automaton: invalid ",
-                      s == res_->pre() ? "initial" : "final",
-                      " entry: ", entry.get());
+              VCSN_REQUIRE(e.size() == 1
+                           && (res_->labelset()->is_special(label_of(*i))
+                               || res_->labelset()->is_one(label_of(*i))),
+                           "edit_automaton: invalid ",
+                           s == res_->pre() ? "initial" : "final",
+                           " entry: ", entry.get());
               res_->add_transition(s, d,
                                    res_->prepost_label(), weight_of(*i));
             }

vcsn/algos/scc.hh
@@ -599,9 +599,9 @@ namespace vcsn
 
       const component_t& component(unsigned num) const
       {
-        require(num < components_.size(),
-                "component: invalid component number: ",
-                num, ": there are ", components_.size(), " components");
+        VCSN_REQUIRE(num < components_.size(),
+                     "component: invalid component number: ",
+                     num, ": there are ", components_.size(), " components");
         return components_[num];
       }
 

vcsn/alphabets/setalpha.hh
@@ -184,8 +184,7 @@ namespace vcsn
     word_t
     get_word(std::istream& i) const
     {
-      require(!i.bad(),
-              "conv: invalid stream");
+      require(!i.bad(), "conv: invalid stream");
       // Either an empty word: "\e", or a sequence of non-separators.
       if (i.good() && i.peek() == '\\')
         {
@@ -218,8 +217,7 @@ namespace vcsn
              && c != ')')
         {
           letter_t l = L::get_letter(i, true);
-          require(has(l),
-                  *this, ": invalid letter: ", str_escape(l));
+          VCSN_REQUIRE(has(l), *this, ": invalid letter: ", str_escape(l));
           // FIXME: in-place mul or temporary vector to build the
           // string.
           res = this->mul(res, l);

vcsn/core/rat/expansionset.hh
@@ -543,10 +543,10 @@ namespace vcsn
           auto es = eset_.template project<Tape>();
           auto res = e;
           es.denormalize(res);
-          require(es.expressionset().weightset()->is_zero(res.constant),
-                  es, ": to-expansion: cannot denormalize ",
-                  to_string(es, res),
-                  ", need support for label one (the empty label)");
+          VCSN_REQUIRE(es.expressionset().weightset()->is_zero(res.constant),
+                       es, ": to-expansion: cannot denormalize ",
+                       to_string(es, res),
+                       ", need support for label one (the empty label)");
           return res.polynomials;
         }
 

vcsn/core/rat/expressionset.hxx
@@ -725,8 +725,8 @@ namespace vcsn
     else
       {
         res = std::make_shared<star_t>(e);
-        require(!ids_.is_distributive() || is_valid(*this, res),
-                "star: not starrable: ", to_string(self(), e));
+        VCSN_REQUIRE(!ids_.is_distributive() || is_valid(*this, res),
+                     "star: not starrable: ", to_string(self(), e));
       }
 
     return res;
@@ -1039,7 +1039,8 @@ namespace vcsn
   {
     using boost::range::find;
     value_t res = zero();
-    auto gens = labelset()->generators();
+    const auto& ls = *labelset();
+    auto gens = ls.generators();
     // FIXME: This piece of code screams for factoring.  Yet, I want
     // to avoid useless costs such as building a empty/full set of
     // letters for [^].
@@ -1050,18 +1051,18 @@ namespace vcsn
         {
           auto i = find(gens, cc.first);
           auto end = std::find(i, std::end(gens), cc.second);
-          require(end != std::end(gens),
-                  self(), ": invalid letter interval: ",
-                  to_string(*labelset(), labelset()->value(std::get<0>(cc))),
-                  '-',
-                  to_string(*labelset(), labelset()->value(std::get<1>(cc))));
+          VCSN_REQUIRE(end != std::end(gens),
+                       self(), ": invalid letter interval: ",
+                       to_string(*labelset(), ls.value(std::get<0>(cc))),
+                       '-',
+                       to_string(*labelset(), ls.value(std::get<1>(cc))));
           for (++end; i != end; ++i)
-            res = add(res, atom(labelset()->value(*i)));
+            res = add(res, atom(ls.value(*i)));
         }
     // [^].
     else if (ccs.empty())
       for (auto l: gens)
-        res = add(res, atom(labelset()->value(l)));
+        res = add(res, atom(ls.value(l)));
     // [^a-z].
     else
       {
@@ -1071,17 +1072,17 @@ namespace vcsn
           {
             auto i = find(gens, cc.first);
             auto end = std::find(i, std::end(gens), cc.second);
-            require(end != std::end(gens),
-                    "invalid letter interval: ",
-                    to_string(*labelset(), labelset()->value(std::get<0>(cc))),
-                    '-',
-                    to_string(*labelset(), labelset()->value(std::get<1>(cc))));
+            VCSN_REQUIRE(end != std::end(gens),
+                         "invalid letter interval: ",
+                         to_string(*labelset(), ls.value(std::get<0>(cc))),
+                         '-',
+                         to_string(*labelset(), ls.value(std::get<1>(cc))));
             for (++end; i != end; ++i)
               accepted.emplace(*i);
           }
         for (auto c: gens)
           if (!has(accepted, c))
-            res = add(res, atom(labelset()->value(c)));
+            res = add(res, atom(ls.value(c)));
       }
     require(!is_zero(res),
             "invalid empty letter class");

vcsn/labelset/genset-labelset.hh
@@ -78,8 +78,8 @@ namespace vcsn
       get_letter(std::istream& i, bool quoted = true) const
       {
         letter_t res = this->genset()->get_letter(i, quoted);
-        require(this->has(res),
-                "invalid label: unexpected ", str_escape(res));
+        VCSN_REQUIRE(this->has(res),
+                     "invalid label: unexpected ", str_escape(res));
         return res;
       }
 

vcsn/labelset/letterset.hh
@@ -189,8 +189,8 @@ namespace vcsn
     value_t
     conv(self_t, value_t v) const
     {
-      require(is_special(v) || is_valid(v),
-              *this, ": conv: invalid label: ", str_escape(v));
+      VCSN_REQUIRE(is_special(v) || is_valid(v),
+                   *this, ": conv: invalid label: ", str_escape(v));
       return v;
     }
 

vcsn/labelset/oneset.hh
@@ -152,8 +152,8 @@ namespace vcsn
     conv(const LabelSet& ls,
          typename LabelSet::value_t v) const
     {
-      require(ls.is_one(v),
-              sname(), ": conv: invalid label: ", to_string(ls, v));
+      VCSN_REQUIRE(ls.is_one(v),
+                   sname(), ": conv: invalid label: ", to_string(ls, v));
       return one();
     }
 

vcsn/labelset/wordset.hh
@@ -212,8 +212,8 @@ namespace vcsn
       else
         {
           auto res = value(v);
-          require(is_valid(res),
-                  *this, ": conv: invalid label: ", str_escape(v));
+          VCSN_REQUIRE(is_valid(res),
+                       *this, ": conv: invalid label: ", str_escape(v));
           return res;
         }
     }
@@ -299,9 +299,9 @@ namespace vcsn
     static value_t ldiv(const value_t& w1, const value_t& w2)
     {
       using boost::algorithm::starts_with;
-      require(starts_with(w2, w1),
-              sname(), ": ldiv: invalid arguments: ", str_escape(w1),
-              ", ", str_escape(w2));
+      VCSN_REQUIRE(starts_with(w2, w1),
+                   sname(), ": ldiv: invalid arguments: ", str_escape(w1),
+                   ", ", str_escape(w2));
       return {begin(w2) + size(w1), end(w2)};
     }
 

vcsn/misc/raise.hh
@@ -71,10 +71,23 @@ namespace vcsn
   }
 
   /// If \a b is not verified, raise an error with \a args as message.
+  ///
+  /// Beware that the arguments are, of course, always evaluated.  So
+  /// avoid passing costly function calls.  See VCSN_REQUIRE.
   template <typename Bool, typename... Args>
   inline void require(Bool b, Args&&... args)
   {
     if (!b)
       raise(std::forward<Args>(args)...);
   }
-};
+}
+
+/// A macro similar to require.
+///
+/// Its point is to avoid the cost of the evaluation of the variadic
+/// arguments.
+#define VCSN_REQUIRE(Cond, ...)                 \
+  do {                                          \
+    if (!(Cond))                                \
+      ::vcsn::raise(__VA_ARGS__);               \
+  } while (false)

vcsn/misc/stream.hh
@@ -31,9 +31,9 @@ namespace vcsn LIBVCSN_API
   {
     std::istringstream i{str};
     auto res = vs.conv(i, std::forward<Args>(args)...);
-    require(i.peek() == EOF,
-            vs.sname(), ": invalid value: ", str,
-            ", unexpected ", str_escape(i.peek()));
+    VCSN_REQUIRE(i.peek() == EOF,
+                 vs.sname(), ": invalid value: ", str,
+                 ", unexpected ", str_escape(i.peek()));
     return res;
   }
 

vcsn/weightset/min-plus.hh
@@ -132,7 +132,7 @@ namespace vcsn
           {
             is.ignore();
             int c = is.get();
-            require(c == 'o', "invalid value: o", str_escape(c));
+            VCSN_REQUIRE(c == 'o', "invalid value: o", str_escape(c));
             res = zero();
           }
         else if (!(is >> res))

vcsn/weightset/polynomialset.hh
@@ -1056,10 +1056,10 @@ namespace vcsn
           //
           // However, we must have at least a weight: a completely
           // empty mononial ($ -> 0 "<2>,") is invalid.
-          require(weighted,
-                  sname(), ": conv: invalid monomial: ",
-                  str_escape(peek),
-                  " (did you mean \\e or \\z?)");
+          VCSN_REQUIRE(weighted,
+                       sname(), ": conv: invalid monomial: ",
+                       str_escape(peek),
+                       " (did you mean \\e or \\z?)");
           res = labelset()->special();
         }
       else

tests/demo/prod-eval.cc
@@ -26,8 +26,7 @@ namespace vcsn
     {
       std::istringstream is{w};
       auto res = read_label(make_word_context(ctx), is);
-      vcsn::require(is.peek() == EOF,
-                    "unexpected trailing characters: ", is);
+      require(is.peek() == EOF, "unexpected trailing characters: ", is);
       return res;
     }
   }
@@ -39,9 +38,9 @@ namespace vcsn
     dyn::automaton res = dyn::read_automaton(f);
     // Automaton typename.
     auto vname = res->vname();
-    require(vname == Aut::element_type::sname(),
-            f, ": invalid context: ", vname,
-            ", expected: ", Aut::element_type::sname());
+    VCSN_REQUIRE(vname == Aut::element_type::sname(),
+                 f, ": invalid context: ", vname,
+                 ", expected: ", Aut::element_type::sname());
     return std::move(res->as<Aut>());
   }
 
@@ -52,8 +51,7 @@ namespace vcsn
     auto c = make_word_context(ctx);
     std::istringstream is{w};
     auto res = read_label(make_word_context(ctx), is);
-    vcsn::require(is.peek() == EOF,
-                  "unexpected trailing characters: ", is);
+    require(is.peek() == EOF, "unexpected trailing characters: ", is);
     return res;
   }
 }