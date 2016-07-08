b9e9eee0e4a7a685d4b9211cebbec7897fb1d1d2
Clément Gillard <clement.gillard@epita.fr>
2016-07-08 12:50:08 +0200

    compile: remove gcc static-if warning
    Remove GCC's `-Wunused-parameter` warning from static-if.hh.
    While at it, fix typo.
    
    * vcsn/misc/static-if.hh: Here.

--- vcsn/misc/static-if.hh
+++ vcsn/misc/static-if.hh
@@ -35,15 +35,22 @@ namespace vcsn
     template <bool cond, typename Then>
     auto static_if(Then&& then)
     {
+#if defined __GNUC__ && ! defined __clang__
+# pragma GCC diagnostic push
+# pragma GCC diagnostic ignored "-Wunused-parameter"
+#endif
       return static_if<cond>(std::forward<Then>(then),
 #if (defined __clang__ && __clang_major__ == 3 && __clang_minor__ < 6   \
      || defined __GNUC__ && !defined __clang__ && __GNUC__ < 5)
                      // Clang 3.5 and GCC 4.9 require that we name the argument.
-                             [](auto&&... argss){}
+                             [](auto&&... args){}
 #else
                              [](auto&&...){}
 #endif
                              );
+#if defined __GNUC__ && ! defined __clang__
+# pragma GCC diagnostic pop
+#endif
     }
   }
 }
