187522aa96879bef008cc94c4c25f1bc01c379b7
Akim Demaille <akim@lrde.epita.fr>
2016-11-01 17:06:41 +0100

    tests: factory: improve likeliness of passing a random test
    * tests/python/factory.py: We don't need `!` when trying to generate a
    weight.

--- tests/python/factory.py
+++ tests/python/factory.py
@@ -232,7 +232,7 @@ for _ in range(10):
 
 # Check rweight and lweight.
 exp = randexp('lal_char(abc), b',
-              '+=1,!=0.5,w.=1,length=50', identities='none')
+              '+=1, w.=1', length=50, identities='none')
 info = exp.info()
 print("Info: ", info)
 CHECK_NE(info['lweight'], 0)
@@ -240,7 +240,7 @@ CHECK_EQ(info['rweight'], 0)
 
 # Check the weight generation on expression.
 exp = randexp('lal_char(abc), b',
-              '+, w., w="1=1", length=20', identities='none')
+              '+, w., w="1=1"', length=20, identities='none')
 CHECK_NE(str(exp).find('<1>'), -1)
 CHECK_EQ(str(exp).find('<0>'), -1)
 
