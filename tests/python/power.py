2fd5b7dc6f953c1099a7a7fcce4fc6b11bca28f5
Akim Demaille <akim@lrde.epita.fr>
2017-01-09 09:27:56 +0100

    tests: check that repeated conjunction delivers an accessible automaton
    * tests/python/power.py: here.

--- tests/python/power.py
+++ tests/python/power.py
@@ -5,8 +5,6 @@ from test import *
 
 z = vcsn.context('lal_char(01), z')
 
-# FIXME: check that only the accessible part is kept.  Start from
-# binary.gv, modified with an inaccessible state.
 binary = load('lal_char_z/binary.gv')
 
 # power 0.
@@ -35,3 +33,10 @@ CHECK_EQ(z.weight('32'), binary5('10'))
 # Power 7.
 binary7 = binary & 7
 CHECK_EQ(z.weight('128'), binary7('10'))
+
+# Check that only the accessible part is kept using binary.gv,
+# modified with an inaccessible part.
+inacc = binary + vcsn.automaton('''context = lal, z
+0 -> 1 0''')
+for i in [0, 1, 4, 7]:
+    CHECK_EQ(binary & i, (inacc & i).__value__())
