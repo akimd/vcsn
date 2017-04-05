aaac589ba8dc21dec278575a6db08244bacb5c8c
Akim Demaille <akim@lrde.epita.fr>
2017-04-05 08:57:14 +0200

    expressions: make a clean error on mixing number of tuples
    Currently we make a compilation-failure error, which is not so nice.
    Preserve the compile-time error for the static layer, make it an
    exception for dyn.
    
    * vcsn/dyn/value.hh: Here.
    * tests/rat/tuple.rat: Check it.

--- vcsn/dyn/value.hh
+++ vcsn/dyn/value.hh
@@ -7,6 +7,9 @@
 #include <vcsn/misc/export.hh>
 #include <vcsn/misc/symbol.hh>
 
+// FIXME: we do not want this include!
+#include <vcsn/labelset/tupleset.hh> // is_multitape
+
 namespace vcsn
 {
   namespace dyn
@@ -140,12 +143,30 @@ namespace vcsn
     auto
     join(const value_impl<Tag>& lhs, const value_impl<Tag>& rhs)
     {
-      const auto& l = lhs->template as<ValueSetLhs>();
-      const auto& r = rhs->template as<ValueSetRhs>();
-      auto rs = join(l.valueset(), r.valueset());
-      auto lr = rs.conv(l.valueset(), l.value());
-      auto rr = rs.conv(r.valueset(), r.value());
-      return std::make_tuple(rs, lr, rr);
+      constexpr auto valid
+        = vcsn::detail::is_multitape<ValueSetLhs>()
+        == vcsn::detail::is_multitape<ValueSetRhs>();
+      // The type used when throwing an error.
+      using stub_t = std::tuple<ValueSetLhs,
+                                typename ValueSetLhs::value_t,
+                                typename ValueSetLhs::value_t>;
+      return vcsn::detail::static_if<valid>
+        ([](const auto& l, const auto& r)
+         {
+           auto rs = join(l.valueset(), r.valueset());
+           auto lr = rs.conv(l.valueset(), l.value());
+           auto rr = rs.conv(r.valueset(), r.value());
+           return std::make_tuple(rs, lr , rr);
+         },
+         [](const auto& l, const auto& r) -> stub_t
+         {
+           raise("join: cannot mix tuplesets and non tuplesets: ",
+                 to_string(l.valueset(), l.value()),
+                 " and ",
+                 to_string(r.valueset(), r.value()));
+         })
+        (lhs->template as<ValueSetLhs>(),
+         rhs->template as<ValueSetRhs>());
     }
 
     // A class representing an expansion/expansionset.
