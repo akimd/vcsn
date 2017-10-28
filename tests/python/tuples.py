cb010365b13026fc389c7aa4e1e173ecf57a7fdb
Akim Demaille <akim.demaille@gmail.com>
2017-10-28 13:43:15 +0200

    lan: more removals for nullableset
    * doc/notebooks/ICTAC-2017.ipynb, doc/notebooks/SACS-2017.ipynb,
    * tests/python/tuples.py: here.
    
    From 66 failures to 33.

--- tests/python/tuples.py
+++ tests/python/tuples.py
@@ -48,7 +48,7 @@ check('lal_char(ab), lat<expressionset<lal_char(xyz), lat<q, q>>, lat<q, q>>',
 ind = lambda e: vcsn.context('lan, q').expression(e).inductive()
 a1 = ind('<2>[ab]')
 a2 = ind('x')
-CHECK_EQ('''context = lat<nullableset<letterset<char_letters(ab)>>, nullableset<letterset<char_letters(x)>>>, q
+CHECK_EQ('''context = lat<letterset<char_letters(ab)>, letterset<char_letters(x)>>, q
 $ -> 0
 0 -> 1 <2>a|x
 0 -> 2 <2>b|x
