9ff9b7fb07fe6564e173547a7fa32c9951a52320
Akim Demaille <akim@lrde.epita.fr>
2018-01-18 06:41:55 +0100

    dyn: use C++17 namespaces
    * lib/vcsn/dyn/translate.hh: here.

--- lib/vcsn/dyn/translate.hh
+++ lib/vcsn/dyn/translate.hh
@@ -5,22 +5,19 @@
 
 #include <vcsn/misc/fwd.hh>
 
-namespace vcsn
+namespace vcsn::dyn
 {
-  namespace dyn
+  /// An exception suited for our compilation errors.
+  struct jit_error: std::runtime_error
   {
-    /// An exception suited for our compilation errors.
-    struct jit_error: std::runtime_error
-    {
-      jit_error(const std::string& assert, const std::string& what);
-      /// If defined, static assertions that failed (ends with a eol).
-      std::string assertions;
-    };
+    jit_error(const std::string& assert, const std::string& what);
+    /// If defined, static assertions that failed (ends with a eol).
+    std::string assertions;
+  };
 
-    /// Compile, and load, a DSO with instantiations for \a ctx.
-    void compile(const std::string& ctx);
+  /// Compile, and load, a DSO with instantiations for \a ctx.
+  void compile(const std::string& ctx);
 
-    /// Compile, and load, a DSO which instantiates \a algo for \a sig.
-    void compile(const std::string& algo, const signature& sig);
-  } // namespace dyn
-} // namespace vcsn
+  /// Compile, and load, a DSO which instantiates \a algo for \a sig.
+  void compile(const std::string& algo, const signature& sig);
+}
