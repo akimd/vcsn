4cc1fee3b1dc51ebc0288ac7a494bdc56a65d198
Akim Demaille <akim@lrde.epita.fr>
2018-01-22 21:34:21 +0100

    tests: avoid letterset<char_letters> in expected output
    * doc/notebooks/automaton.evaluate.ipynb,
    * tests/python/complete.py: here.

--- tests/python/complete.py
+++ tests/python/complete.py
@@ -3,111 +3,57 @@
 import vcsn
 from test import *
 
-# check complete algorithm
-# ------------------------
-def check(i, o):
-    if isinstance(i, str):
-        i = vcsn.automaton(i)
-    CHECK(not i.is_complete())
-    o = vcsn.automaton(o)
-    CHECK(o.is_complete())
-    CHECK_EQ(o, i.complete())
+# check(input, expected-output)
+# -----------------------------
+def check(aut, exp):
+    if isinstance(aut, str):
+        aut = vcsn.automaton(aut)
+    CHECK(not aut.is_complete())
+    exp = vcsn.automaton(exp)
+    CHECK_EQ(exp, aut.complete())
+    CHECK(exp.is_complete())
     # Idempotence.
-    CHECK_EQ(o, o.complete())
+    CHECK_EQ(exp, exp.complete())
 
 
 check('''
-digraph
-{
-  vcsn_context = "lal(abcd), b"
-  I -> 0
-  0 -> 1 [label = "a"]
-  0 -> 2 [label = "b"]
-  1 -> 2 [label = "c"]
-  2 -> F
-}
+context = "lal(abcd), b"
+$ 0
+0 1 a
+0 2 b
+1 2 c
+2 $
 ''', '''
-digraph
-{
-  vcsn_context = "lal(abcd), b"
-  rankdir = LR
-  {
-    node [shape = point, width = 0]
-    I0
-    F2
-  }
-  {
-    node [shape = circle]
-    0
-    1
-    2
-    3 [color = DimGray]
-  }
-  I0 -> 0
-  0 -> 1 [label = "a"]
-  0 -> 2 [label = "b"]
-  0 -> 3 [label = "c, d", color = DimGray]
-  1 -> 2 [label = "c"]
-  1 -> 3 [label = "a, b, d", color = DimGray]
-  2 -> F2
-  2 -> 3 [label = "a, b, c, d", color = DimGray]
-  3 -> 3 [label = "a, b, c, d", color = DimGray]
-}
+context = "lal(abcd), b"
+$ -> 0
+0 -> 1 a
+0 -> 2 b
+0 -> 3 c, d
+1 -> 2 c
+1 -> 3 a, b, d
+2 -> $
+2 -> 3 a, b, c, d
+3 -> 3 a, b, c, d
 ''')
 
 # An automaton with an open context.
 check(vcsn.b.expression('a').standard(), '''
-digraph
-{
-  vcsn_context = "letterset<char_letters(a)>, b"
-  rankdir = LR
-  edge [arrowhead = vee, arrowsize = .6]
-  {
-    node [shape = point, width = 0]
-    I0
-    F1
-  }
-  {
-    node [shape = circle, style = rounded, width = 0.5]
-    0
-    1
-    2 [color = DimGray]
-  }
-  I0 -> 0
-  0 -> 1 [label = "a"]
-  1 -> F1
-  1 -> 2 [label = "a", color = DimGray]
-  2 -> 2 [label = "a", color = DimGray]
-}
+context = "lal(a), b"
+  $ -> 0
+  0 -> 1 a
+  1 -> $
+  1 -> 2 a
+  2 -> 2 a
 ''')
 
 # An automaton without initial state.
 check('''
-digraph
-{
-  vcsn_context = "lal(a), b"
-
-  0 -> 0 [label = "a"]
-  0 -> F0
-}
+context = "lal(a), b"
+0 -> 0 a
+0 -> $
 ''', '''
-digraph
-{
-  vcsn_context = "lal(a), b"
-  rankdir = LR
-  {
-    node [shape = point, width = 0]
-    I1
-    F0
-  }
-  {
-    node [shape = circle]
-    0 [color = DimGray]
-    1 [color = DimGray]
-  }
-  I1 -> 1 [color = DimGray]
-  0 -> F0 [color = DimGray]
-  0 -> 0 [label = "a", color = DimGray]
-  1 -> 1 [label = "a", color = DimGray]
-}
+  0 -> $
+  0 -> 0 a
+  1 -> 1 a
+  $ -> 1
 ''')
