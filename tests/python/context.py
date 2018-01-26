cca95bf1cd1f2555837808ce9481137768217435
Akim Demaille <akim@lrde.epita.fr>
2018-01-26 18:57:58 +0100

    tests: contexts: check the UTF8 rather than the sname one
    * tests/python/context.py: here.

--- tests/python/context.py
+++ tests/python/context.py
@@ -5,11 +5,11 @@ import re
 import vcsn
 from test import *
 
-def check(ctx, exp=None, format="sname"):
+def check(ctx, exp=None, fmt="utf8"):
     c = vcsn.context(ctx)
     if exp is None:
         exp = ctx
-    CHECK_EQ(exp, c.format(format))
+    CHECK_EQ(exp, c.format(fmt))
 
 # Invalid context: invalid weightset.
 XFAIL(lambda: vcsn.context("lal(a)_UNKNOWN"),
@@ -34,14 +34,11 @@ lal
 ## --------------------- ##
 
 # Different types of syntactic sugar.
-for c in ['lal<char(abc)>, b',
-          'lal(abc), b']:
-    check(c, 'letterset<char_letters(abc)>, b')
+for c in ['lal<char(abc)>, b', 'lal(abc), b']:
+    check(c, '[abc]? → 𝔹')
 
-# An open context is not printed as open in text.
-for c in ['lal<char>, b',
-          'lal, b']:
-    check(c, 'letterset<char_letters()>, b')
+for c in ['lal<char>, b', 'lal, b']:
+    check(c, '[...]? → 𝔹')
 
 # An open context is printed as open in LaTeX.
 check('lal, b',      r'\{\ldots\}^?\to\mathbb{B}', 'latex')
@@ -55,16 +52,16 @@ check('lal(), b',    r'[]? -> B',             'text')
 check('lal(), b',    r'[]? → 𝔹',             'utf8')
 
 # letterset and different char_letters.
-check(r'lal(), b',       r'letterset<char_letters()>, b')
-check(r'lal(ab), b',     r'letterset<char_letters(ab)>, b')
-check(r'lal(a-kv-z), b', r'letterset<char_letters(abcdefghijkvwxyz)>, b')
-check(r'lal(-0-9), b',   r'letterset<char_letters(\-0123456789)>, b')
-check(r'lal(<>[]{}), b', r'letterset<char_letters(<>[]{})>, b')
-check(r'lal(\\\-\)), b', r'letterset<char_letters(\)\-\\)>, b')
-check(r'lal(\--\-), b',  r'letterset<char_letters(\-)>, b')
+check(r'lal(), b',       r'[]? → 𝔹')
+check(r'lal(ab), b',     r'[ab]? → 𝔹')
+check(r'lal(a-kv-z), b', r'[abcdefghijkvwxyz]? → 𝔹')
+check(r'lal(-0-9), b',   r'[\-0123456789]? → 𝔹')
+check(r'lal(<>[]{}), b', r'[\<\>\[\]{}]? → 𝔹')
+check(r'lal(\\\-\)), b', r'[)\-\\]? → 𝔹')
+check(r'lal(\--\-), b',  r'[\-]? → 𝔹')
 
 # Check that we cover all the possible chars.  Unfortunately as this
-# is not valid UTF-8, Python will refuse to display it.  Usinsg
+# is not valid UTF-8, Python will refuse to display it.  Using
 # `shortest` is a workaround to display the full range.
 CHECK_EQ(r'''\x01 + \x02 + \x03 + \x04 + \x05 + \x06 + \x07 + \x08 + \x09 + \n + \x0b + \x0c + \x0d + \x0e + \x0f + \x10 + \x11 + \x12 + \x13 + \x14 + \x15 + \x16 + \x17 + \x18 + \x19 + \x1a + \x1b + \x1c + \x1d + \x1e + \x1f + \  + ! + " + # + $ + % + & + \' + ( + ) + * + + + \, + \- + . + / + 0 + 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + : + ; + \< + = + \> + ? + @ + A + B + C + D + E + F + G + H + I + J + K + L + M + N + O + P + Q + R + S + T + U + V + W + X + Y + Z + \[ + \\ + \] + ^ + _ + ` + a + b + c + d + e + f + g + h + i + j + k + l + m + n + o + p + q + r + s + t + u + v + w + x + y + z + { + \| + } + ~ + \x7f + \x80 + \x81 + \x82 + \x83 + \x84 + \x85 + \x86 + \x87 + \x88 + \x89 + \x8a + \x8b + \x8c + \x8d + \x8e + \x8f + \x90 + \x91 + \x92 + \x93 + \x94 + \x95 + \x96 + \x97 + \x98 + \x99 + \x9a + \x9b + \x9c + \x9d + \x9e + \x9f + \xa0 + \xa1 + \xa2 + \xa3 + \xa4 + \xa5 + \xa6 + \xa7 + \xa8 + \xa9 + \xaa + \xab + \xac + \xad + \xae + \xaf + \xb0 + \xb1 + \xb2 + \xb3 + \xb4 + \xb5 + \xb6 + \xb7 + \xb8 + \xb9 + \xba + \xbb + \xbc + \xbd + \xbe + \xbf + \xc0 + \xc1 + \xc2 + \xc3 + \xc4 + \xc5 + \xc6 + \xc7 + \xc8 + \xc9 + \xca + \xcb + \xcc + \xcd + \xce + \xcf + \xd0 + \xd1 + \xd2 + \xd3 + \xd4 + \xd5 + \xd6 + \xd7 + \xd8 + \xd9 + \xda + \xdb + \xdc + \xdd + \xde + \xdf + \xe0 + \xe1 + \xe2 + \xe3 + \xe4 + \xe5 + \xe6 + \xe7 + \xe8 + \xe9 + \xea + \xeb + \xec + \xed + \xee + \xef + \xf0 + \xf1 + \xf2 + \xf3 + \xf4 + \xf5 + \xf6 + \xf7 + \xf8 + \xf9 + \xfa + \xfb + \xfc + \xfd + \xfe''',
          vcsn
@@ -72,39 +69,39 @@ CHECK_EQ(r'''\x01 + \x02 + \x03 + \x04 + \x05 + \x06 + \x07 + \x08 + \x09 + \n +
          .expression('[^]')
          .shortest(len=1))
 
-check('lal(ab), q', 'letterset<char_letters(ab)>, q')
+check('lal(ab), q', '[ab]? → ℚ')
 
 
 ## ------------------- ##
 ## LabelSet: wordset.  ##
 ## ------------------- ##
 
-check('wordset<string_letters>, b', 'wordset<string_letters()>, b')
+check('wordset<string_letters>, b', '[...]* → 𝔹')
 
 
 ## ------------------------- ##
 ## LabelSet: expressionset.  ##
 ## ------------------------- ##
 
-check('expressionset<lal(ab), b>, b', 'expressionset<letterset<char_letters(ab)>, b>, b')
+check('expressionset<lal(ab), b>, b', 'RatE[[ab]? → 𝔹] → 𝔹')
 # expressions weighted by expressions.
-check('expressionset<letterset<char_letters(ab)>, expressionset<letterset<char_letters(xy)>, q>>, b')
+check('RatE[[ab]? → RatE[[xy]? → ℚ]] → 𝔹')
 
 ## -------------------------- ##
 ## WeightSet: polynomialset.  ##
 ## -------------------------- ##
 
 check('lal(ab), polynomialset<law(xy), q>',
-      'letterset<char_letters(ab)>, polynomialset<wordset<char_letters(xy)>, q>')
+      '[ab]? → Poly[[xy]* → ℚ]')
 
 
 ## -------------------- ##
 ## LabelSet: tupleset.  ##
 ## -------------------- ##
 
-check('lat<letterset<char_letters(a)>, letterset<char_letters(a)>>, b')
-check('lat<lal(a),lal(a)>,b', 'lat<letterset<char_letters(a)>, letterset<char_letters(a)>>, b')
-check('lal(ab), lat<b, z>', 'letterset<char_letters(ab)>, lat<b, z>')
+check('[a]? × [a]? → 𝔹')
+check('lat<lal(a),lal(a)>,b', '[a]? × [a]? → 𝔹')
+check('lal(ab), lat<b, z>', '[ab]? → 𝔹 × ℤ')
 
 # Tuple contexts.
 c1 = vcsn.context('lal(abc), b')
@@ -119,10 +116,10 @@ CHECK_EQ(vcsn.context('lat<lal(abc), lal(xyz)>, q'), c1 | c2)
 # This one triggered a bug: shortest is not instantiable because we
 # fail to support label * word in this context.
 check('lat<lal>, b',
-      'lat<letterset<char_letters()>>, b')
+      '[...]? → 𝔹')
 
 check('lat<lat<lal(a)>>, b',
-      'lat<lat<letterset<char_letters(a)>>>, b')
+      '([a]?) → 𝔹')
 
 ctx = '''lat<lal(ba),lal(vu), law(x-z)>,
          lat<expressionset<lat<lal(fe), lal(hg)>, q>, r, q>'''
@@ -137,15 +134,15 @@ check(ctx,
 # Check that spaces are generously accepted.
 ctx = '''
 lat < lal (ba) ,
-                    lat < lal(vu) , law(x-z) >
-               >
+      lat < lal(vu) , law(x-z) >
+    >
 ,
 lat < expressionset < lat < lal < char (fe) > , lal (hg) >  ,
                       lat < r, q > > ,
       lat < b , q >
     >
 '''
-exp = 'lat<letterset<char_letters(ab)>, lat<letterset<char_letters(uv)>, wordset<char_letters(xyz)>>>, lat<expressionset<lat<letterset<char_letters(ef)>, letterset<char_letters(gh)>>, lat<r, q>>, lat<b, q>>'
+exp = '[ab]? × ([uv]? × [xyz]*) → RatE[[ef]? × [gh]? → ℝ × ℚ] × (𝔹 × ℚ)'
 check(ctx, exp)
 ctx = re.sub(r'\s+', '', ctx)
 check(ctx, exp)
