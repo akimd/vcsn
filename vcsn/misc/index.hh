e458e238737db689fa206518fb185af6ead2e792
Akim Demaille <akim@lrde.epita.fr>
2018-02-05 08:37:09 +0100

    C++17: nested namespaces
    * vcsn/misc/index.hh: here.

--- vcsn/misc/index.hh
+++ vcsn/misc/index.hh
@@ -3,45 +3,42 @@
 #include <functional> // std::hash
 #include <type_traits>
 
-namespace vcsn
+namespace vcsn::detail
 {
-  namespace detail
+  /// Lightweight state/transition handle (or index).
+  template <typename Tag>
+  struct index_t_impl
   {
-    /// Lightweight state/transition handle (or index).
-    template <typename Tag>
-    struct index_t_impl
+    using index_t = unsigned;
+
+    constexpr index_t_impl(index_t i)
+    : s{i}
+    {}
+
+    // Disallow index1_t i{index2_t{42}};
+    template <typename T>
+    index_t_impl(index_t_impl<T> t) = delete;
+
+    bool operator==(index_t_impl t) const
     {
-      using index_t = unsigned;
+      return s == t.s;
+    }
 
-      constexpr index_t_impl(index_t i)
-        : s{i}
-      {}
+    // Disallow index1_t{42} ==  index2_t{42};
+    template <typename T>
+    bool operator==(index_t_impl<T> t) const = delete;
 
-      // Disallow index1_t i{index2_t{42}};
-      template <typename T>
-      index_t_impl(index_t_impl<T> t) = delete;
+    /// Default ctor to please containers.
+    index_t_impl() = default;
+    constexpr operator index_t() const { return s; }
+    /// Be compliant with Boost integer ranges.
+    index_t_impl& operator++() { ++s; return *this; }
+    /// Be compliant with Boost integer ranges.
+    index_t_impl& operator--() { --s; return *this; }
 
-      bool operator==(index_t_impl t) const
-      {
-        return s == t.s;
-      }
-
-      // Disallow index1_t{42} ==  index2_t{42};
-      template <typename T>
-      bool operator==(index_t_impl<T> t) const = delete;
-
-      /// Default ctor to please containers.
-      index_t_impl() = default;
-      constexpr operator index_t() const { return s; }
-      /// Be compliant with Boost integer ranges.
-      index_t_impl& operator++() { ++s; return *this; }
-      /// Be compliant with Boost integer ranges.
-      index_t_impl& operator--() { --s; return *this; }
-
-    private:
-      index_t s;
-    };
-  }
+  private:
+    index_t s;
+  };
 }
 
 namespace std
