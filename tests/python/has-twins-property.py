626a1d441532a13ae42f7aca5f70c0fd83752e63
Akim Demaille <akim@lrde.epita.fr>
2018-02-05 07:13:14 +0100

    tests: clarify
    Prefer daut to dot.
    
    * tests/python/is-ambiguous.py: Reorder to reduce "scopes".

--- tests/python/has-twins-property.py
+++ tests/python/has-twins-property.py
@@ -4,138 +4,120 @@ import vcsn
 from test import *
 
 aut1 = vcsn.automaton(r'''
-digraph
-{
-  vcsn_context = "[abcd]? → ℤmin"
-
-  I0 -> 0
-  0 -> 1 [label = "<1>a"]
-  0 -> 2 [label = "<2>a"]
-  1 -> 1 [label = "<3>b"]
-  1 -> 3 [label = "<5>c"]
-  2 -> 2 [label = "<3>b"]
-  2 -> 3 [label = "<6>d"]
-  3 -> F3 [label = "<0>"]
-}
+context = [abcd]? → ℤmin
+$ -> 0 <0>
+0 -> 1 <1>a
+0 -> 2 <2>a
+1 -> 1 <3>b
+1 -> 3 <5>c
+2 -> 2 <3>b
+2 -> 3 <6>d
+3 -> $ <0>
 ''')
 CHECK(aut1.has_twins_property())
 
 
 aut2 = vcsn.automaton(r'''
-digraph
-{
-  vcsn_context = "[abcd]? → ℤmin"
-
-  I0 -> 0
-  0 -> 1 [label = "<1>a"]
-  0 -> 2 [label = "<2>a"]
-  1 -> 1 [label = "<3>b"]
-  1 -> 3 [label = "<5>c"]
-  2 -> 2 [label = "<4>b"]
-  2 -> 3 [label = "<6>d"]
-  3 -> F3 [label = "<0>"]
-}
+context = [abcd]? → ℤmin
+$ -> 0 <0>
+0 -> 1 <1>a
+0 -> 2 <2>a
+1 -> 1 <3>b
+1 -> 3 <5>c
+2 -> 2 <4>b
+2 -> 3 <6>d
+3 -> $ <0>
 ''')
 CHECK(not aut2.has_twins_property())
 
 
-aut3 = vcsn.automaton('''digraph
-{
-  vcsn_context = "[abcd]? → ℤmin"
-  I0 -> 0
-  0 -> 1 [label = "<2>a"]
-  0 -> 2 [label = "<3>a"]
-  1 -> 3 [label = "<3>b"]
-  1 -> 4 [label = "<2>c"]
-  2 -> 4 [label = "<5>d"]
-  2 -> 5 [label = "<3>b"]
-  3 -> 6 [label = "<6>c"]
-  4 -> F4
-  5 -> 7 [label = "<6>c"]
-  6 -> 1 [label = "<9>d"]
-  7 -> 2 [label = "<9>d"]
-}''')
+aut3 = vcsn.automaton('''context = [abcd]? → ℤmin
+$ -> 0 <0>
+0 -> 1 <2>a
+0 -> 2 <3>a
+1 -> 3 <3>b
+1 -> 4 <2>c
+2 -> 4 <5>d
+2 -> 5 <3>b
+3 -> 6 <6>c
+4 -> $ <0>
+5 -> 7 <6>c
+6 -> 1 <9>d
+7 -> 2 <9>d
+''')
 CHECK(aut3.has_twins_property())
 
 
-aut4 = vcsn.automaton('''digraph
-{
-  vcsn_context = "[abcd]? → ℤmin"
-  I0 -> 0
-  0 -> 1 [label = "<2>a"]
-  0 -> 2 [label = "<3>a"]
-  1 -> 3 [label = "<3>b"]
-  1 -> 4 [label = "<2>c"]
-  2 -> 4 [label = "<5>d"]
-  2 -> 5 [label = "<3>b"]
-  3 -> 6 [label = "<6>c"]
-  4 -> F4
-  5 -> 7 [label = "<6>c"]
-  6 -> 1 [label = "<9>d"]
-  7 -> 2 [label = "<13>d"]
-}''')
+aut4 = vcsn.automaton('''context = [abcd]? → ℤmin
+$ -> 0 <0>
+0 -> 1 <2>a
+0 -> 2 <3>a
+1 -> 3 <3>b
+1 -> 4 <2>c
+2 -> 4 <5>d
+2 -> 5 <3>b
+3 -> 6 <6>c
+4 -> $ <0>
+5 -> 7 <6>c
+6 -> 1 <9>d
+7 -> 2 <13>d
+''')
 CHECK(not aut4.has_twins_property())
 
 
-aut5 = vcsn.automaton('''digraph
-{
-  vcsn_context = "[abcd]? → ℚ"
-  I0 -> 0
-  0 -> 1 [label = "<2/3>a"]
-  0 -> 2 [label = "<3/5>a"]
-  1 -> 3 [label = "<4>b"]
-  1 -> 4 [label = "<2>c"]
-  2 -> 4 [label = "<7>d"]
-  2 -> 5 [label = "<2>b"]
-  3 -> 1 [label = "<6>d"]
-  4 -> F4
-  5 -> 2 [label = "<12>d"]
-}''')
+aut5 = vcsn.automaton('''context = [abcd]? → ℚ
+$ -> 0
+0 -> 1 <2/3>a
+0 -> 2 <3/5>a
+1 -> 3 <4>b
+1 -> 4 <2>c
+2 -> 4 <7>d
+2 -> 5 <2>b
+3 -> 1 <6>d
+4 -> $
+5 -> 2 <12>d
+''')
 CHECK(aut5.has_twins_property())
 
 
-aut6 = vcsn.automaton('''digraph
-{
-  vcsn_context = "[abcd]? → ℚ"
-  I0 -> 0
-  0 -> 1 [label = "<2/3>a"]
-  0 -> 2 [label = "<3/5>a"]
-  1 -> 3 [label = "<4>b"]
-  1 -> 4 [label = "<2>c"]
-  2 -> 4 [label = "<7>d"]
-  2 -> 5 [label = "<2>b"]
-  3 -> 1 [label = "<6>d"]
-  4 -> F4
-  5 -> 2 [label = "<17>d"]
-}''')
+aut6 = vcsn.automaton('''context = [abcd]? → ℚ
+$ -> 0
+0 -> 1 <2/3>a
+0 -> 2 <3/5>a
+1 -> 3 <4>b
+1 -> 4 <2>c
+2 -> 4 <7>d
+2 -> 5 <2>b
+3 -> 1 <6>d
+4 -> $
+5 -> 2 <17>d
+''')
 CHECK(not aut6.has_twins_property())
 
 
-aut7 = vcsn.automaton('''digraph {
-  vcsn_context = "[abc]? → ℤ"
-  I0 -> 0
-  0 -> 1 [label ="<3>a"]
-  0 -> 2 [label = "<5>a"]
-  1 -> 3 [label = "<3>b"]
-  1 -> F3
-  2 -> 4 [label = "<2>b"]
-  2 -> F3
-  3 -> 1 [label = "<2>a"]
-  4 -> 2 [label = "<3>a"]
-}''')
+aut7 = vcsn.automaton('''context = [abc]? → ℤ
+$ -> 0
+0 -> 1 <3>a
+0 -> 2 <5>a
+1 -> $
+1 -> 3 <3>b
+2 -> $
+2 -> 4 <2>b
+3 -> 1 <2>a
+4 -> 2 <3>a
+''')
 CHECK(aut7.has_twins_property())
 
 
-aut8 = vcsn.automaton('''digraph {
-  vcsn_context = "[abc]? → ℤ"
-  I0 -> 0
-  0 -> 1 [label ="<3>a"]
-  0 -> 2 [label = "<5>a"]
-  1 -> 3 [label = "<3>b"]
-  1 -> F3
-  2 -> 4 [label = "<2>b"]
-  2 -> F3
-  3 -> 1 [label = "<2>a"]
-  4 -> 2 [label = "<5>a"]
-}''')
+aut8 = vcsn.automaton('''context = [abc]? → ℤ
+$ -> 0
+0 -> 1 <3>a
+0 -> 2 <5>a
+1 -> $
+1 -> 3 <3>b
+2 -> $
+2 -> 4 <2>b
+3 -> 1 <2>a
+4 -> 2 <5>a
+''')
 CHECK(not aut8.has_twins_property())
--- tests/python/is-ambiguous.py
+++ tests/python/is-ambiguous.py
@@ -5,14 +5,11 @@ from test import *
 
 # Not deterministic, yet not ambiguous.
 a = vcsn.automaton('''
-digraph
-{
-  vcsn_context="lal(ab), b"
-  I -> 0
-  0 -> 1 [label = "a"]
-  0 -> 2 [label = "a"]
-  1 -> F
-}
+context = [ab]? → 𝔹
+$ -> 0
+0 -> 1 a
+0 -> 2 a
+1 -> $
 ''')
 CHECK(not a.is_ambiguous())
 XFAIL(lambda: a.ambiguous_word(), "automaton is unambiguous")
@@ -20,15 +17,12 @@ CHECK(not a.is_deterministic())
 
 # Not deterministic, and ambiguous.
 a = vcsn.automaton('''
-digraph
-{
-  vcsn_context="lal(ab), b"
-  I -> 0
-  0 -> 1 [label = "a"]
-  0 -> 2 [label = "a"]
-  1 -> F
-  2 -> F
-}
+context = [ab]? → 𝔹
+$ -> 0
+0 -> 1 a
+0 -> 2 a
+1 -> $
+2 -> $
 ''')
 CHECK(a.is_ambiguous())
 CHECK_EQ("a", a.ambiguous_word())
@@ -48,15 +42,12 @@ CHECK_EQ('introAAAoutro',
 # Test ambiguous word on automata with two ambiguous parts. Hence,
 # test monomial_path with states different from pre and post.
 b = vcsn.automaton('''
-digraph
-{
-  vcsn_context="lal(ab), b"
-  I -> 0
-  I -> 1
-  0 -> 2 [label = "a"]
-  1 -> 2 [label = "a"]
-  2 -> F
-}
+context = [ab]? → 𝔹
+$ -> 0
+$ -> 1
+0 -> 2 a
+1 -> 2 a
+2 -> $
 ''')
 CHECK(b.is_ambiguous())
 CHECK((b*a).is_ambiguous())
@@ -67,15 +58,12 @@ CHECK_EQ("aa", (a*b).proper().ambiguous_word())
 
 # Likewise, but with a non-commutative product.
 a = vcsn.automaton('''
-digraph
-{
-  vcsn_context="lal(ab), expressionset<lal(xy), b>"
-  I -> 0
-  0 -> 1 [label = "<x>a"]
-  0 -> 2 [label = "<y>a"]
-  1 -> F
-  2 -> F
-}
+context = [ab]? → RatE[[xy]? → 𝔹]
+$ -> 0
+0 -> 1 <x>a
+0 -> 2 <y>a
+1 -> $
+2 -> $
 ''')
 CHECK(a.is_ambiguous())
 CHECK_EQ("a", a.ambiguous_word())
@@ -83,15 +71,12 @@ CHECK(not a.is_deterministic())
 
 # deterministic, and unambiguous (obviously).
 a = vcsn.automaton('''
-digraph
-{
-  vcsn_context="lal(ab), b"
-  I -> 0
-  0 -> 1 [label = "a"]
-  0 -> 2 [label = "b"]
-  1 -> F
-  2 -> F
-}
+context = [ab]? → 𝔹
+$ -> 0
+0 -> 1 a
+0 -> 2 b
+1 -> $
+2 -> $
 ''')
 CHECK(not a.is_ambiguous())
 XFAIL(lambda: a.ambiguous_word(), "automaton is unambiguous")
@@ -101,21 +86,18 @@ CHECK(a.is_deterministic())
 # When using lightest to retrieve the shortest ambiguous word, ambiguous_word
 # would return abb instead of ab.
 a = vcsn.automaton('''
-digraph
-{
-  vcsn_context="lal(ab), nmin"
-  I -> 1
-  1 -> 2 [label = "a"]
-  1 -> 3 [label = "a"]
-  2 -> 2 [label = "b"]
-  2 -> 7 [label = "<4>b"]
-  3 -> 4 [label = "b"]
-  3 -> 5 [label = "<4>b"]
-  4 -> 6 [label = "b"]
-  5 -> F
-  6 -> F
-  7 -> F
-}
+context = [ab]? → ℕmin
+$ -> 0 <0>
+0 -> 1 <0>a
+0 -> 2 <0>a
+1 -> 1 <0>b
+1 -> 3 <4>b
+2 -> 4 <0>b
+2 -> 5 <4>b
+3 -> $ <0>
+4 -> 6 <0>b
+5 -> $ <0>
+6 -> $ <0>
 ''')
 CHECK(a.is_ambiguous())
 CHECK_EQ("ab", a.ambiguous_word())
@@ -125,62 +107,60 @@ CHECK(not a.is_deterministic())
 ## is_cycle_ambiguous.  ##
 ## -------------------- ##
 
-aut1 = vcsn.automaton('''digraph {
-  vcsn_context = "[abc]? → 𝔹"
-  I0 -> 0
-  0 -> 1 [label = "a"]
-  0 -> 2 [label = "a"]
-  1 -> 0 [label = "b"]
-  1 -> 3 [label = "b"]
-  2 -> 1 [label = "c"]
-  2 -> 2 [label = "b"]
-  3 -> F3
-  3 -> 1 [label = "c"]
-}''')
+ctx = vcsn.context("[abc] -> b")
+
+aut1 = vcsn.automaton('''context = [abc]? → 𝔹
+$ -> 0
+0 -> 1 a
+0 -> 2 a
+1 -> 0 b
+1 -> 3 b
+2 -> 1 c
+2 -> 2 b
+3 -> $
+3 -> 1 c
+''')
 CHECK(aut1.is_cycle_ambiguous())
 
-aut2 = vcsn.automaton('''digraph {
-  vcsn_context = "[abc]? → 𝔹"
-  I0 -> 0
-  0 -> 1 [label = "a"]
-  0 -> 2 [label = "a"]
-  1 -> 0 [label = "b"]
-  1 -> 3 [label = "b"]
-  2 -> 1 [label = "c"]
-  2 -> 2 [label = "b"]
-  3 -> F3
-  3 -> 1 [label = "b"]
-}''')
+e1 = "((abc)*){5}abc" + format(aut1.expression())
+aut = ctx.expression(e1).derived_term()
+CHECK(aut.is_cycle_ambiguous())
+
+
+aut2 = vcsn.automaton('''context = [abc]? → 𝔹
+$ -> 0
+0 -> 1 a
+0 -> 2 a
+1 -> 0 b
+1 -> 3 b
+2 -> 1 c
+2 -> 2 b
+3 -> $
+3 -> 1 b
+''')
 CHECK(not aut2.is_cycle_ambiguous())
 
-aut3 = vcsn.automaton('''
-digraph
-{
-  vcsn_context = "[abc]? → 𝔹"
-  I0 -> 0
-  0 -> 1 [label = "c"]
-  1 -> 2 [label = "a"]
-  2 -> 3 [label = "b"]
-  2 -> 4 [label = "b"]
-  3 -> 5 [label = "c"]
-  3 -> 6 [label = "c"]
-  4 -> 7 [label = "c"]
-  5 -> 2 [label = "a"]
-  6 -> 0 [label = "b"]
-  7 -> F7
-  7 -> 2 [label = "a"]
-}''')
-CHECK(aut3.is_cycle_ambiguous())
+e2 = "((abc)*){5}abc" + format(aut2.expression())
+aut = ctx.expression(e2).derived_term()
+CHECK(not aut.is_cycle_ambiguous())
 
-ctx = vcsn.context("lal(abc), b")
 
-r1 = "((abc)*){5}abc" + format(aut1.expression())
-aut4 = ctx.expression(r1).derived_term()
-CHECK(aut4.is_cycle_ambiguous())
+aut = vcsn.automaton('''
+context = [abc]? → 𝔹
+$ -> 0
+0 -> 1 c
+1 -> 2 a
+2 -> 3 b
+2 -> 4 b
+3 -> 5 c
+3 -> 6 c
+4 -> 7 c
+5 -> 2 a
+6 -> 0 b
+7 -> $
+7 -> 2 a''')
+CHECK(aut.is_cycle_ambiguous())
 
-r2 = "((abc)*){5}abc" + format(aut2.expression())
-aut5 = ctx.expression(r2).derived_term()
-CHECK(not aut5.is_cycle_ambiguous())
 
 CHECK(ctx.ladybird(5).is_cycle_ambiguous())
 CHECK(not ctx.de_bruijn(5).is_cycle_ambiguous())
